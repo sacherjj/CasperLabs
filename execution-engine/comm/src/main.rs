4e1f77e34b13c6cd67ad95f064691c005501ab84
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-12-12 19:07:06 +0100

    Read socket address from the cli

--- execution-engine/comm/src/main.rs
+++ execution-engine/comm/src/main.rs
@@ -1,19 +1,29 @@
+extern crate clap;
+extern crate execution_engine;
 extern crate grpc;
 extern crate protobuf;
-extern crate execution_engine;
 
 pub mod engine_server;
 
+use clap::{App, Arg};
 use engine_server::*;
 use execution_engine::engine::Engine;
 
 fn main() {
-    let server_builder = engine_server::new("/tmp/socket.sock", Engine::new());
-    let _server =  server_builder.build().expect("Start server");
+    let matches = App::new("Execution engine server")
+        .arg(Arg::with_name("socket").required(true).help("Socket file"))
+        .get_matches();
+    let socket = matches.value_of("socket").expect("missing required argument");
+    let socket_path = std::path::Path::new(socket);
+    if socket_path.exists() {
+        std::fs::remove_file(socket_path).expect("Remove old socket file.");
+    }
+
+    let server_builder = engine_server::new(socket, Engine::new());
+    let _server = server_builder.build().expect("Start server");
 
     // loop idefinitely
     loop {
         std::thread::park();
     }
-
 }
