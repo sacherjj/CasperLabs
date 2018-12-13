281950783ae90882cb98fd1bf7f521d2e6e726e2
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-12-13 16:24:48 +0100

    Preprocess deployed wasm

--- execution-engine/engine/src/engine.rs
+++ execution-engine/engine/src/engine.rs
@@ -1,3 +1,12 @@
+use parity_wasm::elements::Module;
+use wasm_prep::process;
+
+#[derive(Debug)]
+pub enum Error {
+    PreprocessingError { error: String },
+    SignatureError { error: String },
+}
+
 // TODO placeholder for execution engine
 pub struct EngineState<T> {
     // In Michael's PoC state is an instance of TrackingCopy.
@@ -8,25 +17,28 @@ pub struct EngineState<T> {
 
 impl<T> EngineState<T> {
     pub fn new(state: T) -> EngineState<T> {
-        EngineState {
-            state: state
-        }
+        EngineState { state: state }
     }
     //TODO run_deploy should perform preprocessing and validation of the deploy.
     //It should validate the signatures, ocaps etc.
-    pub fn run_deploy(&self, deploy: &Vec<u8>) -> Result<String, String> {
+    pub fn run_deploy(&self, deploy: &[u8]) -> Result<String, Error> {
         self.validate_signatures(deploy, &Vec::new(), "ed25519")?;
-        self.preprocess_deploy(deploy)?;
+        self.preprocess_module(deploy)?;
         Ok(String::from("OK"))
     }
 
-    //TODO return proper error or instance of Wasm module
-    fn preprocess_deploy(&self, deploy: &Vec<u8>) -> Result<String, String> {
-        Ok(String::from("OK"))
+    //TODO: inject gas counter, limit stack size etc
+    fn preprocess_module(&self, module_bytes: &[u8]) -> Result<Module, Error> {
+        process(module_bytes).map_err(|err_str| Error::PreprocessingError { error: err_str })
     }
 
     //TODO return proper error
-    fn validate_signatures(&self, deploy: &Vec<u8>, signature: &Vec<u8>, signature_alg: &str) -> Result<String, String> {
+    fn validate_signatures(
+        &self,
+        deploy: &[u8],
+        signature: &[u8],
+        signature_alg: &str,
+    ) -> Result<String, Error> {
         Ok(String::from("OK"))
     }
 }
--- execution-engine/engine/src/lib.rs
+++ execution-engine/engine/src/lib.rs
@@ -1,1 +1,5 @@
+extern crate parity_wasm;
+extern crate pwasm_utils;
+extern crate wasm_prep;
+
 pub mod engine;
