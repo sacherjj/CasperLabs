d267800192dd892eb63ae6981c9aaee260efa877
birchmd <birchmd@casperlabs.io>
2018-12-17 15:59:31 -0500

    hello world example contracts

---
+++ contract-examples/store-hello-name/src/lib.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![feature(alloc)]
+
+extern crate alloc;
+use alloc::string::String;
+
+extern crate common;
+use common::ext::{get_arg, ret, store_function};
+
+fn hello_name(name: &str) -> String {
+    let mut result = String::from("Hello, ");
+    result.push_str(name);
+    result
+}
+
+#[no_mangle]
+pub extern "C" fn hello_name_ext() {
+    let name: String = get_arg(0);
+    let y = hello_name(&name);
+    ret(&y);
+}
+
+#[no_mangle]
+pub extern "C" fn call() {
+    let export_name = String::from("hello_name_ext");
+    let hash = store_function(&export_name);
+}
