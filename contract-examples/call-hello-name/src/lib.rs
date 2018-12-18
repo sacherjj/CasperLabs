338de0e2dc0749539071bc9a5e1a7c85102862f8
birchmd <birchmd@casperlabs.io>
2018-12-18 10:50:02 -0500

    Add comment saying where the call_hello_name hash comes from

--- contract-examples/call-hello-name/src/lib.rs
+++ contract-examples/call-hello-name/src/lib.rs
@@ -12,6 +12,7 @@ use common::key::Key;
 
 #[no_mangle]
 pub extern "C" fn call() {
+    //This hash comes from the `_hash` output from `store_hello_name/src/lib.rs`
     let hash = Key::Hash([
         252, 193, 165, 207, 104, 236, 69, 249, 43, 184, 46, 86, 93, 138, 54, 138, 233, 69, 41, 234,
         54, 211, 111, 36, 103, 231, 187, 227, 15, 72, 71, 111,
