e046d4e53c8eab9b49482c4a070c17a3ae95e360
Igor Ramazanov <igor@casperlabs.io>
2018-12-20 17:07:20 +0300

    Use preexisting test as a basis for new test

--- integration-testing/test/test_propose.py
+++ integration-testing/test/test_propose.py
@@ -3,16 +3,18 @@ import os
 import pytest
 
 from rnode_testing.rnode import started_standalone_bootstrap_node
+from rnode_testing.rnode import node_payment_code
+from rnode_testing.rnode import node_session_code
 from rnode_testing.wait import (
     wait_for_approved_block_received_handler_state,
+    wait_for_blocks_count_at_least
 )
 
 from typing import TYPE_CHECKING
 
+
 def test_propose(started_standalone_bootstrap_node):
     wait_for_approved_block_received_handler_state(started_standalone_bootstrap_node, started_standalone_bootstrap_node.timeout)
-    relative_paths = started_standalone_bootstrap_node.shell_out('sh', '-c', 'ls /opt/docker/examples/*.rho').splitlines()
-    for relative_path in relative_paths:
-        full_path = os.path.join('/opt/docker/examples', relative_path)
-        started_standalone_bootstrap_node.deploy(full_path)
-        started_standalone_bootstrap_node.propose()
+    started_standalone_bootstrap_node.deploy(node_session_code, node_payment_code)
+    started_standalone_bootstrap_node.propose()
+    wait_for_blocks_count_at_least(started_standalone_bootstrap_node, 1, 1, started_standalone_bootstrap_node.timeout)
