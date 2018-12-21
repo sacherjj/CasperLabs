5617d4082b55bbb9a9a7c3ef52c9a48e3583e219
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-12-21 09:22:52 +0100

    Refactor function to FP style.

--- execution-engine/wasm-prep/src/lib.rs
+++ execution-engine/wasm-prep/src/lib.rs
@@ -4,6 +4,7 @@ extern crate pwasm_utils;
 use parity_wasm::elements::{self, deserialize_buffer, Error as ParityWasmError, Module};
 use pwasm_utils::externalize_mem;
 use std::error::Error;
+use std::iter::Iterator;
 
 const ALLOWED_IMPORTS: &'static [&'static str] = &[
     "read_value",
@@ -31,49 +32,60 @@ pub fn process(module_bytes: &[u8]) -> Result<Module, String> {
 }
 
 fn validate_imports(module: &Module) -> Result<(), String> {
-    for section in module.sections() {
-        let mut has_imported_memory_properly_named = false;
-        match *section {
-            elements::Section::Import(ref import_section) => {
-                for entry in import_section.entries() {
+    module
+        .import_section()
+        .ok_or("No imports section".to_owned())
+        .and_then(|import_section| {
+            import_section
+                .entries()
+                .iter()
+                .try_fold(false, |has_imported_memory_properly_named, ref entry| {
                     if entry.module() != "env" {
-                        return Err(String::from("All imports should be from env"))
+                        Err(String::from("All imports should be from env"))
                     } else {
                         match *entry.external() {
                             elements::External::Function(_) => {
                                 if !ALLOWED_IMPORTS.contains(&entry.field()) {
-                                    return Err(format!("'{}' is not supported by the runtime",  entry.field()))
+                                    Err(format!("'{}' is not supported by the runtime",  entry.field()))
+                                } else {
+                                    Ok(has_imported_memory_properly_named)
                                 }
                             },
                             elements::External::Memory(m) => {
-                                if entry.field() == "memory" {
-                                    has_imported_memory_properly_named  = true;
-                                }
-                                let max = if let Some(max)  = m.limits().maximum() {
-                                    max
-                                } else  {
-                                    return Err(String::from("There is a limit to Wasm memory. This program does not limit memory"))
-                                };
-
-                                if max > 305 {
-                                    return Err(format!("Wasm runtime has 10Mb limit (305 pages each 64KiB) on max contract memory. This program specific {}", max))
-                                }
+                                m
+                                    .limits()
+                                    .maximum()
+                                    .ok_or(String::from("There is a limit to Wasm memory. This program does not limit memory"))
+                                    .and_then(|max| {
+                                        if max > 305 {
+                                            Err(format!("Wasm runtime has 10Mb limit (305 pages each 64KiB) on max contract memory. This program specific {}", max))
+                                        } else {
+                                            Ok(has_imported_memory_properly_named)
+                                        }
+                                    })
+                                    .and_then(|_| {
+                                        if entry.field() == "memory" {
+                                            Ok(true) // memory properly imported
+                                        } else {
+                                            Ok(false)
+                                        }
+                                    })
                             },
                             elements::External::Global(_) => {
-                                return Err(String::from("No globals are provided with the runtime."))
+                                Err(String::from("No globals are provided with the runtime."))
                             },
-                            elements::External::Table(_) => { continue; }
+                            elements::External::Table(_) => { Ok(has_imported_memory_properly_named) }
                         }
                     }
-                }
-                if !has_imported_memory_properly_named {
-                    return Err(String::from("No imported memory from env::memory."))
-                }
-            }
-            _ => { continue; }
+                })
+        })
+    .and_then(|had_imported_memory_properly_named| {
+        if had_imported_memory_properly_named {
+            Ok(())
+        } else {
+            Err(String::from("No imported memory from env::memory."))
         }
-    }
-    Ok(())
+    })
 }
 
 fn remove_memory_export(module: &mut Module) -> Result<(), String> {
