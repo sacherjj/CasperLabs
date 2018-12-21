2a3bc0a546835517bf3f47291db7331f762c23a9
Mateusz Górski <gorski.mateusz@protonmail.ch>
2018-12-21 11:28:04 +0100

    Represent preprocessing errors as enums instead of Strings.

--- execution-engine/wasm-prep/src/lib.rs
+++ execution-engine/wasm-prep/src/lib.rs
@@ -25,33 +25,50 @@ const ALLOWED_IMPORTS: &'static [&'static str] = &[
 const MEM_PAGES: u32 = 128;
 const MAX_MEM_PAGES: u32 = 305; // 10mb
 
+#[derive(Debug)]
+pub enum PreprocessingError {
+    InvalidImportsError(String),
+    NoExportSection,
+    NoImportSection,
+    DeserializeError(String)
+}
 
-pub fn process(module_bytes: &[u8]) -> Result<Module, String> {
+use PreprocessingError::*;
+
+pub fn process(module_bytes: &[u8]) -> Result<Module, PreprocessingError> {
     // type annotation in closure needed
     let err_to_string = |err: ParityWasmError| err.description().to_owned();
-    let module = deserialize_buffer(module_bytes).map_err(err_to_string)?;
+    let module = deserialize_buffer(module_bytes).map_err(err_to_string).map_err(|error| DeserializeError(error))?;
     let mut ext_mod = externalize_mem(module, None, MEM_PAGES);
     remove_memory_export(&mut ext_mod)?;
 	validate_imports(&ext_mod)?;
     Ok(ext_mod)
 }
 
-fn validate_imports(module: &Module) -> Result<(), String> {
+fn invalid_imports<E: AsRef<str>>(s: E) -> PreprocessingError {
+    InvalidImportsError(s.as_ref().to_string())
+}
+
+fn invalid_imports_error<T, E: AsRef<str>>(s: E) -> Result<T, PreprocessingError> {
+    Err(invalid_imports(s))
+}
+
+fn validate_imports(module: &Module) -> Result<(), PreprocessingError> {
     module
         .import_section()
-        .ok_or("No imports section".to_owned())
+        .ok_or(NoImportSection)
         .and_then(|import_section| {
             import_section
                 .entries()
                 .iter()
                 .try_fold(false, |has_imported_memory_properly_named, ref entry| {
                     if entry.module() != "env" {
-                        Err(String::from("All imports should be from env"))
+                        invalid_imports_error("All imports should be from env")
                     } else {
                         match *entry.external() {
                             elements::External::Function(_) => {
                                 if !ALLOWED_IMPORTS.contains(&entry.field()) {
-                                    Err(format!("'{}' is not supported by the runtime",  entry.field()))
+                                    invalid_imports_error::<bool, String>(format!("'{}' is not supported by the runtime",  entry.field()))
                                 } else {
                                     Ok(has_imported_memory_properly_named)
                                 }
@@ -60,10 +77,10 @@ fn validate_imports(module: &Module) -> Result<(), String> {
                                 m
                                     .limits()
                                     .maximum()
-                                    .ok_or(String::from("There is a limit to Wasm memory. This program does not limit memory"))
+                                    .ok_or(invalid_imports("There is a limit to Wasm memory. This program does not limit memory"))
                                     .and_then(|max| {
                                         if max > MAX_MEM_PAGES {
-                                            Err(format!("Wasm runtime has 10Mb limit (305 pages each 64KiB) on max contract memory. This program specific {}", max))
+                                            invalid_imports_error::<bool, String>(format!("Wasm runtime has 10Mb limit (305 pages each 64KiB) on max contract memory. This program specific {}", max))
                                         } else {
                                             Ok(has_imported_memory_properly_named)
                                         }
@@ -77,7 +94,7 @@ fn validate_imports(module: &Module) -> Result<(), String> {
                                     })
                             },
                             elements::External::Global(_) => {
-                                Err(String::from("No globals are provided with the runtime."))
+                                invalid_imports_error::<bool, &str>("No globals are provided with the runtime.")
                             },
                             elements::External::Table(_) => { Ok(has_imported_memory_properly_named) }
                         }
@@ -88,15 +105,15 @@ fn validate_imports(module: &Module) -> Result<(), String> {
         if had_imported_memory_properly_named {
             Ok(())
         } else {
-            Err(String::from("No imported memory from env::memory."))
+            invalid_imports_error::<(), &str>("No imported memory from env::memory.")
         }
     })
 }
 
-fn remove_memory_export(module: &mut Module) -> Result<(), String> {
+fn remove_memory_export(module: &mut Module) -> Result<(), PreprocessingError> {
     let exports = module
         .export_section_mut()
-        .ok_or("Exports section must exist")?;
+        .ok_or(NoExportSection)?;
     let entries = exports.entries_mut();
     let mem_pos = entries.iter().position(|e| e.field() == "memory");
     if let Some(mem_pos) = mem_pos {
