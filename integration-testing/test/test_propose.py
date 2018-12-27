2fdb41a7fbab32e45f1eda95fe3dda6b5f6401ee
Igor Ramazanov <igor@casperlabs.io>
2018-12-27 16:32:42 +0300

    Updated integration tests

--- integration-testing/test/test_propose.py
+++ integration-testing/test/test_propose.py
@@ -3,8 +3,6 @@ import os
 import pytest
 
 from rnode_testing.rnode import started_standalone_bootstrap_node
-from rnode_testing.rnode import node_payment_code
-from rnode_testing.rnode import node_session_code
 from rnode_testing.wait import (
     wait_for_approved_block_received_handler_state,
     wait_for_blocks_count_at_least
@@ -15,6 +13,6 @@ from typing import TYPE_CHECKING
 
 def test_propose(started_standalone_bootstrap_node):
     wait_for_approved_block_received_handler_state(started_standalone_bootstrap_node, started_standalone_bootstrap_node.timeout)
-    started_standalone_bootstrap_node.deploy(node_session_code, node_payment_code)
+    started_standalone_bootstrap_node.deploy()
     started_standalone_bootstrap_node.propose()
     wait_for_blocks_count_at_least(started_standalone_bootstrap_node, 1, 1, started_standalone_bootstrap_node.timeout)
