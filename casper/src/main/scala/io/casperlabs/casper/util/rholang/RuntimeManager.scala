a24698068e0f7292cdbc88d9dec16a243d565c74
Abner Zheng <abnerzheng@gmail.com>
2019-01-09 00:39:22 +0800

    move computeState and replayComputeState to RuntimeManager and refactor

--- casper/src/main/scala/io/casperlabs/casper/util/rholang/RuntimeManager.scala
+++ casper/src/main/scala/io/casperlabs/casper/util/rholang/RuntimeManager.scala
@@ -1,6 +1,8 @@
 package io.casperlabs.casper.util.rholang
 
 import cats.effect.Concurrent
+import cats.syntax.applicative._
+import cats.syntax.functor._
 import com.google.protobuf.ByteString
 import io.casperlabs.casper.protocol._
 import io.casperlabs.casper.util.rholang.RuntimeManager.StateHash
@@ -8,7 +10,8 @@ import io.casperlabs.ipc.{Deploy => IPCDeploy}
 import io.casperlabs.catscontrib.ToAbstractContext
 import io.casperlabs.ipc.{CommutativeEffects, ExecutionEffect}
 import io.casperlabs.models._
-import io.casperlabs.smartcontracts.SmartContractsApi
+import cats.syntax.either._
+import io.casperlabs.smartcontracts.{ExecutionEngineService, SmartContractsApi}
 import monix.eval.Task
 import monix.execution.Scheduler
 
@@ -20,14 +23,27 @@ class RuntimeManager[F[_]: Concurrent: ToAbstractContext] private (
       hash: StateHash,
       terms: Seq[InternalProcessedDeploy],
       time: Option[Long] = None
-  ): Task[Either[(Option[Deploy], Failed), StateHash]] =
-    smartContractsApi.replayEval(terms, hash, time)
+  ): F[Either[(Option[Deploy], Failed), StateHash]] =
+    ByteString.EMPTY.asRight[(Option[Deploy], Failed)].pure
 
   def computeState(
       hash: StateHash,
       terms: Seq[(Deploy, ExecutionEffect)],
       time: Option[Long] = None
-  ): Task[(StateHash, Seq[InternalProcessedDeploy])] = smartContractsApi.newEval(terms, hash, time)
+  ): F[(StateHash, Seq[InternalProcessedDeploy])] = {
+    // todo using maximum commutative rules
+    val commutativeEffects = CommutativeEffects(terms.flatMap(_._2.transformMap))
+    ToAbstractContext[F]
+      .fromTask { smartContractsApi.executeEffects(commutativeEffects) }
+      .map {
+        case Right(_) =>
+          (hash, terms.map(it => InternalProcessedDeploy(it._1, 0, Succeeded)))
+        case Left(err) =>
+          throw new IllegalArgumentException(
+            s"Failed to execute effects: $err"
+          )
+      }
+  }
 
   // todo this should be complemented
   def computeBonds(hash: StateHash)(implicit scheduler: Scheduler): Seq[Bond] = Seq()
--- smart-contracts/src/main/scala/io/casperlabs/smartcontracts/SmartContractsApi.scala
+++ smart-contracts/src/main/scala/io/casperlabs/smartcontracts/SmartContractsApi.scala
@@ -7,8 +7,7 @@ import cats.syntax.either._
 import cats.syntax.functor._
 import com.google.protobuf.ByteString
 import io.casperlabs.casper.protocol.{Bond, Deploy}
-import io.casperlabs.ipc.{Deploy => EEDeploy}
-import io.casperlabs.ipc.{CommutativeEffects, ExecutionEffect}
+import io.casperlabs.ipc.{CommutativeEffects, Done, ExecutionEffect, Deploy => EEDeploy}
 import io.casperlabs.models.{Failed, InternalProcessedDeploy, Succeeded}
 import io.casperlabs.shared.StoreType
 import io.casperlabs.smartcontracts.ExecutionEngineService
@@ -17,19 +16,7 @@ import simulacrum.typeclass
 @typeclass trait SmartContractsApi[F[_]] {
   def sendDeploy(d: EEDeploy): F[Either[Throwable, ExecutionEffect]]
 
-  def newEval(
-      terms: Seq[(Deploy, ExecutionEffect)],
-      initHash: ByteString,
-      time: Option[Long] = None
-  ): F[(ByteString, Seq[InternalProcessedDeploy])]
-
-  def replayEval(
-      terms: Seq[InternalProcessedDeploy],
-      initHash: ByteString,
-      time: Option[Long] = None
-  ): F[Either[(Option[Deploy], Failed), ByteString]]
-
-  def computeBonds(hash: ByteString): F[Bond]
+  def executeEffects(c: CommutativeEffects): F[Either[Throwable, Done]]
 
   def close(): F[Unit]
 }
@@ -41,38 +28,12 @@ object SmartContractsApi {
       storeType: StoreType
   ): SmartContractsApi[F] =
     new SmartContractsApi[F] {
-      override def newEval(
-          terms: Seq[(Deploy, ExecutionEffect)],
-          initHash: ByteString,
-          time: Option[Long] = None
-      ): F[(ByteString, Seq[InternalProcessedDeploy])] = {
-        // todo using maximum commutative rules
-        val commutativeEffects = CommutativeEffects(terms.flatMap(_._2.transformMap))
-        for {
-          d <- ExecutionEngineService[F].executeEffects(commutativeEffects)
-        } yield
-          d match {
-            case Right(_) =>
-              (initHash, terms.map(it => InternalProcessedDeploy(it._1, 0, Succeeded)))
-            case Left(err) =>
-              throw new IllegalArgumentException(
-                s"Failed to execute effects: $err"
-              )
-          }
-      }
-
-      override def replayEval(
-          terms: Seq[InternalProcessedDeploy],
-          initHash: ByteString,
-          time: Option[Long] = None
-      ): F[Either[(Option[Deploy], Failed), ByteString]] =
-        ByteString.EMPTY.asRight[(Option[Deploy], Failed)].pure
 
       override def close(): F[Unit] =
         ().pure
 
-      override def computeBonds(hash: ByteString): F[Bond] =
-        Bond().pure
+      override def executeEffects(c: CommutativeEffects): F[Either[Throwable, Done]] =
+        ExecutionEngineService[F].executeEffects(c)
 
       override def sendDeploy(d: EEDeploy): F[Either[Throwable, ExecutionEffect]] =
         ExecutionEngineService[F].sendDeploy(d)
