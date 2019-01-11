22f863fa2a4184b9a13b0beb478953cc957091b1
birchmd <birchmd@casperlabs.io>
2019-01-11 14:22:10 -0500

    Fix warnings in execution-engine/comm; expand conversion from ipc transaform to transform to include all possible value enums; change hello world example to make a change to the global state (makes hello world get printed to the node logs)

--- execution-engine/comm/src/main.rs
+++ execution-engine/comm/src/main.rs
@@ -23,7 +23,7 @@ fn main() {
         std::fs::remove_file(socket_path).expect("Remove old socket file.");
     }
 
-    let mut engine_state = EngineState::new(storage::InMemGS::new());
+    let engine_state = EngineState::new(storage::InMemGS::new());
     engine_state.with_mocked_account([48u8; 20]);
     let server_builder = engine_server::new(socket, engine_state);
     let _server = server_builder.build().expect("Start server");
