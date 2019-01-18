fec8f23173ed923e72fa50d124b8cf19fb60f747
Igor Ramazanov <igor@casperlabs.io>
2019-01-18 18:13:19 +0300

    Added https://github.com/rchain/rchain/pull/2040

--- comm/src/main/scala/io/casperlabs/comm/transport/StreamHandler.scala
+++ comm/src/main/scala/io/casperlabs/comm/transport/StreamHandler.scala
@@ -1,28 +1,19 @@
 package io.casperlabs.comm.transport
 
 import java.io.FileOutputStream
-import java.nio.file.Path
-
-import java.util.UUID
-import io.casperlabs.shared.GracefulClose._
-import io.casperlabs.catscontrib.ski._
-import java.io.FileOutputStream
 import java.nio.file.{Files, Path}
-import io.casperlabs.shared._
-import Compression._
-import io.casperlabs.comm.{CommError, PeerNode}
-import monix.eval.Task
-import monix.execution.{Cancelable, Scheduler}
-import monix.reactive.Observable
-import monix.reactive.observers.Subscriber
-import monix.reactive.OverflowStrategy._
-import io.casperlabs.comm.rp.ProtocolHelper
-import io.casperlabs.comm.protocol.routing._
-import com.google.protobuf.ByteString
+import java.util.UUID
+
+import cats.data._
 import cats.implicits._
-import io.casperlabs.comm.transport.buffer
-import io.casperlabs.comm.transport.{Blob, ServerMessage, StreamMessage}
-import io.casperlabs.catscontrib.TaskContrib._
+import io.casperlabs.comm.PeerNode
+import io.casperlabs.comm.protocol.routing._
+import io.casperlabs.comm.rp.ProtocolHelper
+import io.casperlabs.shared.Compression._
+import io.casperlabs.shared.GracefulClose._
+import io.casperlabs.shared._
+import monix.eval.Task
+import monix.reactive.Observable
 
 object StreamHandler {
   private case class Streamed(
@@ -36,56 +27,54 @@ object StreamHandler {
 
   def handleStream(
       folder: Path,
-      observable: Observable[Chunk],
-      buff: buffer.LimitedBuffer[ServerMessage]
-  )(implicit logger: Log[Task]): Task[ChunkResponse] =
-    (init(folder).attempt >>= {
-      case Left(ex) => logger.error("could not create a file to store incoming stream", ex)
-      case Right(initStmd) =>
-        (collect(initStmd, observable).attempt >>= {
-          case Left(ex)    => logger.error("could not collect incoming streamed data", ex)
-          case Right(stmd) => push(stmd, buff)
-        }) *> gracefullyClose[Task](initStmd.fos).as(())
-    }).as(ChunkResponse())
+      stream: Observable[Chunk]
+  )(implicit logger: Log[Task]): Task[Either[Throwable, StreamMessage]] =
+    init(folder)
+      .bracket { initStmd =>
+        (collect(initStmd, stream) >>= toResult).value
+      }(stmd => gracefullyClose[Task](stmd.fos).as(()))
+      .attempt
+      .map(_.flatten)
 
   private def init(folder: Path): Task[Streamed] =
     for {
       _        <- Task.delay(folder.toFile.mkdirs())
       fileName <- Task.delay(UUID.randomUUID.toString + "_packet_streamed.bts")
-      file     = folder.resolve(fileName)
+      file     <- Task.delay(folder.resolve(fileName))
       fos      <- Task.delay(new FileOutputStream(file.toFile))
     } yield Streamed(fos = fos, path = file)
 
-  private def collect(init: Streamed, observable: Observable[Chunk]): Task[Streamed] =
-    observable.foldLeftL(init) {
-      case (stmd, Chunk(Chunk.Content.Header(ChunkHeader(sender, typeId, compressed, cl)))) =>
-        stmd.copy(
-          sender = sender.map(ProtocolHelper.toPeerNode(_)),
-          typeId = Some(typeId),
-          compressed = compressed,
-          contentLength = Some(cl)
-        )
-      case (stmd, Chunk(Chunk.Content.Data(ChunkData(newData)))) =>
-        stmd.fos.write(newData.toByteArray)
-        stmd.fos.flush()
-        stmd
-    }
+  private def collect(
+      init: Streamed,
+      stream: Observable[Chunk]
+  ): EitherT[Task, Throwable, Streamed] =
+    EitherT(
+      stream
+        .foldLeftL(init) {
+          case (stmd, Chunk(Chunk.Content.Header(ChunkHeader(sender, typeId, compressed, cl)))) =>
+            stmd.copy(
+              sender = sender.map(ProtocolHelper.toPeerNode),
+              typeId = Some(typeId),
+              compressed = compressed,
+              contentLength = Some(cl)
+            )
+          case (stmd, Chunk(Chunk.Content.Data(ChunkData(newData)))) =>
+            stmd.fos.write(newData.toByteArray)
+            stmd.fos.flush()
+            stmd
+        }
+        .attempt
+    )
 
-  private def push(stmd: Streamed, buff: buffer.LimitedBuffer[ServerMessage])(
-      implicit logger: Log[Task]
-  ): Task[Boolean] = stmd match {
-    case Streamed(Some(sender), Some(packetType), Some(contentLength), compressed, path, _) =>
-      Task.delay {
-        // TODO what if returns false?
-        buff.pushNext(StreamMessage(sender, packetType, path, compressed, contentLength))
+  private def toResult(stmd: Streamed): EitherT[Task, Throwable, StreamMessage] =
+    EitherT(Task.delay {
+      stmd match {
+        case Streamed(Some(sender), Some(packetType), Some(contentLength), compressed, path, _) =>
+          Right(StreamMessage(sender, packetType, path, compressed, contentLength))
+        case stmd =>
+          Left(new RuntimeException(s"received not full stream message, will not process. $stmd"))
       }
-    case stmd =>
-      logger
-        .warn(
-          s"received not full stream message, will not process. $stmd"
-        )
-        .as(false)
-  }
+    })
 
   def restore(msg: StreamMessage)(implicit logger: Log[Task]): Task[Either[Throwable, Blob]] =
     (fetchContent(msg.path).attempt >>= {
