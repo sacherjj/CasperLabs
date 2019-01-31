7ca1008e94b33aec9e0d8ffaa226758cd7e6d451
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-01-31 09:35:37 +0100

    Fix FindAndConnectSpec test.
    Problem was in the Connect#findAndConnect method.
    
    When `Sync` instance was used to handle errors the return type was different than when ApplicativeError_ was being used.
    It was difference between EitherT(Left(Timeout)) and EitherT(Right(Left(Timeout))). Former short circuits traverse while latter continues.

--- comm/src/test/scala/io/casperlabs/comm/rp/FindAndConnectSpec.scala
+++ comm/src/test/scala/io/casperlabs/comm/rp/FindAndConnectSpec.scala
@@ -1,26 +1,20 @@
 package io.casperlabs.comm.rp
 
-import Connect._
-import Connections._
-import io.casperlabs.comm._
-import CommError._
-import protocol.routing._
-import io.casperlabs.p2p.EffectsTestInstances._
-import io.casperlabs.metrics.Metrics
-
-import scala.concurrent.duration._
-import org.scalatest._
-import org.scalatest.enablers.Containing
+import cats.Id
 import cats.data._
 import cats.implicits._
 import io.casperlabs.catscontrib._
-import Catscontrib._
-import cats.Id
-import ski._
-import io.casperlabs.shared._
-import io.casperlabs.comm.transport._
-import io.casperlabs.comm.rp.ProtocolHelper._
 import io.casperlabs.catscontrib.effect.implicits._
+import io.casperlabs.comm.CommError._
+import io.casperlabs.comm._
+import io.casperlabs.comm.rp.Connect.Connections._
+import io.casperlabs.comm.rp.Connect._
+import io.casperlabs.metrics.Metrics
+import io.casperlabs.p2p.EffectsTestInstances._
+import io.casperlabs.shared._
+import org.scalatest._
+
+import scala.concurrent.duration._
 
 class FindAndConnectSpec extends FunSpec with Matchers with BeforeAndAfterEach with AppendedClues {
 
