58430a22993c0a910e734f76a3c4064ae7293f7e
Akosh Farkash <aakoshh@gmail.com>
2019-02-08 16:27:59 +0000

    CON-50: Fixed tabs in shell scripts.

--- docker/client.sh
+++ docker/client.sh
@@ -18,25 +18,25 @@ NODE=$1; shift
 CMD=$1; shift
 
 case "$CMD" in
-	"deploy")
-		# Need to mount the files.
-		VOL=$1; shift
-		docker run --rm \
-		    --network casperlabs \
-		    --volume $VOL:/data \
-		    io.casperlabs/client:latest \
-			--host $NODE --port 40401 deploy $@
-		;;
+    "deploy")
+        # Need to mount the files.
+        VOL=$1; shift
+        docker run --rm \
+            --network casperlabs \
+            --volume $VOL:/data \
+            io.casperlabs/client:latest \
+            --host $NODE --port 40401 deploy $@
+        ;;
 
-	"propose")
-		docker run --rm \
-		    --network casperlabs \
-		    io.casperlabs/client:latest \
-			--host $NODE --port 40401 propose
-		;;
+    "propose")
+        docker run --rm \
+            --network casperlabs \
+            io.casperlabs/client:latest \
+            --host $NODE --port 40401 propose
+        ;;
 
-	*)
-		echo "usage: ./client.sh <node-id> [deploy|propose] [OPTION...]"
-		exit 1
-		;;
+    *)
+        echo "usage: ./client.sh <node-id> [deploy|propose] [OPTION...]"
+        exit 1
+        ;;
 esac
