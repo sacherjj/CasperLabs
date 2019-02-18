0c2e304de9e194a9df582284b0a407a8ead1cf28
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-02-18 11:14:58 +0100

    Fix docker setup after renaming `node` to `casperlabs-node`.

--- docker/template/start-node.sh
+++ docker/template/start-node.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ "$HOSTNAME" = "$BOOTSTRAP_HOSTNAME" ]; then
     # Start the bootstrap node with a fixed key to get a deterministic ID.
-    exec ./bin/node run -s \
+    exec ./bin/casperlabs-node run -s \
         --server-host $HOSTNAME \
         --tls-certificate $HOME/.casperlabs/bootstrap/node.certificate.pem \
         --tls-key $HOME/.casperlabs/bootstrap/node.key.pem \
@@ -12,7 +12,7 @@ if [ "$HOSTNAME" = "$BOOTSTRAP_HOSTNAME" ]; then
         --casper-validator-public-key $CL_VALIDATOR_PUBLIC_KEY \
         --grpc-socket $CL_GRPC_SOCKET
 else
-    exec ./bin/node run \
+    exec ./bin/casperlabs-node run \
         --server-host $HOSTNAME \
         --server-bootstrap "casperlabs://${BOOTSTRAP_ID}@${BOOTSTRAP_HOSTNAME}?protocol=40400&discovery=40404" \
         --casper-validator-private-key $CL_VALIDATOR_PRIVATE_KEY \
