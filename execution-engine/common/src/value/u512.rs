c640abf627a059bdd62a6685618dbd9a8f3ee473
birchmd <birchmd@casperlabs.io>
2019-03-14 15:08:52 -0400

    Fix clippy warnings/errors

--- execution-engine/common/src/value/u512.rs
+++ execution-engine/common/src/value/u512.rs
@@ -1,19 +1,25 @@
 use crate::bytesrepr::{self, Error, FromBytes, ToBytes};
 use alloc::vec::Vec;
 
-construct_uint! {
-    pub struct U512(8);
+// Clippy generates a ton of warnings/errors for the code the macro generates.
+#[allow(clippy::all)]
+mod macro_code {
+    construct_uint! {
+        pub struct U512(8);
+    }
 }
+
+pub use self::macro_code::U512;
 
 impl ToBytes for U512 {
     fn to_bytes(&self) -> Vec<u8> {
         let mut buf = [0u8; 64];
         self.to_little_endian(&mut buf);
         let mut non_zero_bytes: Vec<u8> = buf
-            .into_iter()
+            .iter()
             .rev()
-            .map(|b| *b)
-            .skip_while(|b| *b == 0)
+            .skip_while(|b| **b == 0)
+            .cloned()
             .collect();
         let num_bytes = non_zero_bytes.len() as u8;
         non_zero_bytes.push(num_bytes);
