e8886e4a91fc67eb48d68f1d512d46993a1321f4
Akosh Farkash <aakoshh@gmail.com>
2019-03-15 13:52:34 +0000

    NODE-299: Use @typeclass on ObservableIterant.

--- comm/src/main/scala/io/casperlabs/comm/gossiping/ObservableIterant.scala
+++ comm/src/main/scala/io/casperlabs/comm/gossiping/ObservableIterant.scala
@@ -4,15 +4,16 @@ import cats.effect._
 import monix.tail.Iterant
 import monix.reactive.Observable
 import monix.execution.Scheduler
+import simulacrum.typeclass
 
 /** Convert an Observable to an Iterant and back in a resource safe way. */
+@typeclass
 trait ObservableIterant[F[_]] {
   def toObservable[A](it: Iterant[F, A]): Observable[A]
   def toIterant[A](obs: Observable[A]): Iterant[F, A]
 }
 
 object ObservableIterant {
-  def apply[F[_]](implicit ev: ObservableIterant[F]) = ev
 
   implicit def default[F[_]: Effect](implicit scheduler: Scheduler) =
     new ObservableIterant[F] {
