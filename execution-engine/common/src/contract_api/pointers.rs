d38e062578fa44cfda6e63a17a06459fa7790acb
birchmd <birchmd@casperlabs.io>
2019-03-15 09:09:41 -0400

    Derive additional traits for Pointers

--- execution-engine/common/src/contract_api/pointers.rs
+++ execution-engine/common/src/contract_api/pointers.rs
@@ -3,7 +3,7 @@ use crate::value::Contract;
 use core::marker::PhantomData;
 
 // URef with type information about what value is in the global state
-#[derive(Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Copy, Hash)]
 pub struct UPointer<T>([u8; 32], PhantomData<T>);
 
 impl<T> UPointer<T> {
@@ -12,7 +12,7 @@ impl<T> UPointer<T> {
     }
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ContractPointer {
     Hash([u8; 32]),
     URef(UPointer<Contract>),
