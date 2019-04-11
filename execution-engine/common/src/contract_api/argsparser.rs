84743dfdd9a38b3b9535952bb7bd71553e1b9e28
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-04-11 21:34:26 +0200

    EE-64: Add support for parsing tuple args up to 16 elements.

--- execution-engine/common/src/contract_api/argsparser.rs
+++ execution-engine/common/src/contract_api/argsparser.rs
@@ -41,3 +41,10 @@ impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 }
 impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 }
 impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 T8 }
 impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 T8 T9 }
+impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 T8 T10 }
+impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 T8 T10 T11 }
+impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 T8 T10 T11 T12 }
+impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 T8 T10 T11 T12 T13 }
+impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 T8 T10 T11 T12 T13 T14 }
+impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 T8 T10 T11 T12 T13 T14 T15 }
+impl_argsparser_tuple! { T1 T2 T3 T4 T5 T6 T7 T8 T10 T11 T12 T13 T14 T15 T16 }
