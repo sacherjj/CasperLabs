4af888a875361c8a00766acce451a0b8fbe474be
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-04-11 15:55:10 +0200

    EE-64: Mention that byte representation has to be ABI compliant.

--- execution-engine/common/src/contract_api/argsparser.rs
+++ execution-engine/common/src/contract_api/argsparser.rs
@@ -2,7 +2,8 @@ use crate::bytesrepr;
 use alloc::vec::Vec;
 use bytesrepr::{Error, ToBytes};
 
-/// Parses `Self` into a byte representation.
+/// Parses `Self` into a byte representation that is ABI compliant.
+/// It means that each type of the tuple have to implement `ToBytes`.
 /// Implemented for tuples of various sizes.
 pub trait ArgsParser {
     /// `parse` returns `Vec<Vec<u8>>` because we want to be able to
