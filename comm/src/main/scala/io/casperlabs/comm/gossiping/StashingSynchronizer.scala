5cdeee5661a8ce20289daf754782869325878ab9
Igor Ramazanov <igor@casperlabs.io>
2019-04-22 11:50:59 +0300

    NODE-396: Address code review comments

--- comm/src/main/scala/io/casperlabs/comm/gossiping/StashingSynchronizer.scala
+++ comm/src/main/scala/io/casperlabs/comm/gossiping/StashingSynchronizer.scala
@@ -1,75 +1,49 @@
 package io.casperlabs.comm.gossiping
-import cats.effect.{Concurrent, Sync}
+import cats.effect.concurrent.{Deferred, Ref}
 import cats.effect.implicits._
-import cats.effect.concurrent.{Deferred, Ref, Semaphore}
+import cats.effect.{Concurrent, Sync}
 import cats.implicits._
 import cats.temp.par._
 import com.google.protobuf.ByteString
 import io.casperlabs.casper.consensus.BlockSummary
 import io.casperlabs.comm.discovery.Node
-import io.casperlabs.comm.gossiping.StashingSynchronizer.Request
-import io.casperlabs.shared.Cell
 
 class StashingSynchronizer[F[_]: Concurrent: Par](
     underlying: Synchronizer[F],
-    stashedRequestsRef: Ref[F, Map[Node, List[Request[F]]]],
-    // Prevents concurrent scheduling during processing of kept requests in 'run'
-    semaphoresCell: Cell[F, Map[Node, Semaphore[F]]],
+    stashedRequestsRef: Ref[F, Map[Node, (Deferred[F, Vector[BlockSummary]], Set[ByteString])]],
     transitionedRef: Ref[F, Boolean]
 ) extends Synchronizer[F] {
 
   override def syncDag(source: Node, targetBlockHashes: Set[ByteString]): F[Vector[BlockSummary]] =
     for {
-      _ <- semaphoresCell.flatModify { semaphores =>
-            if (semaphores.contains(source)) {
-              semaphores.pure[F]
-            } else {
-              Semaphore[F](1).map { semaphore =>
-                semaphores.updated(source, semaphore)
-              }
-            }
-          }
-      semaphores   <- semaphoresCell.read
       transitioned <- transitionedRef.get
       dag <- if (transitioned) {
-              semaphores(source).withPermit(underlying.syncDag(source, targetBlockHashes))
+              underlying.syncDag(source, targetBlockHashes)
             } else {
               for {
-                deferred <- Deferred[F, Vector[BlockSummary]]
-                _ <- stashedRequestsRef.update { stashedRequests =>
-                      val previous = stashedRequests.getOrElse(source, List.empty)
-                      val merged   = Request(targetBlockHashes, deferred) :: previous
-                      stashedRequests.updated(source, merged)
-                    }
+                maybeInitialDeferred <- Deferred[F, Vector[BlockSummary]]
+                deferred <- stashedRequestsRef.modify { stashedRequests =>
+                             val (d, hashes) =
+                               stashedRequests.getOrElse(source, (maybeInitialDeferred, Set.empty))
+                             (stashedRequests.updated(source, (d, hashes ++ targetBlockHashes)), d)
+                           }
                 res <- deferred.get
               } yield res
             }
     } yield dag
 
-  def run: F[Unit] =
+  private def run: F[Unit] =
     for {
       _               <- transitionedRef.set(true)
       stashedRequests <- stashedRequestsRef.get
       _ <- stashedRequests.toList.parTraverse {
-            case (source, requests) =>
-              requests.reverse.traverse {
-                case Request(hashes, deferred) =>
-                  for {
-                    semaphores <- semaphoresCell.read
-                    _ <- semaphores(source).withPermit(
-                          underlying.syncDag(source, hashes) >>= deferred.complete
-                        )
-                  } yield ()
-              }.void
-          }.void
+            case (source, (deferred, hashes)) =>
+              underlying.syncDag(source, hashes) >>= deferred.complete
+          }
     } yield ()
 }
 
 object StashingSynchronizer {
-  final case class Request[F[_]](
-      targetBlockHashes: Set[ByteString],
-      deferred: Deferred[F, Vector[BlockSummary]]
-  )
 
   def wrap[F[_]: Concurrent: Par](
       underlying: Synchronizer[F],
@@ -77,14 +51,15 @@ object StashingSynchronizer {
   ): F[Synchronizer[F]] =
     for {
       stashedRequestsRef <- Ref
-                             .of[F, Map[Node, List[Request[F]]]](Map.empty)
-      semaphoresCell  <- Cell.mvarCell[F, Map[Node, Semaphore[F]]](Map.empty)
+                             .of[F, Map[
+                               Node,
+                               (Deferred[F, Vector[BlockSummary]], Set[ByteString])
+                             ]](Map.empty)
       transitionedRef <- Ref.of[F, Boolean](false)
       s <- Sync[F].delay(
             new StashingSynchronizer[F](
               underlying,
               stashedRequestsRef,
-              semaphoresCell,
               transitionedRef
             )
           )
