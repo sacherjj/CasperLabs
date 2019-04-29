75bc90e104940839cb2f19b9f59a21ff3ec26c99
Henry Till <henrytill@gmail.com>
2019-04-29 17:27:28 -0400

    NO-TICKET: execution-engine: add braces to PointerBlock's Debug impl

--- execution-engine/storage/src/history/trie/mod.rs
+++ execution-engine/storage/src/history/trie/mod.rs
@@ -130,12 +130,12 @@ impl ::std::fmt::Debug for PointerBlock {
     #[allow(clippy::assertions_on_constants)]
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
         assert!(RADIX > 1, "RADIX must be > 1");
-        write!(f, "{}(", stringify!(PointerBlock))?;
+        write!(f, "{}([", stringify!(PointerBlock))?;
         write!(f, "{:?}", self.0[0])?;
         for item in self.0[1..].iter() {
             write!(f, ", {:?}", item)?;
         }
-        write!(f, ")")
+        write!(f, "])")
     }
 }
 
