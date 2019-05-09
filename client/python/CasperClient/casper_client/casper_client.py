dbaf973ad772c55c3d01f934bc97edecf3f650ce
Joe Sacher <joe@casperlabs.io>
2019-05-08 21:29:57 -0400

    Integrating Piotr's script updates and modifying files to be compatible.

--- client/python/CasperClient/casper_client/casper_client.py
+++ client/python/CasperClient/casper_client/casper_client.py
@@ -8,8 +8,8 @@ import argparse
 import grpc
 import functools
 
-from .protos import CasperMessage_pb2
-from .protos.CasperMessage_pb2_grpc import DeployServiceStub
+from .proto import CasperMessage_pb2
+from .proto.CasperMessage_pb2_grpc import DeployServiceStub
 
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 40401
