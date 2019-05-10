2588cd789487574aa24f49358137831e681fcecf
Piotr Kuchta <piotr@casperlabs.io>
2019-05-10 15:30:13 -0700

    Update integration-testing/test/cl_node/casperlabsnode.py
    Ha! This is what I usually do, I guess I wanted to be extra careful this time, as the two expressions are not semantically equivalent in Python, due to its funny definition of falseness. But, we don't expect session to be `0` or `'''`.
    
    Co-Authored-By: Akosh Farkash <aakoshh@gmail.com>

--- integration-testing/test/cl_node/casperlabsnode.py
+++ integration-testing/test/cl_node/casperlabsnode.py
@@ -212,7 +212,7 @@ class Node:
                session: str = None, payment: str = None,
                gas_limit: int = 1000000, gas_price: int = 1, nonce: int = 0) -> str:
 
-        session = session if session is not None else CONTRACT_NAME
+        session = session or CONTRACT_NAME
         payment = payment if payment is not None else CONTRACT_NAME
 
         command = " ".join([
