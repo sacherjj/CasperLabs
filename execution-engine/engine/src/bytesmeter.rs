8d5f2f1142033fb13af58ef3583956d1780d6617
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-05-14 13:00:03 +0200

    EE-260: Split `ByteSize` into `HeapSizeOf` and `ByteSize`.
    To accurately measure size of a type we have to measure both
    heap and stack size it occupies. Previously both were (or weren't) measured
    inside implementations of `ByteSize`. In order to be consistent, I have
    split it into heap and stack measuring traits.

--- execution-engine/engine/src/bytesmeter.rs
+++ execution-engine/engine/src/bytesmeter.rs
@@ -1,67 +1,108 @@
 use common::bytesrepr::I32_SIZE;
 use common::key::Key;
-use common::value::{Account, Value};
+use common::value::{Account, Contract, Value};
 use std::collections::btree_map::BTreeMap;
 
-/// Returns byte size of the element.
+/// Returns byte size of the element - both heap size and stack size.
 pub trait ByteSize {
     fn byte_size(&self) -> usize;
 }
 
 impl ByteSize for Key {
     fn byte_size(&self) -> usize {
-        std::mem::size_of::<Key>()
+        std::mem::size_of::<Self>() + self.heap_size()
+    }
+}
+
+impl ByteSize for Account {
+    fn byte_size(&self) -> usize {
+        std::mem::size_of::<Self>() + self.heap_size() + self.urefs_lookup().byte_size()
+    }
+}
+
+impl ByteSize for Contract {
+    fn byte_size(&self) -> usize {
+        std::mem::size_of::<Self>()
+            + self.heap_size()
+            + self.urefs_lookup().byte_size()
+            + self.bytes().len()
+    }
+}
+
+impl ByteSize for String {
+    fn byte_size(&self) -> usize {
+        std::mem::size_of::<Self>() + self.heap_size()
+    }
+}
+
+impl<K: HeapSizeOf, V: HeapSizeOf> ByteSize for BTreeMap<K, V> {
+    fn byte_size(&self) -> usize {
+        std::mem::size_of::<BTreeMap<K, V>>()
+            + self.heap_size()
+            + self.len() * (std::mem::size_of::<K>() + std::mem::size_of::<V>())
     }
 }
 
 impl ByteSize for Value {
     fn byte_size(&self) -> usize {
-        match self {
-            Value::Int32(_) | Value::UInt128(_) | Value::UInt256(_) | Value::UInt512(_) => 0,
-            Value::ByteArray(vec) => vec.capacity(),
-            Value::ListInt32(list) => list.capacity() * I32_SIZE,
-            Value::String(s) => s.byte_size(),
-            Value::ListString(list) => list.iter().fold(0, |sum, el| {
-                sum + std::mem::size_of::<String>() + el.byte_size()
-            }),
-            // NOTE: We don't measure `key` as its size will be returned with `std::mem::size_of::<Value>()` call
-            Value::NamedKey(name, _key) => name.byte_size(),
-            Value::Account(account) => {
-                std::mem::size_of::<Account>() + account.urefs_lookup().byte_size()
+        std::mem::size_of::<Self>()
+            + match self {
+                Value::Int32(_) | Value::UInt128(_) | Value::UInt256(_) | Value::UInt512(_) => 0,
+                Value::ByteArray(vec) => std::mem::size_of::<Vec<u8>>() + vec.capacity(),
+                Value::ListInt32(list) => {
+                    std::mem::size_of::<Vec<i32>>() + list.capacity() * I32_SIZE
+                }
+                Value::String(s) => s.byte_size(),
+                Value::ListString(list) => list.iter().fold(0, |sum, el| sum + el.byte_size()),
+                // NOTE: We don't measure `key` as its size will be returned with `std::mem::size_of::<Value>()` call
+                Value::NamedKey(name, _key) => name.byte_size(),
+                Value::Account(account) => account.byte_size(),
+                Value::Contract(contract) => contract.byte_size(),
             }
-            Value::Contract(contract) => {
-                std::mem::size_of::<Account>()
-                    + contract.bytes().len()
-                    + contract.urefs_lookup().byte_size()
-            }
-        }
+    }
+}
+
+/// Returns heap size of the value.
+/// Note it's different from [ByteSize] that returns both heap and stack size.
+pub trait HeapSizeOf {
+    fn heap_size(&self) -> usize;
+}
+
+impl HeapSizeOf for Key {
+    fn heap_size(&self) -> usize {
+        0
+    }
+}
+
+impl HeapSizeOf for Account {
+    fn heap_size(&self) -> usize {
+        self.urefs_lookup().heap_size()
+    }
+}
+
+impl HeapSizeOf for Contract {
+    fn heap_size(&self) -> usize {
+        self.urefs_lookup().heap_size() + self.bytes().len()
     }
 }
 
 // NOTE: We're ignoring size of the tree's nodes.
-impl<K: ByteSize, V: ByteSize> ByteSize for BTreeMap<K, V> {
-    fn byte_size(&self) -> usize {
-        self.iter().fold(0, |sum, (k, v)| {
-            sum + std::mem::size_of::<K>()
-                + k.byte_size()
-                + std::mem::size_of::<V>()
-                + v.byte_size()
-        })
+impl<K: HeapSizeOf, V: HeapSizeOf> HeapSizeOf for BTreeMap<K, V> {
+    fn heap_size(&self) -> usize {
+        self.iter()
+            .fold(0, |sum, (k, v)| sum + k.heap_size() + v.heap_size())
     }
 }
 
-impl<T: ByteSize> ByteSize for [T] {
+impl<T: HeapSizeOf> ByteSize for [T] {
     fn byte_size(&self) -> usize {
-        self.iter().fold(0, |sum, el| sum + el.byte_size())
+        self.iter()
+            .fold(0, |sum, el| sum + std::mem::size_of::<T>() + el.heap_size())
     }
 }
 
-impl ByteSize for String {
-    // size_of_val(some_string) always returns 24 bytes,
-    // It's the same as the size of empty vector because String is encoded as `Vec<u8>`.
-    // but it's a private field so we can't extract it. The closest thing we can get to know
-    // String's real size is capacity of the vector it uses to store characters.
-    fn byte_size(&self) -> usize {
+impl HeapSizeOf for String {
+    fn heap_size(&self) -> usize {
         self.capacity()
     }
 }
@@ -69,42 +110,29 @@ impl ByteSize for String {
 #[cfg(test)]
 mod tests {
     use bytesmeter::ByteSize;
-    use common::bytesrepr::{I32_SIZE, U64_SIZE, U8_SIZE};
+    use common::key::Key;
     use std::collections::btree_map::BTreeMap;
 
-    fn assert_size<T: ByteSize>(el: T, expected: usize) {
+    fn assert_byte_size<T: ByteSize>(el: T, expected: usize) {
         assert_eq!(el.byte_size(), expected)
     }
 
     #[test]
-    fn size_of_primitives() {
-        assert_size(10u8, U8_SIZE);
-        assert_size(1i32, I32_SIZE);
-        assert_size(1u64, U64_SIZE);
+    fn byte_size_of_string() {
+        assert_byte_size("Hello".to_owned(), 5 + std::mem::size_of::<String>())
     }
 
     #[test]
-    fn size_of_string() {
-        assert_size("Hello".to_owned(), 5)
-    }
-
-    #[test]
-    fn size_of_slices() {
-        let v = vec![1u32, 2, 3, 4];
-        let v_ref: &[u32] = &v;
-        assert_eq!(v_ref.byte_size(), 16);
-    }
-
-    #[test]
-    fn size_of_btree() {
+    fn byte_size_of_map() {
         let v = vec![
-            (1u8, "A".to_string()),
-            (2, "B".to_string()),
-            (3, "C".to_string()),
-            (4, "D".to_string()),
+            (Key::Hash([1u8; 32]), "A".to_string()),
+            (Key::Hash([2u8; 32]), "B".to_string()),
+            (Key::Hash([3u8; 32]), "C".to_string()),
+            (Key::Hash([4u8; 32]), "D".to_string()),
         ];
-        let it_size: usize = 4 * 2; // 4 pairs, each 1-byte key and 1-byte value
-        let map: BTreeMap<u8, String> = v.into_iter().collect();
-        assert_size(map, it_size);
+        let it_size: usize = std::mem::size_of::<BTreeMap<Key, String>>()
+            + 4 * (std::mem::size_of::<Key>() + std::mem::size_of::<String>() + 1);
+        let map: BTreeMap<Key, String> = v.into_iter().collect();
+        assert_byte_size(map, it_size);
     }
 }
