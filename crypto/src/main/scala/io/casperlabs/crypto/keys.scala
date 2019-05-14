93b64a04a4cb0a91f5cc8d2ca210fe750ca4ed21
Igor Ramazanov <igor@casperlabs.io>
2019-05-15 01:44:06 +0300

    NODE-448: Updated keys encoding to Shapeless tags. Add parsing of keys.

--- crypto/src/main/scala/io/casperlabs/crypto/keys.scala
+++ crypto/src/main/scala/io/casperlabs/crypto/keys.scala
@@ -1,5 +1,108 @@
 package io.casperlabs.crypto
+import cats.syntax.either._
+import com.google.protobuf.ByteString
+import io.casperlabs.crypto.signatures.SignatureAlgorithm
+import shapeless.tag.@@
 
-final case class PublicKey(bytes: Array[Byte])
+object Keys {
+  sealed trait PublicKeyTag
 
-final case class PrivateKey(bytes: Array[Byte])
+  /**
+    * Encodes Array[Byte]
+    */
+  type PublicKeyA = Array[Byte] @@ PublicKeyTag
+
+  /**
+    * Encodes ByteString
+    */
+  type PublicKeyBS = ByteString @@ PublicKeyTag
+
+  def PublicKey(a: Array[Byte]): PublicKeyA = a.asInstanceOf[PublicKeyA]
+
+  def PublicKey(a: ByteString): PublicKeyBS = a.asInstanceOf[PublicKeyBS]
+
+  implicit def convertTypeclasses[F[_]](implicit Typeclass: F[Array[Byte]]): F[PublicKeyA] =
+    Typeclass.asInstanceOf[F[PublicKeyA]]
+
+  sealed trait PrivateKeyTag
+  type PrivateKey = Array[Byte] @@ PrivateKeyTag
+
+  def PrivateKey(a: Array[Byte]): PrivateKey = a.asInstanceOf[PrivateKey]
+
+  sealed trait SignatureTag
+  type Signature = Array[Byte] @@ SignatureTag
+
+  def Signature(a: Array[Byte]): Signature = a.asInstanceOf[Signature]
+
+  sealed trait ParseError extends Product with Serializable { self =>
+    def errorMessage: String
+    def asParseError: ParseError = self
+  }
+  object ParseError {
+    case object PublicKeysDoNotMatch extends ParseError {
+      override def errorMessage: String =
+        "Given public key not compatible with derived public key from private key."
+    }
+    case object KeysDoNotMatch extends ParseError {
+      override def errorMessage: String = "Private and public keys do not match each other."
+    }
+    case object EmptyPrivateKey extends ParseError {
+      override def errorMessage: String = "Private key must be specified."
+    }
+    case object EmptyPublicKey extends ParseError {
+      override def errorMessage: String =
+        "Public must be specified or private key is invalid, failed to infer public key from private key."
+    }
+    case object UnknownSignatureAlgorithm extends ParseError {
+      override def errorMessage: String =
+        "Unknown signature algorithm. Supported values: ed25519, secp256k1"
+    }
+  }
+
+  type ParseResult = (PrivateKey, PublicKeyA, SignatureAlgorithm)
+
+  def parse(
+      signatureAlgorithm: String,
+      privateKeyRaw: String,
+      maybePublicKeyRaw: Option[String]
+  ): Either[ParseError, ParseResult] =
+    signatureAlgorithm match {
+      case SignatureAlgorithm(sa) => parse(sa, privateKeyRaw, maybePublicKeyRaw)
+      case _                      => ParseError.UnknownSignatureAlgorithm.asLeft[ParseResult]
+    }
+
+  private def parse(
+      signatureAlgorithm: SignatureAlgorithm,
+      privateKeyRaw: String,
+      maybePublicKeyRaw: Option[String]
+  ): Either[ParseError, ParseResult] = {
+    val maybePrivateKey        = signatureAlgorithm.tryParsePrivateKey(privateKeyRaw)
+    val maybeGivenPublicKey    = maybePublicKeyRaw.flatMap(signatureAlgorithm.tryParsePublicKey)
+    val maybeInferredPublicKey = maybePrivateKey.flatMap(signatureAlgorithm.tryToPublic)
+
+    maybePrivateKey.fold(ParseError.EmptyPrivateKey.asParseError.asLeft[ParseResult]) {
+      privateKey =>
+        (maybeGivenPublicKey, maybeInferredPublicKey) match {
+
+          case (Some(pub1), Some(pub2)) if pub1 sameElements pub2 =>
+            (privateKey, pub1, signatureAlgorithm).asRight[ParseError]
+
+          case (Some(_), Some(_)) =>
+            ParseError.PublicKeysDoNotMatch.asParseError.asLeft[ParseResult]
+
+          case (Some(publicKey), None)
+              if signatureAlgorithm.areMatchTogether(publicKey, privateKey) =>
+            (privateKey, publicKey, signatureAlgorithm).asRight[ParseError]
+
+          case (Some(_), None) =>
+            ParseError.KeysDoNotMatch.asParseError.asLeft[ParseResult]
+
+          case (None, Some(publicKey)) =>
+            (privateKey, publicKey, signatureAlgorithm).asRight[ParseError]
+
+          case (None, None) =>
+            ParseError.EmptyPublicKey.asParseError.asLeft[ParseResult]
+        }
+    }
+  }
+}
