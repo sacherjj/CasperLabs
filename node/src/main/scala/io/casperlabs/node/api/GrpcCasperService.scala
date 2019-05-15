4f02f2949a306a91c0559ec23ba58c9d1bd26b95
Akosh Farkash <aakoshh@gmail.com>
2019-05-16 00:28:18 +0100

    NODE-473: Use google.protobuf.Empty in CasperService.

--- node/src/main/scala/io/casperlabs/node/api/GrpcCasperService.scala
+++ node/src/main/scala/io/casperlabs/node/api/GrpcCasperService.scala
@@ -3,6 +3,7 @@ package io.casperlabs.node.api
 import cats.implicits._
 import cats.effect._
 import cats.effect.concurrent._
+import com.google.protobuf.empty.Empty
 import io.casperlabs.casper.MultiParentCasperRef.MultiParentCasperRef
 import io.casperlabs.casper.api.BlockAPI
 import io.casperlabs.metrics.Metrics
@@ -17,9 +18,9 @@ object GrpcCasperService {
   ): F[CasperGrpcMonix.CasperService] =
     BlockAPI.establishMetrics[F] *> Sync[F].delay {
       new CasperGrpcMonix.CasperService {
-        override def deploy(request: DeployRequest): Task[DeployResponse] =
+        override def deploy(request: DeployRequest): Task[Empty] =
           TaskLike[F].toTask {
-            BlockAPI.deploy[F](request.getDeploy).map(_ => DeployResponse())
+            BlockAPI.deploy[F](request.getDeploy).map(_ => Empty())
           }
       }
     }
