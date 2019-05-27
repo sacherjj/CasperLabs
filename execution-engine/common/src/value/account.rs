57b2bb3ef14bd97d611e451fcec07da5a2648057
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-05-27 10:45:42 +0200

    EE-273: Add structs supporting associated keys.

--- execution-engine/common/src/value/account.rs
+++ execution-engine/common/src/value/account.rs
@@ -5,6 +5,83 @@ use alloc::string::String;
 use alloc::vec::Vec;
 
 pub const KEY_SIZE: usize = 32;
+/// Maximum number of associated keys.
+/// Value chosen arbitrary, shouldn't be too large to prevent bloating
+/// `associated_keys` table.
+pub const MAX_KEYS: usize = 10;
+
+#[derive(PartialOrd, Ord, PartialEq, Eq, Clone, Debug)]
+pub struct Weight(u8);
+
+impl Weight {
+    pub fn new(weight: u8) -> Weight {
+        Weight(weight)
+    }
+}
+
+#[derive(PartialOrd, Ord, PartialEq, Eq, Clone, Debug)]
+pub struct PublicKey([u8; KEY_SIZE]);
+
+impl PublicKey {
+    pub fn new(key: [u8; KEY_SIZE]) -> PublicKey {
+        PublicKey(key)
+    }
+}
+
+impl From<[u8; KEY_SIZE]> for PublicKey {
+    fn from(key: [u8; KEY_SIZE]) -> Self {
+        PublicKey(key)
+    }
+}
+
+// Represents a key associated with some account and its weight.
+pub struct AssociatedKey {
+    key: PublicKey,
+    weight: Weight,
+}
+
+impl AssociatedKey {
+    pub fn new(key: PublicKey, weight: Weight) -> AssociatedKey {
+        AssociatedKey { key, weight }
+    }
+}
+
+#[derive(PartialOrd, Ord, PartialEq, Eq, Clone, Debug)]
+pub struct AssociatedKeys(BTreeMap<PublicKey, Weight>);
+
+impl AssociatedKeys {
+    pub fn empty() -> AssociatedKeys {
+        AssociatedKeys(BTreeMap::new())
+    }
+    pub fn new(key: PublicKey, weight: Weight) -> AssociatedKeys {
+        let mut bt: BTreeMap<PublicKey, Weight> = BTreeMap::new();
+        bt.insert(key, weight);
+        AssociatedKeys(bt)
+    }
+    /// Adds new AssociatedKey to the set.
+    /// Returns true if added successfully, false otherwise.
+    pub fn add_key(&mut self, key: PublicKey, weight: Weight) -> bool {
+        if self.0.len() == MAX_KEYS || self.0.contains_key(&key) {
+            false
+        } else {
+            self.0.insert(key, weight);
+            true
+        }
+    }
+
+    /// Removes key from the associated keys set.
+    /// Returns true if value was found in the set prior to the removal, false otherwise.
+    pub fn remove_key(&mut self, key: &PublicKey) -> bool {
+        match self.0.remove(key) {
+            Some(_) => true,
+            None => false,
+        }
+    }
+
+    pub fn get(&self, key: &PublicKey) -> Option<&Weight> {
+        self.0.get(key)
+    }
+}
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct Account {
@@ -70,5 +147,52 @@ impl FromBytes for Account {
             },
             rem3,
         ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::value::account::{AssociatedKeys, PublicKey, Weight, KEY_SIZE, MAX_KEYS};
+
+    #[test]
+    fn associated_keys_add() {
+        let mut keys = AssociatedKeys::new([0u8; KEY_SIZE].into(), Weight::new(1));
+        let new_pk = PublicKey([1u8; KEY_SIZE]);
+        let new_pk_weight = Weight::new(2);
+        assert!(keys.add_key(new_pk.clone(), new_pk_weight.clone()));
+        assert_eq!(keys.get(&new_pk), Some(&new_pk_weight))
+    }
+
+    #[test]
+    fn associated_keys_add_full() {
+        let map = (0..MAX_KEYS)
+            .map(|k| (PublicKey([k as u8; KEY_SIZE]), Weight::new(k as u8)));
+        assert_eq!(map.len(), 10);
+        let mut keys = {
+            let mut tmp = AssociatedKeys::empty();
+            map.for_each(|(key, weight)| {
+                assert!(tmp.add_key(key, weight))
+            });
+            tmp
+        };
+        assert!(!keys.add_key(PublicKey([100u8; KEY_SIZE]), Weight::new(100)))
+    }
+
+    #[test]
+    fn associated_keys_add_duplicate() {
+        let pk = PublicKey([0u8; KEY_SIZE]);
+        let weight = Weight::new(1);
+        let mut keys = AssociatedKeys::new(pk.clone(), weight.clone());
+        assert!(!keys.add_key(pk.clone(), Weight::new(10)));
+        assert_eq!(keys.get(&pk), Some(&weight));
+    }
+
+    #[test]
+    fn associated_keys_remove() {
+        let pk = PublicKey([0u8; KEY_SIZE]);
+        let weight = Weight::new(1);
+        let mut keys = AssociatedKeys::new(pk.clone(), weight.clone());
+        assert!(keys.remove_key(&pk));
+        assert!(!keys.remove_key(&PublicKey([1u8; KEY_SIZE])));
     }
 }
--- execution-engine/shared/src/init/mod.rs
+++ execution-engine/shared/src/init/mod.rs
@@ -1,9 +1,15 @@
 use std::collections::btree_map::BTreeMap;
 
 use common::key::Key;
+use common::value::account::{AssociatedKeys, PublicKey, Weight};
 use common::value::{Account, Value};
 
 pub fn mocked_account(account_addr: [u8; 32]) -> Vec<(Key, Value)> {
-    let account = Account::new(account_addr, 0, BTreeMap::new());
+    let associated_keys = {
+        let mut associated_keys = AssociatedKeys::empty();
+        associated_keys.add_key(PublicKey::new(account_addr), Weight::new(1));
+        associated_keys
+    };
+    let account = Account::new(account_addr, 0, BTreeMap::new(), associated_keys);
     vec![(Key::Account(account_addr), Value::Account(account))]
 }
