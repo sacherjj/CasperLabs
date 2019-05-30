3db098878770b4d5ec83b30f34d15f9cc1f8c019
birchmd <birchmd@casperlabs.io>
2019-05-30 07:27:08 -0400

    CON-204: Mint-internal representation of purse ids

---
+++ execution-engine/blessed-contracts/mint-token/src/internal_purse_id.rs
@@ -0,0 +1,43 @@
+use cl_std::contract_api;
+use cl_std::key::Key;
+use core::convert::TryFrom;
+
+pub struct WithdrawId([u8; 32]);
+pub struct DepositId([u8; 32]);
+
+impl WithdrawId {
+    pub fn raw_id(&self) -> [u8; 32] {
+        self.0
+    }
+}
+impl DepositId {
+    pub fn raw_id(&self) -> [u8; 32] {
+        self.0
+    }
+}
+
+impl TryFrom<Key> for WithdrawId {
+    type Error = ();
+
+    fn try_from(k: Key) -> Result<WithdrawId, ()> {
+        if contract_api::is_valid(k) {
+            match k {
+                Key::URef(id, _) => Ok(WithdrawId(id)),
+                _ => Err(()),
+            }
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl TryFrom<Key> for DepositId {
+    type Error = ();
+
+    fn try_from(k: Key) -> Result<DepositId, ()> {
+        match k {
+            Key::URef(id, _) => Ok(DepositId(id)),
+            _ => Err(()),
+        }
+    }
+}
--- execution-engine/blessed-contracts/mint-token/src/lib.rs
+++ execution-engine/blessed-contracts/mint-token/src/lib.rs
@@ -5,6 +5,13 @@ extern crate alloc;
 extern crate cl_std;
 
 mod capabilities;
+
+// These types are purposely defined in a separate module
+// so that their constructors are hidden and therefore
+// we must use the conversion methods from Key elsewhere
+// in the code.
+mod internal_purse_id;
+
 mod mint;
 
 use alloc::collections::BTreeMap;
