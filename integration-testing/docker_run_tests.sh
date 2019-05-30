394a820b7adcd71990fd07776f7d7310165214e6
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2019-05-30 12:48:38 -0400

    Adding error exits.

--- integration-testing/docker_run_tests.sh
+++ integration-testing/docker_run_tests.sh
@@ -1,17 +1,34 @@
 #!/usr/bin/env bash
 
-
 if [[ -n $DRONE_BUILD_NUMBER ]]; then
     export TAG_NAME=DRONE-${DRONE_BUILD_NUMBER}
 else
     export TAG_NAME="test"
 fi
 
-
 # We need networks for the Python Client to talk directly to the DockerNode.
 # We cannot share a network as we might have DockerNodes partitioned.
 # This number of networks is the count of CasperLabNodes we can have active at one time.
 MAX_NODE_COUNT=10
+
+cleanup() {
+    # Eliminate this for next run
+    rm docker-compose.yml
+
+    for num in $(seq 0 $MAX_NODE_COUNT)
+    do
+        docker network rm cl-${TAG_NAME}-${num}
+    done
+}
+trap cleanup 0
+
+error_exit()
+{
+    if [[ "$?" != "0" ]]; then
+        log.sh "$1"
+        exit 1
+    fi
+}
 
 for num in $(seq 0 $MAX_NODE_COUNT)
 do
@@ -21,13 +38,7 @@ done
 # Need to make network names in docker-compose.yml match tag based network.
 # Using ||TAG|| as replacable element in docker-compose.yml.template
 sed 's/||TAG||/'"${TAG_NAME}"'/g' docker-compose.yml.template > docker-compose.yml
+error_exit "Creating docker-compose.yml"
 
 docker-compose up
-
-# Eliminate this for next run
-rm docker-compose.yml
-
-for num in $(seq 0 $MAX_NODE_COUNT)
-do
-    docker network rm cl-${TAG_NAME}-${num}
-done
+error_exit "Running integration tests"
