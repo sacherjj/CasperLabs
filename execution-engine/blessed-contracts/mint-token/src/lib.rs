6c215eb40b1c9822debcdf21e5b9763f82b3b826
Henry Till <henrytill@gmail.com>
2019-06-03 16:50:02 -0400

    CON-204: add @goral09's comments for posterity

--- execution-engine/blessed-contracts/mint-token/src/lib.rs
+++ execution-engine/blessed-contracts/mint-token/src/lib.rs
@@ -34,6 +34,11 @@ impl Mint<ARef<U512>, RAWRef<U512>> for CLMint {
     type DepOnlyId = DepositId;
 
     fn create(&self, balance: U512) -> Self::PurseId {
+        // Gorski writes:
+        //   This creates money out of nowhere. It doesn't decrease Mint's available tokens,
+        //   doesn't check whether it reached the limit, etc. I think that it should be created
+        //   with a 0 value at the start and the code that calls the `Mint::creates method would
+        //   transfer funds in a separate call.
         let balance_uref: Key = contract_api::new_uref(balance).into();
 
         let purse_key: Key = contract_api::new_uref(()).into();
@@ -43,6 +48,13 @@ impl Mint<ARef<U512>, RAWRef<U512>> for CLMint {
         contract_api::add_uref(&format!("{:?}", purse_id.raw_id()), &balance_uref);
 
         // store association between purse id and balance uref
+        //
+        // Gorski writes:
+        //   I'm worried that this can lead to overwriting of values in the local state.
+        //   Since it accepts a raw byte array it's possible to construct one by hand. Of course,
+        //   a key can be overwritten only when that write is performed in the "owner" context
+        //   so it aligns with other semantics of write but I would prefer if were able to enforce
+        //   uniqueness somehow.
         contract_api::write_local(purse_id.raw_id(), balance_uref);
 
         purse_id
