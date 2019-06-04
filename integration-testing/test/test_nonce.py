a31d32b58d4af3d14b836f7f9e3c8b5e7a6dcb38
Piotr Kuchta <piotr@casperlabs.io>
2019-06-04 17:51:59 +0200

    OP-227: Add (disabled for now) test_deploy_with_lower_nonce

--- integration-testing/test/test_nonce.py
+++ integration-testing/test/test_nonce.py
@@ -4,7 +4,7 @@ import pytest
 from test.cl_node.client_parser import parse_show_blocks
 from test.cl_node.docker_node import DockerNode
 from .cl_node.wait import wait_for_blocks_count_at_least
-
+from test.cl_node.errors import NonZeroExitCodeError
 
 """
 Feature file: ~/CasperLabs/integration-testing/features/deploy.feature
@@ -18,14 +18,14 @@ def node(one_node_network):
             wait_for_blocks_count_at_least(node, 1, 1, node.timeout)
         yield network.docker_nodes[0]
 
-# DID NOT RAISE
+# DISABLED: DID NOT RAISE
 @pytest.mark.parametrize("contract", ['test_helloname.wasm',])
 def disabled_test_deploy_without_nonce(node, contract: str):
     """
     Feature file: deploy.feature
     Scenario: Deploy without nonce
     """
-    with pytest.raises(Exception):
+    with pytest.raises(NonZeroExitCodeError):
         node.client.deploy(session_contract = contract,
                            payment_contract = contract,
                            nonce = None,
@@ -33,3 +33,28 @@ def disabled_test_deploy_without_nonce(node, contract: str):
                            public_key = "validator-0-public.pem")
         node.client.propose()
         wait_for_blocks_count_at_least(node, 2, 2, node.timeout)
+
+
+@pytest.mark.parametrize("contract", ['test_helloname.wasm',])
+def disable_test_deploy_with_lower_nonce(node, contract: str):
+    """
+    Feature file: deploy.feature
+    Scenario: Deploy with lower nonce
+    """
+
+    for i in range(5):
+        node.client.deploy(session_contract = contract,
+                           payment_contract = contract,
+                           nonce = i,
+                           private_key = "validator-0-private.pem",
+                           public_key = "validator-0-public.pem")
+        node.client.propose()
+        wait_for_blocks_count_at_least(node, i+1, i+1, node.timeout)
+
+    with pytest.raises(NonZeroExitCodeError):
+        node.client.deploy(session_contract = contract,
+                           payment_contract = contract,
+                           nonce = 3,
+                           private_key = "validator-0-private.pem",
+                           public_key = "validator-0-public.pem")
+        node.client.propose()
