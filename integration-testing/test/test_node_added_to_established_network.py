984bf8b924ee95a14a9a300d6e91f3223a58257c
Srinivas Reddy Thatiparthy <srini@casperlabs.io>
2019-06-05 16:22:26 +0530

    OP-271: Add test case  and feature file references

---
+++ integration-testing/test/test_node_added_to_established_network.py
@@ -0,0 +1,5 @@
+def test_newly_joined_node_should_not_gossip_blocks():
+    """
+    Feature file: node_added_to_established_network.feature
+    Scenario: New node should not gossip old blocks back to network
+    """
