e717fd84fa83894314ced4d2fefbff913d682760
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-06-05 14:23:59 +0200

    NO-TICKET: Add --validate-nonce flag.

--- execution-engine/comm/src/main.rs
+++ execution-engine/comm/src/main.rs
@@ -80,6 +80,8 @@ const ARG_SOCKET_EXPECT: &str = "socket required";
 const REMOVING_SOCKET_FILE_MESSAGE: &str = "removing old socket file";
 const REMOVING_SOCKET_FILE_EXPECT: &str = "failed to remove old socket file";
 
+const VALIDATE_NONCE: &str = "validate-nonce";
+
 // loglevel
 const ARG_LOG_LEVEL: &str = "loglevel";
 const ARG_LOG_LEVEL_VALUE: &str = "LOGLEVEL";
@@ -119,7 +121,9 @@ fn main() {
 
     let map_size = get_map_size(matches);
 
-    let _server = get_grpc_server(&socket, data_dir, map_size);
+    let nonce_check = get_nonce_check(matches);
+
+    let _server = get_grpc_server(&socket, data_dir, map_size, nonce_check);
 
     log_listening_message(&socket);
 
@@ -187,6 +191,10 @@ fn get_args() -> ArgMatches<'static> {
                 .help(ARG_SOCKET_HELP)
                 .index(1),
         )
+        .arg(
+            Arg::with_name(VALIDATE_NONCE)
+                .required(false)
+        )
         .get_matches()
 }
 
@@ -233,9 +241,13 @@ fn get_map_size(matches: &ArgMatches) -> usize {
     page_size * pages
 }
 
+fn get_nonce_check(matches: &ArgMatches) -> bool {
+    matches.is_present(VALIDATE_NONCE)
+}
+
 /// Builds and returns a gRPC server.
-fn get_grpc_server(socket: &socket::Socket, data_dir: PathBuf, map_size: usize) -> grpc::Server {
-    let engine_state = get_engine_state(data_dir, map_size);
+fn get_grpc_server(socket: &socket::Socket, data_dir: PathBuf, map_size: usize, nonce_check: bool) -> grpc::Server {
+    let engine_state = get_engine_state(data_dir, map_size, nonce_check);
 
     engine_server::new(socket.as_str(), engine_state)
         .build()
@@ -243,7 +255,7 @@ fn get_grpc_server(socket: &socket::Socket, data_dir: PathBuf, map_size: usize) 
 }
 
 /// Builds and returns engine global state
-fn get_engine_state(data_dir: PathBuf, map_size: usize) -> EngineState<LmdbGlobalState> {
+fn get_engine_state(data_dir: PathBuf, map_size: usize, nonce_check: bool) -> EngineState<LmdbGlobalState> {
     let environment = {
         let ret = LmdbEnvironment::new(&data_dir, map_size).expect(LMDB_ENVIRONMENT_EXPECT);
         Arc::new(ret)
@@ -266,7 +278,7 @@ fn get_engine_state(data_dir: PathBuf, map_size: usize) -> EngineState<LmdbGloba
         .expect(LMDB_GLOBAL_STATE_EXPECT)
     };
 
-    EngineState::new(global_state)
+    EngineState::new(global_state, nonce_check)
 }
 
 /// Builds and returns log_settings
