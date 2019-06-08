0e55626b18eca245af17cde2347719c97f9c68c2
Igor Ramazanov <igor@casperlabs.io>
2019-06-08 23:51:22 +0300

    NODE-550: Add arguments

--- node/src/main/scala/io/casperlabs/node/api/graphql/schema/globalstate/arguments/package.scala
+++ node/src/main/scala/io/casperlabs/node/api/graphql/schema/globalstate/arguments/package.scala
@@ -1,3 +1,74 @@
 package io.casperlabs.node.api.graphql.schema.globalstate
 
-package object arguments {}
+import cats.syntax.option._
+import sangria.marshalling.{CoercedScalaResultMarshaller, FromInput, ResultMarshaller}
+import sangria.schema._
+
+package object arguments {
+  val GlobalStateKeyType = EnumType(
+    "KeyType",
+    "Type of key used to query the global state".some,
+    List(
+      EnumValue(
+        "Hash",
+        value = "Hash"
+      ),
+      EnumValue(
+        "URef",
+        value = "URef"
+      ),
+      EnumValue(
+        "Address",
+        value = "Address"
+      )
+    )
+  )
+
+  val KeyType = Argument(
+    "keyType",
+    GlobalStateKeyType,
+    "Type of key used to query the global state"
+  )
+
+  val Key = Argument(
+    "keyBase16",
+    StringType,
+    "Base-16 encoded key"
+  )
+
+  case class StateQuery(keyType: String, key: String, pathSegments: Seq[String])
+
+  implicit val decoder = new FromInput[StateQuery] {
+    override val marshaller: ResultMarshaller = CoercedScalaResultMarshaller.default
+
+    override def fromResult(node: marshaller.Node): StateQuery = {
+      val ad = node.asInstanceOf[Map[String, Any]]
+      StateQuery(
+        ad("keyType").asInstanceOf[String],
+        ad("keyBase16").asInstanceOf[String],
+        ad("pathSegments").asInstanceOf[Seq[String]]
+      )
+    }
+  }
+
+  val StateQueryType: InputObjectType[StateQuery] = InputObjectType[StateQuery](
+    "StateQuery",
+    List(
+      InputField(
+        "keyType",
+        GlobalStateKeyType
+      ),
+      InputField(
+        "keyBase16",
+        StringType,
+        "Must be 32 bytes long"
+      ),
+      InputField(
+        "pathSegments",
+        ListInputType(StringType)
+      )
+    )
+  )
+
+  val StateQueryArgument = Argument("StateQueries", ListInputType(StateQueryType))
+}
