0e6ce7e4449c6830c59e34191cade07c0435f444
Piotr Kuchta <piotr@casperlabs.io>
2019-06-08 19:42:10 +0200

    OP-296: Fix test: after adding new node to network wait till it syncs all blocks.

--- integration-testing/test/test_node_added_to_established_network.py
+++ integration-testing/test/test_node_added_to_established_network.py
@@ -11,21 +11,26 @@ def test_newly_joined_node_should_not_gossip_blocks(two_node_network):
     Feature file: node_added_to_established_network.feature
     Scenario: New node should not gossip old blocks back to network
     """
-    block_hashes = []
-    node0, node1 = two_node_network.docker_nodes
-    for node in two_node_network.docker_nodes:
-        block_hashes.append(node.deploy_and_propose(session_contract=HELLO_NAME,
-                                                    private_key="validator-0-private.pem",
-                                                    public_key="validator-0-public.pem"))
-    for node in two_node_network.docker_nodes:
-        wait_for_blocks_count_at_least(node, 3, 3, node.timeout)
+    network = two_node_network
+
+    def wait_for_blocks_propagated(n):
+        for node in network.docker_nodes:
+            wait_for_blocks_count_at_least(node, n, n, node.timeout)
+
+    block_hashes = [node.deploy_and_propose(session_contract=HELLO_NAME) for node in network.docker_nodes]
+    wait_for_blocks_propagated(3)
+
+    node0, node1 = network.docker_nodes
     node0_new_blocks_requests_total = get_new_blocks_requests_total(node0)
     node1_new_blocks_requests_total = get_new_blocks_requests_total(node1)
 
-    two_node_network.add_new_node_to_network()
-    node0, node1, node2 = two_node_network.docker_nodes
+    network.add_new_node_to_network()
+    wait_for_blocks_propagated(3)
+
+    node0, node1, node2 = network.docker_nodes
     for block in block_hashes:
         assert f"Attempting to add Block {block}... to DAG" in node2.logs()
+
     wait_for_gossip_metrics_and_assert_blocks_gossiped(node2, node2.timeout, 0)
     assert node0_new_blocks_requests_total == get_new_blocks_requests_total(node0)
     assert node1_new_blocks_requests_total == get_new_blocks_requests_total(node1)
