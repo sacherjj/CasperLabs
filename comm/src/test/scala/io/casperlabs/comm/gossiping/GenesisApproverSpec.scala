62a918c102a64ccce03b558d8ec136b898eece01
Akosh Farkash <aakoshh@gmail.com>
2019-06-12 15:22:02 +0100

    FIX: More time in GenesisApproverSpec.

--- comm/src/test/scala/io/casperlabs/comm/gossiping/GenesisApproverSpec.scala
+++ comm/src/test/scala/io/casperlabs/comm/gossiping/GenesisApproverSpec.scala
@@ -247,7 +247,7 @@ class GenesisApproverSpec extends WordSpecLike with Matchers with ArbitraryConse
         relayFactor = 5
       ) { approver =>
         for {
-          _ <- Task.sleep(50.millis)
+          _ <- Task.sleep(500.millis)
           r <- approver.getCandidate
         } yield {
           r.isRight shouldBe true
