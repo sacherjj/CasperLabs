f08528c5adca8aae83f9c64eb35c89b17f9fb37c
Piotr Kuchta <piotr@casperlabs.io>
2019-06-13 20:26:53 +0200

    OP-321: Address review comments from Joe.

--- integration-testing/test/test_query_state_error_handling.py
+++ integration-testing/test/test_query_state_error_handling.py
@@ -10,15 +10,16 @@ import logging
 
 # Examples of query-state executed with the Scala client that result in errors:
 
-# aakoshh@af-dp:~/projects/CasperLabs/docker$ ./client.sh node-0 propose
+# CasperLabs/docker $ ./client.sh node-0 propose
 # Response: Success! Block 9d38836598... created and added.
-# aakoshh@af-dp:~/projects/CasperLabs/docker$ ./client.sh node-0 query-state --block-hash '"9d"' --key '"a91208047c"' --path file.xxx --type hash
+
+# CasperLabs/docker $ ./client.sh node-0 query-state --block-hash '"9d"' --key '"a91208047c"' --path file.xxx --type hash
 # NOT_FOUND: Cannot find block matching hash "9d"
 
-# aakoshh@af-dp:~/projects/CasperLabs/docker$ ./client.sh node-0 query-state --block-hash 9d --key '"a91208047c"' --path file.xxx --type hash
+# CasperLabs/docker$ ./client.sh node-0 query-state --block-hash 9d --key '"a91208047c"' --path file.xxx --type hash
 # INVALID_ARGUMENT: Key of type hash must have exactly 32 bytes, 5 =/= 32 provided.
 
-# aakoshh@af-dp:~/projects/CasperLabs/docker$ ./client.sh node-0 query-state --block-hash 9d --key 3030303030303030303030303030303030303030303030303030303030303030 --path file.xxx --type hash
+# CasperLabs/docker$ ./client.sh node-0 query-state --block-hash 9d --key 3030303030303030303030303030303030303030303030303030303030303030 --path file.xxx --type hash
 # INVALID_ARGUMENT: Value not found: " Hash([48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48])"
 
 
@@ -39,13 +40,13 @@ def block_hash(node):
     return node.deploy_and_propose()
 
 block_hash_queries = [
-    (dict(blockHash = "9d", key = "a91208047c", path = "file.xxx", keyType = "hash"),
+    ({'blockHash': "9d", 'key': "a91208047c", 'path': "file.xxx", 'keyType': "hash"},
      "NOT_FOUND: Cannot find block matching"),
 
-    (dict(                  key = "a91208047c", path = "file.xxx", keyType = "hash"),
+    ({                   'key': "a91208047c", 'path': "file.xxx", 'keyType': "hash"},
      "INVALID_ARGUMENT: Key of type hash must have exactly 32 bytes"),
 
-    (dict(                  key = KEY,          path = "file.xxx", keyType = "hash"),
+    ({                   'key': KEY,          'path': "file.xxx", 'keyType': "hash"},
      "INVALID_ARGUMENT: Value not found"),
 ]
 
