873dfd7341d21c73ac92f423e6484666c700484c
Ed Hastings <ed@casperlabs.io>
2019-06-17 15:47:31 -0700

    EE-{266,359}: address comments from @goral09

--- execution-engine/engine/src/engine_state/mod.rs
+++ execution-engine/engine/src/engine_state/mod.rs
@@ -122,7 +122,7 @@ pub fn create_genesis_effects(
 
     let mint_known_urefs = {
         let mut ret: BTreeMap<String, Key> = BTreeMap::new();
-        ret.insert(balance_uref.as_string(), Key::URef(balance_uref));
+        ret.insert(balance_uref.as_string(), balance_uref_key);
         ret
     };
 
@@ -249,50 +249,320 @@ where
 
 #[cfg(test)]
 mod tests {
-    use common::value::U512;
-    use engine_state::create_genesis_effects;
-    use engine_state::utils::WasmiBytes;
-    use shared::test_utils;
+    use std::collections::btree_map::BTreeMap;
+    use std::collections::HashMap;
+
+    use rand::RngCore;
+
+    use common::key::Key;
+    use common::uref::{AccessRights, URef};
+    use common::value::account::PurseId;
+    use common::value::{Account, Contract, Value, U512};
+    use execution;
+    use shared::newtypes::Blake2bHash;
     use shared::transform::Transform;
+    use shared::{init, test_utils};
     use wasm_prep::wasm_costs::WasmCosts;
 
-    #[test]
-    fn create_genesis_effects_creates_expected_effects() {
-        let genesis_account_addr = [6u8; 32];
+    use engine_state::create_genesis_effects;
+    use engine_state::utils::WasmiBytes;
 
-        let initial_tokens = U512::from_dec_str("1000").expect("should create U512");
+    const GENESIS_ACCOUNT_ADDR: [u8; 32] = [6u8; 32];
+    const PROTOCOL_VERSION: u64 = 1;
+    const EXPECTED_GENESIS_TRANSFORM_COUNT: usize = 5;
+    const INITIAL_BALANCE: &str = "1000";
 
-        let mint_code_bytes = {
-            let raw_bytes = test_utils::create_empty_wasm_module_bytes();
-            WasmiBytes::new(raw_bytes.as_slice(), WasmCosts::free())
-                .expect("should create wasmi bytes")
-        };
+    fn get_initial_tokens(initial_balance: &str) -> U512 {
+        U512::from_dec_str(initial_balance).expect("should create U512")
+    }
 
-        let proof_of_stake_bytes = {
-            let raw_bytes = test_utils::create_empty_wasm_module_bytes();
-            WasmiBytes::new(raw_bytes.as_slice(), WasmCosts::free())
-                .expect("should create wasmi bytes")
-        };
+    fn get_mint_code_bytes() -> WasmiBytes {
+        let raw_bytes = test_utils::create_empty_wasm_module_bytes();
+        WasmiBytes::new(raw_bytes.as_slice(), WasmCosts::free()).expect("should create wasmi bytes")
+    }
 
-        let protocol_version = 1;
+    fn get_proof_of_stake_code_bytes() -> WasmiBytes {
+        let raw_bytes = test_utils::create_empty_wasm_module_bytes();
+        WasmiBytes::new(raw_bytes.as_slice(), WasmCosts::free()).expect("should create wasmi bytes")
+    }
 
-        let effects = create_genesis_effects(
-            genesis_account_addr,
+    fn get_genesis_effects() -> HashMap<Key, Transform> {
+        let initial_tokens = get_initial_tokens(INITIAL_BALANCE);
+
+        let mint_code_bytes = get_mint_code_bytes();
+
+        let proof_of_stake_bytes = get_proof_of_stake_code_bytes();
+
+        create_genesis_effects(
+            GENESIS_ACCOUNT_ADDR,
             initial_tokens,
             mint_code_bytes,
             proof_of_stake_bytes,
-            protocol_version,
+            PROTOCOL_VERSION,
         )
-        .expect("should create effects");
-
-        assert_eq!(effects.len(), 5);
-
-        assert!(effects
-            .iter()
-            .all(|(_, effect)| if let Transform::Write(_) = effect {
-                true
-            } else {
-                false
-            }));
+        .expect("should create effects")
     }
+
+    fn is_write(transform: &Transform) -> bool {
+        if let Transform::Write(_) = transform {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn extract_transform_account(effects: HashMap<Key, Transform>, key: &Key) -> Option<Account> {
+        if let Transform::Write(Value::Account(value)) =
+            effects.get(key).expect("should have value")
+        {
+            Some(value.to_owned())
+        } else {
+            None
+        }
+    }
+
+    fn extract_transform_key(effects: HashMap<Key, Transform>, key: &Key) -> Option<Key> {
+        if let Transform::Write(Value::Key(value)) = effects.get(key).expect("should have value") {
+            Some(*value)
+        } else {
+            None
+        }
+    }
+
+    fn extract_transform_u512(effects: HashMap<Key, Transform>, key: &Key) -> Option<U512> {
+        if let Transform::Write(Value::UInt512(value)) =
+            effects.get(key).expect("should have value")
+        {
+            Some(*value)
+        } else {
+            None
+        }
+    }
+
+    fn extract_transform_contract_bytes(
+        effects: HashMap<Key, Transform>,
+        key: &Key,
+    ) -> Option<Contract> {
+        if let Transform::Write(Value::Contract(value)) =
+            effects.get(key).expect("should have value")
+        {
+            Some(value.to_owned())
+        } else {
+            None
+        }
+    }
+
+    #[test]
+    fn create_genesis_effects_creates_expected_effects() {
+        let effects = get_genesis_effects();
+
+        assert_eq!(effects.len(), EXPECTED_GENESIS_TRANSFORM_COUNT);
+
+        assert!(effects.iter().all(|(_, effect)| is_write(effect)));
+    }
+
+    #[test]
+    fn create_genesis_effects_stores_mint_contract_uref_at_public_uref() {
+        // given predictable uref(s) should be able to retrieve values and assert expected
+
+        let mut rng = execution::create_rng(GENESIS_ACCOUNT_ADDR, 0, 0);
+
+        let public_uref_key = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            Key::URef(URef::new(addr, AccessRights::READ_ADD_WRITE))
+        };
+
+        let effects = get_genesis_effects();
+
+        assert!(
+            effects.contains_key(&public_uref_key),
+            "should have expected public_uref"
+        );
+
+        let actual = extract_transform_key(effects, &public_uref_key)
+            .expect("transform was not a write of a key");
+
+        let mint_contract_uref_key = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            Key::URef(URef::new(addr, AccessRights::READ))
+        };
+
+        // the value under the outer mint_contract_uref should be a key value pointing at
+        // the current contract bytes
+        assert_eq!(
+            actual, mint_contract_uref_key,
+            "invalid mint contract indirection"
+        );
+    }
+
+    #[test]
+    fn create_genesis_effects_stores_mint_contract_code_at_mint_contract_uref() {
+        let mut rng = execution::create_rng(GENESIS_ACCOUNT_ADDR, 0, 0);
+
+        // this is passing as currently designed, but see bug: EE-380
+        let mint_contract_uref_key = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            rng.fill_bytes(&mut addr);
+            Key::URef(URef::new(addr, AccessRights::READ))
+        };
+
+        let effects = get_genesis_effects();
+
+        let actual = extract_transform_contract_bytes(effects, &mint_contract_uref_key)
+            .expect("transform was not a write of a key");
+
+        let mint_code_bytes = get_mint_code_bytes();
+
+        // this is passing as currently designed, but see bug: EE-380
+        let balance_uref = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            rng.fill_bytes(&mut addr);
+            URef::new(addr, AccessRights::READ_ADD_WRITE)
+        };
+
+        let balance_uref_key = Key::URef(balance_uref);
+
+        let mint_known_urefs = {
+            let mut ret: BTreeMap<String, Key> = BTreeMap::new();
+            ret.insert(balance_uref.as_string(), balance_uref_key);
+            ret
+        };
+
+        let mint_contract: Contract = Contract::new(mint_code_bytes.into(), mint_known_urefs, 1);
+
+        // the value under the mint_contract_uref_key should be the current contract bytes
+        assert_eq!(actual, mint_contract, "invalid mint contract bytes");
+    }
+
+    #[test]
+    fn create_genesis_effects_balance_uref_at_purse_id() {
+        let mut rng = execution::create_rng(GENESIS_ACCOUNT_ADDR, 0, 0);
+
+        // this is passing as currently designed, but see bug: EE-380
+        let mint_contract_uref = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            rng.fill_bytes(&mut addr);
+            URef::new(addr, AccessRights::READ)
+        };
+
+        let purse_id_uref = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            URef::new(addr, AccessRights::READ_ADD_WRITE)
+        };
+
+        let purse_id_local_key = {
+            let seed = mint_contract_uref.addr();
+            let local_key = purse_id_uref.addr();
+            let key_hash = Blake2bHash::new(&local_key).into();
+            Key::Local { seed, key_hash }
+        };
+
+        let balance_uref = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            URef::new(addr, AccessRights::READ_ADD_WRITE)
+        };
+
+        let effects = get_genesis_effects();
+
+        assert!(
+            effects.contains_key(&purse_id_local_key),
+            "effects should contain purse_id_local_key"
+        );
+
+        let actual = extract_transform_key(effects, &purse_id_local_key)
+            .expect("transform was not a write of a key");
+
+        // the value under the outer mint_contract_uref should be a key value pointing at
+        // the current contract bytes
+        assert_eq!(
+            actual,
+            Key::URef(balance_uref),
+            "invalid balance indirection"
+        );
+    }
+
+    #[test]
+    fn create_genesis_effects_balance_at_balance_uref() {
+        let mut rng = execution::create_rng(GENESIS_ACCOUNT_ADDR, 0, 0);
+
+        // this is passing as currently designed, but see bug: EE-380
+        let mint_contract_uref = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            rng.fill_bytes(&mut addr);
+            URef::new(addr, AccessRights::READ)
+        };
+
+        let purse_id_uref = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            URef::new(addr, AccessRights::READ_ADD_WRITE)
+        };
+
+        let purse_id_local_key = {
+            let seed = mint_contract_uref.addr();
+            let local_key = purse_id_uref.addr();
+            let key_hash = Blake2bHash::new(&local_key).into();
+            Key::Local { seed, key_hash }
+        };
+
+        let balance_uref = {
+            let mut addr = [0u8; 32];
+            rng.fill_bytes(&mut addr);
+            URef::new(addr, AccessRights::READ_ADD_WRITE)
+        };
+
+        let effects = get_genesis_effects();
+
+        assert!(
+            effects.contains_key(&purse_id_local_key),
+            "effects should contain purse_id_local_key"
+        );
+
+        let balance_uref_key = Key::URef(balance_uref);
+
+        let actual = extract_transform_u512(effects, &balance_uref_key)
+            .expect("transform was not a write of a key");
+
+        let initial_tokens = get_initial_tokens(INITIAL_BALANCE);
+
+        // the value under the outer balance_uref_key should be a U512 value (the actual balance)
+        assert_eq!(actual, initial_tokens, "invalid balance");
+    }
+
+    #[test]
+    fn create_genesis_effects_stores_genesis_account_at_genesis_account_addr() {
+        let account_key = Key::Account(GENESIS_ACCOUNT_ADDR);
+
+        let effects = get_genesis_effects();
+
+        assert!(
+            effects.contains_key(&account_key),
+            "should have expected account key"
+        );
+
+        let mut rng = execution::create_rng(GENESIS_ACCOUNT_ADDR, 0, 0);
+
+        // this is passing as currently designed, but see bug: EE-380
+        let purse_id = {
+            let mut bytes = [0u8; 32];
+            rng.fill_bytes(&mut bytes);
+            rng.fill_bytes(&mut bytes);
+            rng.fill_bytes(&mut bytes);
+            PurseId::new(URef::new(bytes, AccessRights::READ_ADD_WRITE))
+        };
+
+        let genesis_account = init::create_genesis_account(GENESIS_ACCOUNT_ADDR, purse_id);
+        let actual = extract_transform_account(effects, &account_key).expect("should have account");
+
+        assert_eq!(actual, genesis_account, "invalid account indirection");
+    }
+
 }
