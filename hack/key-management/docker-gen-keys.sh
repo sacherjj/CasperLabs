bd06d3e412a00ad71aca4c9d2d277b086958ec5a
Igor Ramazanov <igor@casperlabs.io>
2019-06-18 00:39:58 +0300

    NODE-571: Add keys generator Docker image to CI

--- hack/key-management/docker-gen-keys.sh
+++ hack/key-management/docker-gen-keys.sh
@@ -1,21 +1,37 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
 # Generates all necessary keys for a node using Docker image
 # Usage:
-# ./docker-gen-keys.sh <directory where to put keys>
+# ./docker-gen-keys.sh <directory where to put keys> [--test]
 
 if [[ "$1" == /* ]] || [[ "$1" == ~* ]]; then
-	OUTPUT_DIR="$1"
+    OUTPUT_DIR="$1"
 else
-	OUTPUT_DIR="$(PWD)/$1"	
+    OUTPUT_DIR="$(PWD)/$1"
 fi
 
 if [[ ! -d "$OUTPUT_DIR" ]]; then
     echo "ERROR: output dir doesn't exist"
     echo "usage: ./docker-gen-keys.sh <dir>"
-    exit 1
+    exit "1"
 fi
 
-docker pull casperlabs/keys-generator
+shift
+
+docker pull casperlabs/keys-generator:latest || true
 docker run --rm -it -v "$OUTPUT_DIR":/keys casperlabs/keys-generator /keys
+
+if [[ "$1" == "--test" ]]; then
+    if [[ -f "$OUTPUT_DIR/node-id" ]] && \
+       [[ -f "$OUTPUT_DIR/validator-id" ]] && \
+       [[ -f "$OUTPUT_DIR/node.key.pem" ]] && \
+       [[ -f "$OUTPUT_DIR/node.certificate.pem" ]] && \
+       [[ -f "$OUTPUT_DIR/validator-private.pem" ]] && \
+       [[ -f "$OUTPUT_DIR/validator-public.pem" ]]; then
+        exit "0"
+    else
+        echo "ERROR: Some of required files haven't created"
+        exit "1"
+    fi
+fi
