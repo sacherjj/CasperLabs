19585a0968b637c3c0125a5c631f433add4b5ed0
Akosh Farkash <aakoshh@gmail.com>
2019-06-17 12:59:45 +0100

    NODE-424: Change docker/client.sh to look for node-id and use TLS.

--- hack/docker/template/start-node.sh
+++ hack/docker/template/start-node.sh
@@ -5,15 +5,11 @@ set -e
 if [ "$HOSTNAME" = "$BOOTSTRAP_HOSTNAME" ]; then
     # Start the bootstrap node with a fixed key to get a deterministic ID.
     exec ./bin/casperlabs-node run -s \
-        --server-host $HOSTNAME \
-        --grpc-socket $CL_GRPC_SOCKET \
-        --metrics-prometheus
+        --server-host $HOSTNAME
 else
     # Connect to the bootstrap node.
     BOOTSTRAP_ID=$(cat $HOME/.casperlabs/bootstrap/node-id)
     exec ./bin/casperlabs-node run \
         --server-host $HOSTNAME \
-        --server-bootstrap "casperlabs://${BOOTSTRAP_ID}@${BOOTSTRAP_HOSTNAME}?protocol=40400&discovery=40404" \
-        --grpc-socket $CL_GRPC_SOCKET \
-        --metrics-prometheus
+        --server-bootstrap "casperlabs://${BOOTSTRAP_ID}@${BOOTSTRAP_HOSTNAME}?protocol=40400&discovery=40404"
 fi
