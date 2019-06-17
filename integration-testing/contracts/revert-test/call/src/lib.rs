3769a642173bf94ab7f0647a36447b76fe2eed93
Piotr Kuchta <piotr@casperlabs.io>
2019-06-17 14:32:33 +0200

    OP-300: Add contract for testing revert.

---
+++ integration-testing/contracts/revert-test/call/src/lib.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![feature(alloc)]
+
+extern crate alloc;
+use alloc::vec::Vec;
+
+extern crate common;
+use common::contract_api::call_contract;
+use common::contract_api::pointers::ContractPointer;
+
+#[no_mangle]
+pub extern "C" fn call() {
+    //This hash comes from blake2b256( [0;32] ++ [0;8] ++ [0;4] )
+    //                                  pk    ++ nonce ++ function_counter 
+    // nonce=0, function_counter=0
+    let hash = ContractPointer::Hash([164, 102, 153, 51, 236, 214, 169, 167, 126, 44, 250, 247, 179, 214, 203, 229, 239, 69, 145, 25, 5, 153, 113, 55, 255, 188, 176, 201, 7, 4, 42, 100]
+);
+    let arg = "inc";
+    let _result: () = call_contract(hash.clone(), &arg, &Vec::new());
+    let _value: i32 = {
+        let arg = "get";
+        call_contract(hash, &arg, &Vec::new())
+    };
+}
---
+++ integration-testing/contracts/revert-test/define/src/lib.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![feature(alloc)]
+
+extern crate alloc;
+use alloc::collections::BTreeMap;
+use alloc::string::String;
+
+extern crate common;
+use common::contract_api::*;
+use common::key::Key;
+
+#[no_mangle]
+pub extern "C" fn revert_test_ext() {
+    revert(1);
+}
+
+#[no_mangle]
+pub extern "C" fn call() {
+
+    //create map of references for stored contract
+    let revert_test_urefs: BTreeMap<String, Key> = BTreeMap::new();
+
+    let _hash = store_function("revert_test_ext", revert_test_urefs);
+    add_uref("revert_test", &_hash.into());
+}
