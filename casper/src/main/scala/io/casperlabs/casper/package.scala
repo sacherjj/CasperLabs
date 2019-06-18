6eb6df2f0f51ddcb79e456b1044198b9b5c0acd3
Akosh Farkash <aakoshh@gmail.com>
2019-06-18 16:29:52 +0100

    NODE-591: Add MultiParentCasperImpl.requeueOrphanedDeploys

--- casper/src/main/scala/io/casperlabs/casper/package.scala
+++ casper/src/main/scala/io/casperlabs/casper/package.scala
@@ -1,7 +1,9 @@
 package io.casperlabs
 
+import com.google.protobuf.ByteString
 import io.casperlabs.metrics.Metrics
 
 package object casper {
   val CasperMetricsSource: Metrics.Source = Metrics.Source(Metrics.BaseSource, "casper")
+  type DeployHash = ByteString
 }
