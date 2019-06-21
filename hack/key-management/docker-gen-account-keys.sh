0be9983208c4c7d36802a3ba2d91846e8936df62
Akosh Farkash <aakoshh@gmail.com>
2019-06-21 17:33:28 +0100

    NODE-595: Script to generate account keys.

---
+++ hack/key-management/docker-gen-account-keys.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+$(dirname $0)/docker-gen-keys.sh $1
+cd $1
+rm node*
+mv validator-id account-id
+mv validator-public.pem account-public.pem
+mv validator-private.pem account-private.pem
