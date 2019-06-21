1e68ff73427976df6ad92b8618b317f8dd35eea3
TomVasile <43349666+TomVasile@users.noreply.github.com>
2019-06-21 14:42:45 +0000

    add hex to test

--- hack/key-management/docker-gen-keys.sh
+++ hack/key-management/docker-gen-keys.sh
@@ -37,6 +37,7 @@ fi
 if [[ "$1" == "--test" ]]; then
     if [[ -f "$OUTPUT_DIR/node-id" ]] && \
        [[ -f "$OUTPUT_DIR/validator-id" ]] && \
+       [[ -f "$OUTPUT_DIR/validator-id-hex" ]] && \
        [[ -f "$OUTPUT_DIR/node.key.pem" ]] && \
        [[ -f "$OUTPUT_DIR/node.certificate.pem" ]] && \
        [[ -f "$OUTPUT_DIR/validator-private.pem" ]] && \
