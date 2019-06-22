f17195841f631e1e45a2bf8ea01eb1fdad53d1a1
Piotr Kuchta <piotr@casperlabs.io>
2019-06-22 15:19:52 +0200

    OP-344: Fix test

--- integration-testing/test/test_counter.py
+++ integration-testing/test/test_counter.py
@@ -7,6 +7,6 @@ def test_counter(one_node_network):
     def create_block(contract):
         return node.deploy_and_propose(session_contract=contract, payment_contract=contract)
 
-    block_hash = create_block("test_countercall.wasm")
-    r = client.query_state(block_hash=block_hash, key=node.from_address(), key_type="address", path="counter/coount")
-
+    block_hash = create_block("test_counterdefine.wasm")
+    r = client.query_state(block_hash=block_hash, key=node.from_address(), key_type="address", path="counter/count")
+    assert r.int_value == 0
