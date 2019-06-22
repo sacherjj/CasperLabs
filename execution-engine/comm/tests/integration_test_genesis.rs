5ba3f32354900238c55374ced0fd6ebfa675c849
Henry Till <henrytill@gmail.com>
2019-06-22 13:49:24 -0400

    execution-engine/comm: fix should_run_genesis

--- execution-engine/comm/tests/integration_test_genesis.rs
+++ execution-engine/comm/tests/integration_test_genesis.rs
@@ -14,14 +14,54 @@ use grpc::RequestOptions;
 use execution_engine::engine_state::EngineState;
 use storage::global_state::in_memory::InMemoryGlobalState;
 
+use casperlabs_engine_grpc_server::engine_server::ipc::{DeployCode, GenesisRequest};
 use casperlabs_engine_grpc_server::engine_server::ipc_grpc::ExecutionEngineService;
+use casperlabs_engine_grpc_server::engine_server::state::{BigInt, ProtocolVersion};
+use shared::test_utils;
 
 #[test]
 fn should_run_genesis() {
     let global_state = InMemoryGlobalState::empty().expect("should create global state");
     let engine_state = EngineState::new(global_state, false);
 
-    let genesis_request = test_support::create_genesis_request();
+    let genesis_request = {
+        let genesis_account_addr = [6u8; 32].to_vec();
+
+        let initial_tokens = {
+            let mut ret = BigInt::new();
+            ret.set_bit_width(512);
+            ret.set_value("1000000".to_string());
+            ret
+        };
+
+        let mint_code = {
+            let mut ret = DeployCode::new();
+            let wasm_bytes = test_utils::create_empty_wasm_module_bytes();
+            ret.set_code(wasm_bytes);
+            ret
+        };
+
+        let proof_of_stake_code = {
+            let mut ret = DeployCode::new();
+            let wasm_bytes = test_utils::create_empty_wasm_module_bytes();
+            ret.set_code(wasm_bytes);
+            ret
+        };
+
+        let protocol_version = {
+            let mut ret = ProtocolVersion::new();
+            ret.set_value(1);
+            ret
+        };
+
+        let mut ret = GenesisRequest::new();
+        ret.set_address(genesis_account_addr.to_vec());
+        ret.set_initial_tokens(initial_tokens);
+        ret.set_mint_code(mint_code);
+        ret.set_proof_of_stake_code(proof_of_stake_code);
+        ret.set_protocol_version(protocol_version);
+        ret
+    };
 
     let request_options = RequestOptions::new();
 
