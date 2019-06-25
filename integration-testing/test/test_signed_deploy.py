f1523b2b45cdff7e0005904306cb83d0cf0dab2c
Piotr Kuchta <piotr@casperlabs.io>
2019-06-25 23:00:06 +0200

    OP-344: Fix test_signed_deploy.py

--- integration-testing/test/test_signed_deploy.py
+++ integration-testing/test/test_signed_deploy.py
@@ -4,7 +4,9 @@ def test_deploy_with_valid_signature(one_node_network_signed):
     Scenario: Deploy with valid signature
     """
     node0 = one_node_network_signed.docker_nodes[0]
-    node0.client.deploy(private_key="validator-0-private.pem",
+    node0.client.deploy(session_contract='test_helloname.wasm',
+                        payment_contract='test_helloname.wasm',
+                        private_key="validator-0-private.pem",
                         public_key="validator-0-public.pem")
 
 
@@ -17,7 +19,9 @@ def test_deploy_with_invalid_signature(one_node_network_signed):
     node0 = one_node_network_signed.docker_nodes[0]
 
     try:
-        node0.client.deploy(private_key="validator-0-private-invalid.pem",
+        node0.client.deploy(session_contract='test_helloname.wasm',
+                            payment_contract='test_helloname.wasm',
+                            private_key="validator-0-private-invalid.pem",
                             public_key="validator-0-public-invalid.pem")
         assert False, "Deploy signed with invalid signatures has been passed"
     except Exception as ex:
