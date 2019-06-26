1bb14517df05bbde7418af8d78fc059c91513482
Igor Ramazanov <igor@casperlabs.io>
2019-06-26 18:05:13 +0300

    NODE-590: Shutdown application on fatal errors

--- shared/src/main/scala/io/casperlabs/shared/UncaughtExceptionHandler.scala
+++ shared/src/main/scala/io/casperlabs/shared/UncaughtExceptionHandler.scala
@@ -1,13 +1,18 @@
 package io.casperlabs.shared
 
 import cats.Id
-
 import monix.execution.UncaughtExceptionReporter
 
-object UncaughtExceptionLogger extends UncaughtExceptionReporter {
+import scala.util.control.NonFatal
+
+object UncaughtExceptionHandler extends UncaughtExceptionReporter {
   private implicit val logSource: LogSource = LogSource(this.getClass)
   private val log: Log[Id]                  = Log.logId
 
-  def reportFailure(ex: scala.Throwable): Unit =
+  override def reportFailure(ex: scala.Throwable): Unit = {
     log.error(s"Uncaught Exception : ${ex.getMessage}", ex)
+    if (!NonFatal(ex)) {
+      sys.exit(1)
+    }
+  }
 }
