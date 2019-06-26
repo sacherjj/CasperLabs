3ddec21338ec03b9f052ec3a0dac067491d4756e
Henry Till <henrytill@gmail.com>
2019-06-26 14:36:10 -0400

    EE-405: address comments by @goral09

--- execution-engine/test-contracts/create-purse-01/src/lib.rs
+++ execution-engine/test-contracts/create-purse-01/src/lib.rs
@@ -7,6 +7,8 @@ extern crate cl_std;
 use cl_std::uref::{AccessRights, URef};
 use cl_std::value::account::PurseId;
 
+// This value was acquired by observing the output of an execution of this contract
+// made by ACCOUNT_1.
 const EXPECTED_UREF_BYTES: [u8; 32] = [
     73, 143, 110, 138, 106, 168, 247, 100, 112, 181, 14, 171, 133, 47, 108, 16, 3, 147, 232, 172,
     251, 67, 247, 26, 160, 197, 79, 100, 233, 232, 174, 118,
