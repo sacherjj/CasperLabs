aaf9844ed58ea082435987880141ef67560226fb
Srinivas Reddy Thatiparthy <srini@casperlabs.io>
2019-06-27 19:50:07 +0530

    OP-334: changes to the runtests.sh and install.sh files

--- integration-testing/run_tests.sh
+++ integration-testing/run_tests.sh
@@ -13,5 +13,6 @@ if [[ "$TEST_RUN_ARGS" == "" ]]; then
 fi
 pip install pipenv
 pipenv sync
+pipenv run client/CasperClient/install.sh
 pipenv run py.test ${PYTEST_ARGS} -v "$TEST_RUN_ARGS"
 pipenv run python3 ./docker_cleanup_assurance.py
