ddc70e2caa05fa880286af4a58724a5ca365cabb
Piotr Kuchta <piotr@casperlabs.io>
2019-07-01 22:12:43 +0200

    OP-344: Fix for mailinglist contracts from Ed.

--- integration-testing/contracts/mailing-list/call/src/lib.rs
+++ integration-testing/contracts/mailing-list/call/src/lib.rs
@@ -1,7 +1,6 @@
 #![no_std]
 #![feature(alloc)]
 
-#[macro_use]
 extern crate alloc;
 use alloc::string::String;
 use alloc::vec::Vec;
@@ -22,25 +21,28 @@ pub extern "C" fn call() {
     let method = "sub";
     let name = "CasperLabs";
     let args = (method, name);
-    let maybe_sub_key: Option<Key> = call_contract(pointer.clone(), &args, &Vec::new());
-    let sub_key = maybe_sub_key.unwrap();
+    match call_contract(pointer.clone(), &args, &Vec::new()){
+        Some(sub_key) => {
+            let key_name = "mail_feed";
+            add_uref(key_name, &sub_key);
+            if sub_key != get_uref(key_name){
+                revert(1);
+            }
 
+            let method = "pub";
+            let message = "Hello, World!";
+            let args = (method, message);
+            let _result: () = call_contract(pointer, &args, &Vec::new());
 
-    let key_name = "mail_feed";
-    add_uref(key_name, &sub_key);
-    assert_eq!(sub_key, get_uref(key_name));
+            let list_key: UPointer<Vec<String>> = sub_key.to_u_ptr().unwrap();
+            let messages = read(list_key);
 
-
-    let method = "pub";
-    let message = "Hello, World!";
-    let args = (method, message);
-    let _result: () = call_contract(pointer, &args, &Vec::new());
-
-    let list_key: UPointer<Vec<String>> = sub_key.to_u_ptr().unwrap();
-    let messages = read(list_key);
-
-    assert_eq!(
-        vec![String::from("Welcome!"), String::from(message)],
-        messages
-    );
+            if  messages.is_empty(){
+                revert(2);
+            }
+        },
+        None=>{
+            revert(3);
+        }
+    }
 }
