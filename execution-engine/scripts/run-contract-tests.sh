1976ad4795c03340ff25a31239eb373d6562a3fa
Michał Papierski <michal@casperlabs.io>
2019-07-02 14:22:30 +0200

    EE-414: Allow filtering of contract tests.
    Usage:
    
        ./scripts/run-contract-test.sh test1 test2 test3
    
    This will pass `test1` `test2` and `test3` as parameters into `cargo
    test` command.

--- execution-engine/scripts/run-contract-tests.sh
+++ execution-engine/scripts/run-contract-tests.sh
@@ -24,4 +24,4 @@ for CONTRACT in "${CONTRACTS[@]}"; do
     cargo build -p "${CONTRACT}" --target wasm32-unknown-unknown
 done
 
-cargo test -p casperlabs-engine-grpc-server -- --ignored --nocapture
+cargo test -p casperlabs-engine-grpc-server -- --ignored --nocapture $@
