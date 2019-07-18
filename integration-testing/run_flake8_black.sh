ee277adf1e890818f7f301d518d47c77ab095ec6
Srinivas Reddy Thatiparthy <srini@casperlabs.io>
2019-07-19 01:58:44 +0530

    OP-437: Add black, flake8, End of file, debug statement checks via pre-commit hooks.
    * Whenever you commit, pre-commit hooks check for black, flake8, and et.al violations
    in your code. If you disabled the check on your local computer by using '--no-verify',
    this PR should catch them in drone.

---
+++ integration-testing/run_flake8_black.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+python3 -m pip install pipenv
+pipenv sync
+pipenv run pre-commit install
+pipenv run pre-commit run --files $(git diff --relative --name-only $DRONE_BRANCH..$DRONE_SOURCE_BRANCH)
+if [[ $? -eq 0 ]]; then
+    exit 0
+else
+    exit 1
+fi
