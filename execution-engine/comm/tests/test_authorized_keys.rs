3594d4f363a945b96d97b257ce22c86f0a1c29d7
Michał Papierski <michal@casperlabs.io>
2019-07-31 23:28:07 +0200

    EE-526: Add a test case for cumulative keys weight

--- execution-engine/comm/tests/test_authorized_keys.rs
+++ execution-engine/comm/tests/test_authorized_keys.rs
@@ -10,9 +10,10 @@ mod test_support;
 
 use std::collections::HashMap;
 
-use common::value::account::PublicKey;
+use common::value::account::{PublicKey, Weight};
 use execution_engine::engine_state::error;
-use test_support::WasmTestBuilder;
+use execution_engine::execution;
+use test_support::{WasmTestBuilder, DEFAULT_BLOCK_TIME};
 
 const GENESIS_ADDR: [u8; 32] = [7u8; 32];
 
@@ -25,9 +26,9 @@ fn should_deploy_with_authorized_keys() {
         .exec_with_args_and_keys(
             GENESIS_ADDR,
             "authorized_keys.wasm",
-            1,  // blocktime
-            1,  // nonce
-            (), //args
+            1,                                // blocktime
+            1,                                // nonce
+            (Weight::new(1), Weight::new(1)), //args
             vec![PublicKey::new(GENESIS_ADDR)],
         )
         .commit()
@@ -46,9 +47,9 @@ fn should_raise_auth_failure_with_invalid_keys() {
         .exec_with_args_and_keys(
             GENESIS_ADDR,
             "authorized_keys.wasm",
-            1,  // blocktime
-            1,  // nonce
-            (), //args
+            1,                                // blocktime
+            1,                                // nonce
+            (Weight::new(1), Weight::new(1)), //args
             vec![PublicKey::new(key_1)],
         )
         .commit()
@@ -67,4 +68,79 @@ fn should_raise_auth_failure_with_invalid_keys() {
     let message = deploy_result.get_precondition_failure().get_message();
 
     assert_eq!(message, format!("{}", error::Error::AuthorizationFailure))
+}
+
+#[ignore]
+#[test]
+fn should_raise_deploy_with_insufficient_weight() {
+    // tests that authorized keys needs sufficient cumulative weight
+    let key_1 = [254; 32];
+    assert_ne!(GENESIS_ADDR, key_1);
+    // Basic deploy with single key
+    let result1 = WasmTestBuilder::default()
+        .run_genesis(GENESIS_ADDR, HashMap::new())
+        // Reusing a test contract that would add new key
+        .exec_with_args(
+            GENESIS_ADDR,
+            "add_update_associated_key.wasm",
+            DEFAULT_BLOCK_TIME,
+            1,
+            PublicKey::new(key_1),
+        )
+        .expect_success()
+        .commit()
+        // This should execute successfuly - change deploy and key management
+        // thresholds.
+        .exec_with_args_and_keys(
+            GENESIS_ADDR,
+            "authorized_keys.wasm",
+            DEFAULT_BLOCK_TIME,
+            2, // nonce
+            // Deploy threshold is equal to 3, keymgmnt is still 1.
+            // Even after verifying weights and thresholds to not
+            // lock out the account, those values should work as
+            // account now has 1. identity key with weight=1 and
+            // a key with weight=2.
+            (Weight::new(3), Weight::new(4)), //args
+            vec![PublicKey::new(GENESIS_ADDR)],
+        )
+        .expect_success()
+        .commit()
+        .finish();
+
+    // With deploy threshold == 3 using single secondary key
+    // with weight == 2 should raise deploy authorization failure.
+    let result2 = WasmTestBuilder::from_result(result1)
+        .exec_with_args_and_keys(
+            GENESIS_ADDR,
+            "authorized_keys.wasm",
+            DEFAULT_BLOCK_TIME,
+            3, // nonce
+            // Deploy threshold is equal to 3, keymgmnt is still 1
+            (Weight::new(3), Weight::new(4)), //args
+            vec![PublicKey::new(key_1)],
+        )
+        .commit()
+        .finish();
+
+    let deploy_result = result2
+        .builder()
+        .get_exec_response(0)
+        .expect("should have exec response")
+        .get_success()
+        .get_deploy_results()
+        .get(0)
+        .expect("should have at least one deploy result");
+
+    assert!(deploy_result.has_execution_result());
+    let execution_result = deploy_result.get_execution_result();
+    assert!(execution_result.has_error());
+    let error = execution_result.get_error();
+    assert!(error.has_exec_error());
+    let exec_error = error.get_exec_error();
+
+    assert_eq!(
+        exec_error.get_message(),
+        format!("{}", execution::Error::DeploymentAuthorizationFailure)
+    )
 }
--- execution-engine/contracts/test/authorized-keys/src/lib.rs
+++ execution-engine/contracts/test/authorized-keys/src/lib.rs
@@ -3,11 +3,14 @@
 
 extern crate alloc;
 extern crate cl_std;
+use cl_std::contract_api::{get_arg, revert, set_action_threshold};
+use cl_std::value::account::{ActionType, Weight};
 
 #[no_mangle]
 pub extern "C" fn call() {
-    // NOTE: To verify that the authorized deploys are working, no special
-    // logic is necessary in the context.
-    // Once contract API gain support for listing authorized keys we could
-    // add them here.
+    let deploy_threshold: Weight = get_arg(0);
+    let key_management_threshold: Weight = get_arg(1);
+    set_action_threshold(ActionType::KeyManagement, key_management_threshold)
+        .unwrap_or_else(|_| revert(100));
+    set_action_threshold(ActionType::Deployment, deploy_threshold).unwrap_or_else(|_| revert(200));
 }
