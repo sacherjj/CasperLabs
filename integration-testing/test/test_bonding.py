475fbe31b68d5a94689a8155ec9c1f9f41d92cd8
Srinivas Reddy Thatiparthy <srini@casperlabs.io>
2019-07-31 14:22:45 +0530

    OP-394: Add a test case to unbond a validator which was not bonded to a network

--- integration-testing/test/test_bonding.py
+++ integration-testing/test/test_bonding.py
@@ -1,7 +1,7 @@
 from test.cl_node.casperlabsnode import BONDING_CONTRACT, UNBONDING_CONTRACT
 from test.cl_node.client_parser import parse_show_block
 from test.cl_node.client_parser import parse_show_blocks
-from test.cl_node.casperlabs_network import  OneNodeNetwork
+from test.cl_node.casperlabs_network import OneNodeNetwork
 
 
 def bond_to_the_network(network: OneNodeNetwork, contract: str, bond_amount: int):
@@ -43,7 +43,8 @@ def test_bonding(one_node_network):
     public_key = node1.from_address
     item = list(
         filter(
-            lambda x: x.stake == bonding_amount and x.validator_public_key == public_key,
+            lambda x: x.stake == bonding_amount
+            and x.validator_public_key == public_key,
             block_ds.summary[0].header[0].state[0].bonds,
         )
     )
@@ -57,9 +58,7 @@ def test_invalid_bonding(one_node_network):
     """
     # 190 is current total staked amount.
     bonding_amount = (190 * 1000) + 1
-    block_hash = bond_to_the_network(
-        one_node_network, BONDING_CONTRACT, bonding_amount
-    )
+    block_hash = bond_to_the_network(one_node_network, BONDING_CONTRACT, bonding_amount)
     assert block_hash is not None
     node1 = one_node_network.docker_nodes[1]
     block1 = node1.client.show_block(block_hash)
@@ -97,7 +96,8 @@ def test_unbonding(one_node_network):
     block_ds = parse_show_block(block2)
     item = list(
         filter(
-            lambda x: x.stake == bonding_amount and x.validator_public_key == public_key,
+            lambda x: x.stake == bonding_amount
+            and x.validator_public_key == public_key,
             block_ds.summary[0].header[0].state[0].bonds,
         )
     )
@@ -117,7 +117,7 @@ def test_invalid_unbonding(one_node_network):
     block_hash2 = node1.unbond(
         session_contract=UNBONDING_CONTRACT,
         payment_contract=UNBONDING_CONTRACT,
-        maybe_amount=1985,
+        maybe_amount=1985,  # 1985 > (2000+190) * 0.9
     )
 
     assert block_hash2 is not None
@@ -125,8 +125,39 @@ def test_invalid_unbonding(one_node_network):
     block_ds = parse_show_block(block2)
     item = list(
         filter(
-            lambda x: x.stake == bonding_amount and x.validator_public_key == node1.from_address,
+            lambda x: x.stake == bonding_amount
+            and x.validator_public_key == node1.from_address,
             block_ds.summary[0].header[0].state[0].bonds,
         )
     )
     assert len(item) == 1
+
+
+def test_unbonding_without_bonding(one_node_network):
+    """
+    Feature file: consensus.feature
+    Scenario: unbonding a validator node which was not bonded to an existing network.
+    """
+    bonding_amount = 1
+    assert_pre_state_of_network(one_node_network, bonding_amount)
+    one_node_network.add_new_node_to_network()
+    assert len(one_node_network.docker_nodes) == 2, "Total number of nodes should be 2."
+    node1 = one_node_network.docker_nodes[1]
+    public_key = node1.from_address
+    block_hash2 = node1.unbond(
+        session_contract=UNBONDING_CONTRACT,
+        payment_contract=UNBONDING_CONTRACT,
+        maybe_amount=None,
+    )
+
+    assert block_hash2 is not None
+    block2 = node1.client.show_block(block_hash2)
+    block_ds = parse_show_block(block2)
+    item = list(
+        filter(
+            lambda x: x.stake == bonding_amount
+            and x.validator_public_key == public_key,
+            block_ds.summary[0].header[0].state[0].bonds,
+        )
+    )
+    assert len(item) == 0
