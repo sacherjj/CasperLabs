2df594eca8a6c491906cf5ab2462f9a81084fb6c
Srinivas Reddy Thatiparthy <srini@casperlabs.io>
2019-08-05 17:23:44 +0530

    OP-437: Build is not able to get the diff files. Make the build retrieve pull request files by creating a temp branch.

--- integration-testing/run_flake8_black.sh
+++ integration-testing/run_flake8_black.sh
@@ -4,7 +4,7 @@ set -e
 python3 -m pip install pipenv
 pipenv sync
 pipenv run pre-commit install
-pipenv run pre-commit run --files $(git diff --relative --name-only $DRONE_BRANCH..$DRONE_SOURCE_BRANCH)
+pipenv run pre-commit run --files $(git diff --relative --name-only $DRONE_BRANCH..temp)
 if [[ $? -eq 0 ]]; then
     exit 0
 else
