068c07684656fdb747afbce98b6d968e42e9f49a
birchmd <birchmd@casperlabs.io>
2019-08-06 16:32:34 -0400

    EE-543: PoS finalize_payment function

--- execution-engine/contracts/system/pos/src/error.rs
+++ execution-engine/contracts/system/pos/src/error.rs
@@ -20,9 +20,12 @@ pub enum Error {
     StakesNotFound,
     PaymentPurseNotFound,
     PaymentPurseKeyUnexpectedType,
+    PaymentPurseBalanceNotFound,
     BondingPurseNotFound,
     BondingPurseKeyUnexpectedType,
     RefundPurseKeyUnexpectedType,
+    RewardsPurseNotFound,
+    RewardsPurseKeyUnexpectedType,
     // TODO: Put these in their own enum, and wrap them separately in `BondingError` and
     // `UnbondingError`.
     QueueNotStoredAsByteArray,
@@ -30,6 +33,10 @@ pub enum Error {
     QueueDeserializationExtraBytes,
     StakesKeyDeserializationFailed,
     StakesDeserializationFailed,
+    SystemFunctionCalledByUserAccount,
+    InsufficientPaymentForAmountSpent,
+    FailedTransferToRewardsPurse,
+    FailedTransferToAccountPurse,
 }
 
 pub type Result<T> = result::Result<T, Error>;
@@ -51,14 +58,21 @@ impl Into<u32> for Error {
             Error::StakesNotFound => 0x100 + 1,
             Error::PaymentPurseNotFound => 0x100 + 2,
             Error::PaymentPurseKeyUnexpectedType => 0x100 + 3,
-            Error::BondingPurseNotFound => 0x100 + 4,
-            Error::BondingPurseKeyUnexpectedType => 0x100 + 5,
-            Error::RefundPurseKeyUnexpectedType => 0x100 + 6,
+            Error::PaymentPurseBalanceNotFound => 0x100 + 4,
+            Error::BondingPurseNotFound => 0x100 + 5,
+            Error::BondingPurseKeyUnexpectedType => 0x100 + 6,
+            Error::RefundPurseKeyUnexpectedType => 0x100 + 7,
+            Error::RewardsPurseNotFound => 0x100 + 8,
+            Error::RewardsPurseKeyUnexpectedType => 0x100 + 9,
             Error::QueueNotStoredAsByteArray => 0x200,
             Error::QueueDeserializationFailed => 0x200 + 1,
             Error::QueueDeserializationExtraBytes => 0x200 + 2,
             Error::StakesKeyDeserializationFailed => 0x300,
             Error::StakesDeserializationFailed => 0x300 + 1,
+            Error::SystemFunctionCalledByUserAccount => 0x400,
+            Error::InsufficientPaymentForAmountSpent => 0x400 + 1,
+            Error::FailedTransferToRewardsPurse => 0x400 + 2,
+            Error::FailedTransferToAccountPurse => 0x400 + 3,
         }
     }
 }
@@ -90,6 +104,13 @@ impl PurseLookupError {
         match err {
             PurseLookupError::KeyNotFound => Error::PaymentPurseNotFound,
             PurseLookupError::KeyUnexpectedType => Error::PaymentPurseKeyUnexpectedType,
+        }
+    }
+
+    pub fn rewards(err: PurseLookupError) -> Error {
+        match err {
+            PurseLookupError::KeyNotFound => Error::RewardsPurseNotFound,
+            PurseLookupError::KeyUnexpectedType => Error::RewardsPurseKeyUnexpectedType,
         }
     }
 }
