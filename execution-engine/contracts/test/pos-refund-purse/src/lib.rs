d0cdf6b6262fe47ba7a550e4161c7dc9ff74ee63
birchmd <birchmd@casperlabs.io>
2019-08-06 11:49:20 -0400

    EE-542: Tests for PoS set_refund_purse and related functions

---
+++ execution-engine/contracts/test/pos-refund-purse/src/lib.rs
@@ -0,0 +1,90 @@
+#![no_std]
+#![feature(alloc)]
+
+#[macro_use]
+extern crate alloc;
+extern crate cl_std;
+
+use alloc::vec::Vec;
+
+use cl_std::contract_api;
+use cl_std::contract_api::pointers::{ContractPointer, UPointer};
+use cl_std::key::Key;
+use cl_std::value::account::PurseId;
+
+fn purse_to_key(p: &PurseId) -> Key {
+    Key::URef(p.value())
+}
+
+fn set_refund_purse(pos: &ContractPointer, p: &PurseId) {
+    contract_api::call_contract::<_, ()>(
+        pos.clone(),
+        &("set_refund_purse", *p),
+        &vec![purse_to_key(p)],
+    );
+}
+
+fn unset_refund_purse(pos: &ContractPointer) {
+    contract_api::call_contract::<_, ()>(pos.clone(), &"unset_refund_purse", &Vec::new());
+}
+
+fn get_refund_purse(pos: &ContractPointer) -> Option<PurseId> {
+    contract_api::call_contract(pos.clone(), &"get_refund_purse", &Vec::new())
+}
+
+fn create_purse(mint: &ContractPointer) -> PurseId {
+    contract_api::call_contract(mint.clone(), &"create", &Vec::new())
+}
+
+#[no_mangle]
+pub extern "C" fn call() {
+    let pos_public: UPointer<Key> = contract_api::get_uref("pos").unwrap().to_u_ptr().unwrap();
+    let pos_contract: Key = contract_api::read(pos_public);
+    let pos_pointer = pos_contract.to_c_ptr().unwrap();
+
+    let mint_public: UPointer<Key> = contract_api::get_uref("mint").unwrap().to_u_ptr().unwrap();
+    let mint_contract: Key = contract_api::read(mint_public);
+    let mint_pointer = mint_contract.to_c_ptr().unwrap();
+
+    let p1 = create_purse(&mint_pointer);
+    let p2 = create_purse(&mint_pointer);
+
+    // get_refund_purse should return None before setting it
+    let refund_result = get_refund_purse(&pos_pointer);
+    if refund_result.is_some() {
+        contract_api::revert(1);
+    }
+
+    // it should return Some(x) after calling set_refund_purse(x)
+    set_refund_purse(&pos_pointer, &p1);
+    let refund_purse = match get_refund_purse(&pos_pointer) {
+        None => contract_api::revert(2),
+        Some(x) if x.value().addr() == p1.value().addr() => x.value(),
+        Some(_) => contract_api::revert(3),
+    };
+
+    // the returned purse should not have any access rights
+    if refund_purse.is_addable() || refund_purse.is_writeable() || refund_purse.is_readable() {
+        contract_api::revert(4)
+    }
+
+    // get_refund_purse should return correct value after setting a second time
+    set_refund_purse(&pos_pointer, &p2);
+    match get_refund_purse(&pos_pointer) {
+        None => contract_api::revert(5),
+        Some(x) if x.value().addr() == p2.value().addr() => (),
+        Some(_) => contract_api::revert(6),
+    }
+
+    // it should return None again after calling unset_refund_purse
+    unset_refund_purse(&pos_pointer);
+    if get_refund_purse(&pos_pointer).is_some() {
+        contract_api::revert(7);
+    }
+
+    // unset_refund_purse is idempotent
+    unset_refund_purse(&pos_pointer);
+    if get_refund_purse(&pos_pointer).is_some() {
+        contract_api::revert(8);
+    }
+}
