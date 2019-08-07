927165dcad142b8192eedc8c2fd29b8c44fc7336
Srinivas Reddy Thatiparthy <srini@casperlabs.io>
2019-08-07 12:02:45 +0530

    OP-437: The following files are ignored from running flake8, and black. * Files which are generated by protoc compiler for protocol buffers. * Files which are generated by command 'python setup.py install/build' and stored in build folder.

--- integration-testing/run_flake8_black.sh
+++ integration-testing/run_flake8_black.sh
@@ -4,7 +4,7 @@ set -e
 python3 -m pip install pipenv
 pipenv sync
 pipenv run pre-commit install
-pipenv run pre-commit run --files $(git diff --relative --name-only $DRONE_BRANCH..temp)
+pipenv run pre-commit run --files $(find . -type f -name \*.py | grep -v "ignore_test_.*.py\|.*_pb2.py\|.*_pb2_grpc.py\|CasperClient\/build\/lib\/")
 if [[ $? -eq 0 ]]; then
     exit 0
 else
