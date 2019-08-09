9ba47d5b6fa49edd16f367a8b3ff7df101ebeaff
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2019-08-09 13:29:48 -0400

    Tests functioning, but need updates for key mgmt failure.

--- integration-testing/test/test_key_management.py
+++ integration-testing/test/test_key_management.py
@@ -94,9 +94,13 @@ def assert_deploy_is_not_error(node, block_hash):
     assert not deploys[0].is_error, deploys[0].error_message
 
 
-def assert_deploy_is_error(node, block_hash):
+def assert_deploy_is_error(node,
+                           block_hash: str,
+                           error_message: str = None):
     deploys = list(node.client.show_deploys(block_hash))
     assert deploys[0].is_error
+    if error_message:
+        assert deploys[0].error_message == error_message
 
 
 def test_key_management(one_node_network):
@@ -132,7 +136,7 @@ def test_key_management(one_node_network):
     # Update identity key to 30, using other key
     block_hash = update_associated_key(
         node,
-        identity_key=key_mgmt_key.public_key_hex,
+        identity_key=identity_key.public_key_hex,
         weight_key=key_mgmt_key,
         key=identity_key.public_key_hex,
         weight=30,
@@ -151,8 +155,8 @@ def test_key_management(one_node_network):
     # Deploy with weight under threshold
     block_hash = node.deploy_and_propose(
         from_address=identity_key.public_key_hex,
-        payment_contract=SET_THRESHOLDS_CONTRACT,
-        session_contract=SET_THRESHOLDS_CONTRACT,
+        payment_contract=HELLO_NAME_CONTRACT,
+        session_contract=HELLO_NAME_CONTRACT,
         public_key=deploy_key.public_key_path,
         private_key=deploy_key.private_key_path,
     )
@@ -171,8 +175,8 @@ def test_key_management(one_node_network):
     # Deploy with weight at threshold
     block_hash = node.deploy_and_propose(
         from_address=identity_key.public_key_hex,
-        payment_contract=SET_THRESHOLDS_CONTRACT,
-        session_contract=SET_THRESHOLDS_CONTRACT,
+        payment_contract=HELLO_NAME_CONTRACT,
+        session_contract=HELLO_NAME_CONTRACT,
         public_key=deploy_key.public_key_path,
         private_key=deploy_key.private_key_path,
     )
@@ -191,19 +195,20 @@ def test_key_management(one_node_network):
     # Deploy with weight under threshold
     block_hash = node.deploy_and_propose(
         from_address=identity_key.public_key_hex,
-        payment_contract=SET_THRESHOLDS_CONTRACT,
-        session_contract=SET_THRESHOLDS_CONTRACT,
+        payment_contract=HELLO_NAME_CONTRACT,
+        session_contract=HELLO_NAME_CONTRACT,
         public_key=deploy_key.public_key_path,
         private_key=deploy_key.private_key_path,
     )
-    assert_deploy_is_error(node, block_hash)
+    assert_deploy_is_error(node, block_hash, "DeploymentAuthorizationFailure")
 
     # Key management weight under threshold
-    block_hash = set_key_thresholds(
-        node,
-        identity_key=identity_key.public_key_hex,
-        weight_key=key_mgmt_key,
-        deploy_weight=11,
-        key_management_weight=21,
-    )
-    assert_deploy_is_error(node, block_hash)
+    with pytest.raises(Exception):
+        block_hash = set_key_thresholds(
+            node,
+            identity_key=identity_key.public_key_hex,
+            weight_key=key_mgmt_key,
+            deploy_weight=12,
+            key_management_weight=22,
+        )
+        assert_deploy_is_error(node, block_hash)
