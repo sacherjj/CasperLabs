c5c70273c3876a66c8fe7399f81b05e66f34b61a
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2019-08-12 14:49:34 -0400

    Black and lint corrections.

--- integration-testing/test/cl_node/graphql.py
+++ integration-testing/test/cl_node/graphql.py
@@ -4,17 +4,16 @@ import logging
 
 
 class GraphQL:
-
     def __init__(self, node):
         self.node = node
 
     @property
     def url(self) -> str:
-        server = self.node.container_name if self.node.is_in_docker else '0.0.0.0'
-        return f'http://{server}:{self.node.http_port}/graphql'
+        server = self.node.container_name if self.node.is_in_docker else "0.0.0.0"
+        return f"http://{server}:{self.node.http_port}/graphql"
 
     def query(self, query_json: dict) -> dict:
-        logging.info(f'GraphQL Query to {self.url} with {query_json}')
+        logging.info(f"GraphQL Query to {self.url} with {query_json}")
         r = requests.post(url=self.url, json=query_json)
         return json.loads(r.text)
 
@@ -43,22 +42,22 @@ class GraphQL:
         :return: dict of response
         """
         if sub_select is None:
-            sub_select = '''blockHash
+            sub_select = """blockHash
                             parentHashes
                             deployCount
                             deployErrorCount
-                            blockSizeBytes'''
-        q_json = {"query":
-                      f'''{{ 
-                        block(blockHashBase16Prefix: "{block_hash_prefix_hex}") 
-                        {{ {sub_select} }}    
-                      }}'''
-                  }
+                            blockSizeBytes"""
+        q_json = {
+            "query": f"""{{
+                        block(blockHashBase16Prefix: "{block_hash_prefix_hex}")
+                        {{ {sub_select} }}
+                      }}"""
+        }
         return self.query(q_json)
 
     def query_deploy(self, deploy_hash_hex: str, sub_select: str = None) -> dict:
         if sub_select is None:
-            sub_select = '''deploy {
+            sub_select = """deploy {
                                 deployHash
                                 accountId
                                 timestamp
@@ -71,12 +70,12 @@ class GraphQL:
                                 cost
                                 isError
                                 errorMessage
-                            }'''
-        q_json = {'query':
-                      f'''{{ deploy(deployHashBase16: "{deploy_hash_hex}") 
+                            }"""
+        q_json = {
+            "query": f"""{{ deploy(deployHashBase16: "{deploy_hash_hex}")
                         {{ {sub_select} }}
-                      }}'''
-                  }
+                      }}"""
+        }
         return self.query(q_json)
 
     # TODO: Figure out proper ValueUnion! type to use for sub query
--- integration-testing/test/test_graph_ql.py
+++ integration-testing/test/test_graph_ql.py
@@ -1,25 +1,24 @@
-import requests
-
 from test.cl_node.common import extract_deploy_hash_from_deploy_output
-from test.cl_node.casperlabsnode import extract_block_hash_from_propose_output
+from test.cl_node.common import extract_block_hash_from_propose_output
 
 
 def test_graphql(one_node_network):
     node = one_node_network.docker_nodes[0]
-    deploy_output = node.client.deploy(session_contract='test_helloname.wasm',
-                                       payment_contract='test_helloname.wasm')
+    deploy_output = node.client.deploy(
+        session_contract="test_helloname.wasm", payment_contract="test_helloname.wasm"
+    )
     deploy_hash = extract_deploy_hash_from_deploy_output(deploy_output)
     propose_output = node.client.propose()
     block_hash = extract_block_hash_from_propose_output(propose_output)
 
     block_dict = node.graphql.query_block(block_hash)
-    block = block_dict['data']['block']
-    assert block['blockHash'] == block_hash
-    assert block['deployCount'] == 1
-    assert block['deployErrorCount'] == 0
+    block = block_dict["data"]["block"]
+    assert block["blockHash"] == block_hash
+    assert block["deployCount"] == 1
+    assert block["deployErrorCount"] == 0
 
     deploy_dict = node.graphql.query_deploy(deploy_hash)
-    deploy = deploy_dict['data']['deploy']['deploy']
-    processing_results = deploy_dict['data']['deploy']['processingResults']
-    assert deploy['deployHash'] == deploy_hash
-    assert processing_results[0]['block']['blockHash'] == block_hash
+    deploy = deploy_dict["data"]["deploy"]["deploy"]
+    processing_results = deploy_dict["data"]["deploy"]["processingResults"]
+    assert deploy["deployHash"] == deploy_hash
+    assert processing_results[0]["block"]["blockHash"] == block_hash
