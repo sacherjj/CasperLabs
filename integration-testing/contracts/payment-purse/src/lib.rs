47ed65ac8c2b3e46326ad49fb7a41983464864c4
Piotr Kuchta <piotr@casperlabs.io>
2019-08-14 23:41:01 +0200

    OP-228: Add test contracts from Srini's branch

---
+++ integration-testing/contracts/payment-purse/src/lib.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![feature(alloc)]
+
+extern crate alloc;
+extern crate cl_std;
+
+use alloc::vec::Vec;
+use cl_std::value::account::PublicKey;
+use cl_std::contract_api::pointers::UPointer;
+use cl_std::contract_api::{self, PurseTransferResult};
+use cl_std::key::Key;
+use cl_std::value::account::PurseId;
+use cl_std::value::uint::U512;
+
+#[no_mangle]
+pub extern "C" fn call() {
+    let pos_public: UPointer<Key> = contract_api::get_uref("pos").unwrap().to_u_ptr().unwrap();
+    let pos_contract: Key = contract_api::read(pos_public);
+    let pos_pointer = pos_contract.to_c_ptr().unwrap();
+
+    let source_purse = contract_api::main_purse();
+    let account: PublicKey = contract_api::get_arg(0);
+    let payment_amount: U512 = U512::from(contract_api::get_arg::<u32>(1));
+    let payment_purse: PurseId =
+        contract_api::call_contract(pos_pointer, &"get_payment_purse", &Vec::new());
+
+    // can deposit
+    if let PurseTransferResult::TransferError =
+        contract_api::transfer_from_purse_to_purse(source_purse, payment_purse, payment_amount)
+    {
+        contract_api::revert(1);
+    }
+
+    let payment_balance = match contract_api::get_balance(payment_purse) {
+        Some(amount) => amount,
+        None => contract_api::revert(2),
+    };
+
+    if payment_balance != payment_amount {
+        contract_api::revert(3)
+    }
+
+    // cannot withdraw
+    if let PurseTransferResult::TransferSuccessful =
+        contract_api::transfer_from_purse_to_purse(payment_purse, source_purse, payment_amount)
+    {
+        contract_api::revert(4);
+    }
+}
---
+++ integration-testing/contracts/standard-payment/src/lib.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![feature(alloc)]
+
+#[macro_use]
+extern crate alloc;
+extern crate contract_ffi;
+
+use contract_ffi::contract_api::pointers::UPointer;
+use contract_ffi::contract_api::{self, PurseTransferResult};
+use contract_ffi::key::Key;
+use contract_ffi::value::account::PurseId;
+use contract_ffi::value::U512;
+
+const POS_CONTRACT_NAME: &str = "pos";
+const GET_PAYMENT_PURSE: &str = "get_payment_purse";
+
+#[no_mangle]
+pub extern "C" fn call() {
+    // let amount = contract_api::get_arg(0);
+    let amount: U512 = 10_000_000.into();
+
+    let main_purse = contract_api::main_purse();
+
+    let pos_public: UPointer<Key> = contract_api::get_uref(POS_CONTRACT_NAME)
+        .and_then(contract_ffi::key::Key::to_u_ptr)
+        .unwrap_or_else(|| contract_api::revert(66));
+
+    let pos_contract = contract_api::read(pos_public)
+        .to_c_ptr()
+        .unwrap_or_else(|| contract_api::revert(67));
+
+    let payment_purse: PurseId =
+        contract_api::call_contract(pos_contract, &(GET_PAYMENT_PURSE), &vec![]);
+
+    if let PurseTransferResult::TransferError =
+        contract_api::transfer_from_purse_to_purse(main_purse, payment_purse, amount)
+    {
+        contract_api::revert(99);
+
+    }
+}
