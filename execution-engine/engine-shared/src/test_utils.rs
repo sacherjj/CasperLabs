a125d9bad1fb9983eb41a05904d44c86ba74be15
Henry Till <henrytill@gmail.com>
2019-08-14 19:56:07 -0400

    NO-TICKET: execution-engine: update shared's directory structure

--- execution-engine/engine-shared/src/test_utils.rs
+++ execution-engine/engine-shared/src/test_utils.rs
@@ -1,16 +1,21 @@
 //! Some functions to use in tests.
+use std::collections::btree_map::BTreeMap;
 
 use parity_wasm::builder::ModuleBuilder;
 use parity_wasm::elements::{MemorySection, MemoryType, Module, Section, Serialize};
 
-use contract_ffi::bytesrepr::{deserialize, FromBytes, ToBytes};
+use contract_ffi::bytesrepr::{self, FromBytes, ToBytes};
+use contract_ffi::key::Key;
+use contract_ffi::uref::{AccessRights, URef};
+use contract_ffi::value::account::PurseId;
+use contract_ffi::value::{Account, Value};
 
 /// Returns `true` if a we can serialize and then deserialize a value
 pub fn test_serialization_roundtrip<T>(t: &T) -> bool
 where
     T: ToBytes + FromBytes + PartialEq + std::fmt::Debug,
 {
-    match deserialize::<T>(&ToBytes::to_bytes(t).expect("Unable to serialize data"))
+    match bytesrepr::deserialize::<T>(&ToBytes::to_bytes(t).expect("Unable to serialize data"))
         .map(|r| r == *t)
         .ok()
     {
@@ -28,4 +33,11 @@ pub fn create_empty_wasm_module_bytes() -> Vec<u8> {
     let mut wasm_bytes = vec![];
     parity_module.serialize(&mut wasm_bytes).unwrap();
     wasm_bytes
+}
+
+/// Returns an account value paired with its key
+pub fn mocked_account(account_addr: [u8; 32]) -> Vec<(Key, Value)> {
+    let purse_id = PurseId::new(URef::new([0u8; 32], AccessRights::READ_ADD_WRITE));
+    let account = Account::create(account_addr, BTreeMap::new(), purse_id);
+    vec![(Key::Account(account_addr), Value::Account(account))]
 }
