aa3ddf047be7807239d05124bc05a05742c158f4
birchmd <birchmd@casperlabs.io>
2019-08-19 13:15:38 -0400

    EE-549: ToBytes/FromBytes implementations for execution::Phase

--- execution-engine/contract-ffi/src/execution/mod.rs
+++ execution-engine/contract-ffi/src/execution/mod.rs
@@ -1,6 +1,38 @@
-#[derive(PartialEq, Eq, Clone, Copy)]
+use crate::bytesrepr::{Error, FromBytes, ToBytes};
+use alloc::vec::Vec;
+
+const PAYMENT_ID: u8 = 0;
+const SESSION_ID: u8 = 1;
+const FINALIZE_PAYMENT_ID: u8 = 2;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Phase {
     Payment,
     Session,
     FinalizePayment,
+}
+
+impl ToBytes for Phase {
+    fn to_bytes(&self) -> Result<Vec<u8>, Error> {
+        let id = match self {
+            Phase::Payment => PAYMENT_ID,
+            Phase::Session => SESSION_ID,
+            Phase::FinalizePayment => FINALIZE_PAYMENT_ID,
+        };
+
+        Ok(vec![id])
+    }
+}
+
+impl FromBytes for Phase {
+    fn from_bytes(bytes: &[u8]) -> Result<(Self, &[u8]), Error> {
+        let (id, rest): (u8, &[u8]) = FromBytes::from_bytes(bytes)?;
+
+        match id {
+            PAYMENT_ID => Ok((Phase::Payment, rest)),
+            SESSION_ID => Ok((Phase::Session, rest)),
+            FINALIZE_PAYMENT_ID => Ok((Phase::FinalizePayment, rest)),
+            _ => Err(Error::FormattingError),
+        }
+    }
 }
