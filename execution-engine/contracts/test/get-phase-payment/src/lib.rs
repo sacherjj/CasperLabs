45b4d79775a646e1b2a5d593c871a1e187f3d4e0
birchmd <birchmd@casperlabs.io>
2019-08-19 15:31:13 -0400

    EE-549: Improve get_phase test to cover both payment and session

---
+++ execution-engine/contracts/test/get-phase-payment/src/lib.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![feature(alloc)]
+
+#[macro_use]
+extern crate alloc;
+extern crate cl_std;
+
+use cl_std::contract_api::pointers::UPointer;
+use cl_std::contract_api::{self, PurseTransferResult};
+use cl_std::execution::Phase;
+use cl_std::key::Key;
+use cl_std::value::account::PurseId;
+use cl_std::value::U512;
+
+const POS_CONTRACT_NAME: &str = "pos";
+const GET_PAYMENT_PURSE: &str = "get_payment_purse";
+
+fn standard_payment(amount: U512) {
+    let main_purse = contract_api::main_purse();
+
+    let pos_public: UPointer<Key> = contract_api::get_uref(POS_CONTRACT_NAME)
+        .and_then(Key::to_u_ptr)
+        .unwrap_or_else(|| contract_api::revert(66));
+
+    let pos_contract = contract_api::read(pos_public)
+        .to_c_ptr()
+        .unwrap_or_else(|| contract_api::revert(67));
+
+    let payment_purse: PurseId =
+        contract_api::call_contract(pos_contract, &(GET_PAYMENT_PURSE), &vec![]);
+
+    if let PurseTransferResult::TransferError =
+        contract_api::transfer_from_purse_to_purse(main_purse, payment_purse, amount)
+    {
+        contract_api::revert(99);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn call() {
+    let known_phase: Phase = contract_api::get_arg(0);
+    let get_pahse = contract_api::get_phase();
+    assert_eq!(
+        get_pahse, known_phase,
+        "get_phase did not return known_phase"
+    );
+
+    standard_payment(U512::from(10_000_000));
+}
--- execution-engine/engine-grpc-server/tests/test_get_phase.rs
+++ execution-engine/engine-grpc-server/tests/test_get_phase.rs
@@ -8,7 +8,9 @@ extern crate grpc;
 use std::collections::HashMap;
 
 use contract_ffi::execution::Phase;
-use test_support::{WasmTestBuilder, DEFAULT_BLOCK_TIME};
+use contract_ffi::value::account::PublicKey;
+use engine_core::engine_state::EngineConfig;
+use test_support::{DeployBuilder, ExecRequestBuilder, WasmTestBuilder};
 
 #[allow(dead_code)]
 mod test_support;
@@ -18,15 +20,25 @@ const GENESIS_ADDR: [u8; 32] = [7u8; 32];
 #[ignore]
 #[test]
 fn should_run_get_phase_contract() {
-    WasmTestBuilder::default()
-        .run_genesis(GENESIS_ADDR, HashMap::new())
-        .exec_with_args(
-            GENESIS_ADDR,
-            "get_phase.wasm",
-            DEFAULT_BLOCK_TIME,
-            1,
-            Phase::Session,
-        )
+    let genesis_public_key = PublicKey::new(GENESIS_ADDR);
+
+    let engine_config = EngineConfig::new().set_use_payment_code(true);
+
+    let exec_request = {
+        let deploy = DeployBuilder::new()
+            .with_address(GENESIS_ADDR)
+            .with_session_code("get_phase.wasm", Phase::Session)
+            .with_payment_code("get_phase_payment.wasm", Phase::Payment)
+            .with_authorization_keys(&[genesis_public_key])
+            .with_nonce(1)
+            .build();
+
+        ExecRequestBuilder::new().push_deploy(deploy).build()
+    };
+
+    WasmTestBuilder::new(engine_config)
+        .run_genesis(GENESIS_ADDR, HashMap::default())
+        .exec_with_exec_request(exec_request)
         .commit()
         .expect_success();
 }
