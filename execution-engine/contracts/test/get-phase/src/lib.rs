9ec9df840e869d7c0a698aa3f295c4135793d01a
birchmd <birchmd@casperlabs.io>
2019-08-21 13:28:27 -0400

    EE-549: Fix typo

--- execution-engine/contracts/test/get-phase/src/lib.rs
+++ execution-engine/contracts/test/get-phase/src/lib.rs
@@ -9,9 +9,9 @@ use cl_std::execution::Phase;
 #[no_mangle]
 pub extern "C" fn call() {
     let known_phase: Phase = contract_api::get_arg(0);
-    let get_pahse = contract_api::get_phase();
+    let get_phase = contract_api::get_phase();
     assert_eq!(
-        get_pahse, known_phase,
+        get_phase, known_phase,
         "get_phase did not return known_phase"
     );
 }
