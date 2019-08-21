3b969f579158c69b31b5439eaa0dfc5f65448c95
Abner Zheng <abnerzheng@gmail.com>
2019-08-21 10:41:18 +0800

    CON-359: update comment

--- casper/src/test/scala/io/casperlabs/casper/finality/FinalityDetectorByVotingMatrixTest.scala
+++ casper/src/test/scala/io/casperlabs/casper/finality/FinalityDetectorByVotingMatrixTest.scala
@@ -157,6 +157,8 @@ class FinalityDetectorByVotingMatrixTest
         /* The DAG looks like:
          *
          *
+         *    b7 ----
+         *           \
          *            b6
          *           /
          *        b5
