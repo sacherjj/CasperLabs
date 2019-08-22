a36fff0416cd97b489d3f54a565e8bafa25ac8a2
Akosh Farkash <aakoshh@gmail.com>
2019-08-22 15:08:13 +0100

    NODE-579: Remove motes_transferred from Rust tests.

--- execution-engine/engine-grpc-server/tests/test_support.rs
+++ execution-engine/engine-grpc-server/tests/test_support.rs
@@ -108,7 +108,6 @@ impl DeployBuilder {
 impl Default for DeployBuilder {
     fn default() -> Self {
         let mut deploy = Deploy::new();
-        deploy.set_motes_transferred_in_payment(1_000_000_000);
         deploy.set_gas_price(1);
         DeployBuilder { deploy }
     }
@@ -181,7 +180,6 @@ pub fn get_protocol_version() -> ProtocolVersion {
 pub fn get_mock_deploy() -> Deploy {
     let mut deploy = Deploy::new();
     deploy.set_address(MOCKED_ACCOUNT_ADDRESS.to_vec());
-    deploy.set_motes_transferred_in_payment(1000);
     deploy.set_gas_price(1);
     deploy.set_nonce(1);
     let mut deploy_code = DeployCode::new();
