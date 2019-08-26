e972e635075a683de8215690185081a7489d2d80
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2019-08-26 12:04:03 -0400

    Adding nonce back into protobuf object call.

--- integration-testing/client/CasperLabsClient/casperlabs_client/casperlabs_client.py
+++ integration-testing/client/CasperLabsClient/casperlabs_client/casperlabs_client.py
@@ -346,6 +346,7 @@ class CasperLabsClient:
 
         header = consensus.Deploy.Header(
             account_public_key=account_public_key,
+            nonce=0,  # Not used, but still required in interface currently.
             timestamp=int(time.time()),
             gas_price=gas_price,
             body_hash=hash(serialize(body)),
