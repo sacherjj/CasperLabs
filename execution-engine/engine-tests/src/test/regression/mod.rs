4f48492a498d83c883f1cf4fe65995a3eb4eaaa4
Ed Hastings <ed@casperlabs.io>
2019-08-28 14:46:52 -0700

    EE-558: namespacing tests

---
+++ execution-engine/engine-tests/src/test/regression/mod.rs
@@ -0,0 +1,22 @@
+#[cfg(test)]
+mod ee_221;
+#[cfg(test)]
+mod ee_401;
+#[cfg(test)]
+mod ee_441;
+#[cfg(test)]
+mod ee_460;
+#[cfg(test)]
+mod ee_468;
+#[cfg(test)]
+mod ee_470;
+#[cfg(test)]
+mod ee_532;
+#[cfg(test)]
+mod ee_536;
+#[cfg(test)]
+mod ee_539;
+#[cfg(test)]
+mod ee_549;
+#[cfg(test)]
+mod ee_572;
