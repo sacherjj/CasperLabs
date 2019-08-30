9ae60af55594fad9e17959c29ebd3d119f165e31
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-08-30 22:27:58 +0200

    NO-TICKET: Remove validation on the nonce. It's not checked anywhere now.

--- client/src/main/scala/io/casperlabs/client/configuration/Options.scala
+++ client/src/main/scala/io/casperlabs/client/configuration/Options.scala
@@ -76,9 +76,7 @@ final case class Options(arguments: Seq[String]) extends ScallopConf(arguments) 
 
     val nonce = opt[Long](
       descr = "This allows you to overwrite your own pending transactions that use the same nonce.",
-      validate = _ > 0,
-      required = false,
-      default = Some(2L)
+      required = false
     )
 
     val session =
@@ -157,9 +155,7 @@ final case class Options(arguments: Seq[String]) extends ScallopConf(arguments) 
 
     val nonce = opt[Long](
       descr = "This allows you to overwrite your own pending transactions that use the same nonce.",
-      validate = _ > 1,
-      required = false,
-      default = Some(2L)
+      required = false
     )
 
     val session =
@@ -317,9 +313,7 @@ final case class Options(arguments: Seq[String]) extends ScallopConf(arguments) 
     val nonce = opt[Long](
       descr =
         "Nonce of the account. Sequences deploys from that account. Every new deploy has to use nonce one higher than current account's nonce.",
-      validate = _ > 0,
-      required = false,
-      default = Some(2L)
+      required = false
     )
 
     val privateKey =
@@ -358,9 +352,7 @@ final case class Options(arguments: Seq[String]) extends ScallopConf(arguments) 
     val nonce = opt[Long](
       descr =
         "Nonce of the account. Sequences deploys from that account. Every new deploy has to use nonce one higher than current account's nonce.",
-      validate = _ > 0,
-      required = false,
-      default = Some(2L)
+      required = false
     )
 
     val privateKey =
@@ -399,9 +391,7 @@ final case class Options(arguments: Seq[String]) extends ScallopConf(arguments) 
     val nonce = opt[Long](
       descr =
         "Nonce of the account. Sequences deploys from that account. Every new deploy has to use nonce one higher than current account's nonce.",
-      validate = _ > 0,
-      required = false,
-      default = Some(2L)
+      required = false
     )
 
     val privateKey =
