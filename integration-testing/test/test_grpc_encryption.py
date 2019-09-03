ce3a16a2d14b9f5354bdcc81d4d6eba1bd382e9b
Piotr Kuchta <piotr@casperlabs.io>
2019-09-03 20:05:22 +0200

    NODE-781: Add --node-id. Guard against "Casper instance not available yet' with wait_for_genesis_block.

--- integration-testing/test/test_grpc_encryption.py
+++ integration-testing/test/test_grpc_encryption.py
@@ -4,8 +4,8 @@ from test.cl_node.cli import DockerCLI, CLI
 from casperlabs_client import CasperLabsClient, extract_common_name
 
 
-def test_grpc_encryption_scala_cli(encrypted_one_node_network):
-    node = encrypted_one_node_network.docker_nodes[0]
+def test_grpc_encryption_scala_cli(encrypted_two_node_network):
+    node = encrypted_two_node_network.docker_nodes[0]
     cli = DockerCLI(
         node,
         tls_parameters={
@@ -16,14 +16,14 @@ def test_grpc_encryption_scala_cli(encrypted_one_node_network):
     logging.debug(f"{blocks}")
 
 
-def test_grpc_encryption_python_lib(encrypted_one_node_network):
-    node = encrypted_one_node_network.docker_nodes[0]
+def test_grpc_encryption_python_lib(encrypted_two_node_network):
+    node = encrypted_two_node_network.docker_nodes[0]
     host = os.environ.get("TAG_NAME", None) and node.container_name or "localhost"
     client = CasperLabsClient(
         host,
         node.grpc_external_docker_port,
         node.grpc_internal_docker_port,
-        extract_common_name(node.config.tls_certificate_local_path()),
+        None,  # extract_common_name(node.config.tls_certificate_local_path()),
         node.config.tls_certificate_local_path(),
     )
     blocks = list(client.showBlocks(1))
@@ -31,13 +31,13 @@ def test_grpc_encryption_python_lib(encrypted_one_node_network):
     logging.debug(f"{blocks}")
 
 
-def test_grpc_encryption_python_cli(encrypted_one_node_network):
-    node = encrypted_one_node_network.docker_nodes[0]
+def test_grpc_encryption_python_cli(encrypted_two_node_network):
+    nodes = encrypted_two_node_network.docker_nodes
     cli = CLI(
-        node,
+        nodes[0],
         tls_parameters={
-            "--certificate-file": node.config.tls_certificate_local_path(),
-            "--node-id": extract_common_name(node.config.tls_certificate_local_path()),
+            "--certificate-file": nodes[0].config.tls_certificate_local_path(),
+            # "--node-id": extract_common_name(nodes[0].config.tls_certificate_local_path()),
         },
     )
     blocks = cli("show-blocks", "--depth", 1)
