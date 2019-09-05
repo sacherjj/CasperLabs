f7104027f3997b282137c1d0dd1ccb308170cde8
Akosh Farkash <aakoshh@gmail.com>
2019-09-05 16:16:04 +0100

    NODE-831: Fix deploy buffer test.

--- casper/src/test/scala/io/casperlabs/casper/deploybuffer/DeployBufferSpec.scala
+++ casper/src/test/scala/io/casperlabs/casper/deploybuffer/DeployBufferSpec.scala
@@ -317,9 +317,9 @@ trait DeployBufferSpec
               .groupBy(_.getHeader.accountPublicKey)
               .mapValues(_.minBy(_.getHeader.timestamp))
               .values
-              .toSet
+              .map(_.deployHash)
             got <- db.readAccountPendingOldest().compile.toList
-          } yield got.toSet shouldBe expected
+          } yield got should contain theSameElementsAs expected
         }
       }
     }
