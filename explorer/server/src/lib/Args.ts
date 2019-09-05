8e1417b143dba8b91a8a5f1886572d90dc932720
Akosh Farkash <aakoshh@gmail.com>
2019-09-05 15:23:48 +0100

    ECO-103: Pass payment to the faucet.

--- explorer/server/src/lib/Args.ts
+++ explorer/server/src/lib/Args.ts
@@ -1,4 +1,5 @@
 import { Deploy } from "../grpc/io/casperlabs/casper/consensus/consensus_pb";
+import { BigInt } from "../grpc/io/casperlabs/casper/consensus/state_pb";
 
 // Functions to convert data to protobuf Deploy.Arg
 
@@ -14,6 +15,13 @@ function toValue<T>(set: (value: Deploy.Arg.Value, x: T) => void): ToValue<T> {
 
 export const BytesValue = toValue<ByteArray>((value, x) => value.setBytesValue(x));
 export const LongValue = toValue<bigint>((value, x) => value.setLongValue(Number(x)));
+export const BigIntValue = toValue<bigint>((value, x) => {
+  const bi = new BigInt();
+  bi.setBitWidth(512);
+  bi.setValue(x.toString());
+  value.setBigInt(bi);
+  return value;
+});
 
 export function Args(...args: Array<[string, Deploy.Arg.Value]>): Deploy.Arg[] {
   return args.map((x) => {
