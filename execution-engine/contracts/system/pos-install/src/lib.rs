2b26ae4ad4c5cc04ffafd921d335612ec68d4c91
Michael Birch <birchmd@casperlabs.io>
2019-09-07 11:13:51 -0400

    EE-653: add bonded validators to known_urefs; add bonding purse to known_urefs

--- execution-engine/contracts/system/pos-install/src/lib.rs
+++ execution-engine/contracts/system/pos-install/src/lib.rs
@@ -12,11 +12,14 @@ use contract_ffi::contract_api::pointers::{ContractPointer, UPointer};
 use contract_ffi::key::Key;
 use contract_ffi::system_contracts::mint;
 use contract_ffi::uref::{AccessRights, URef};
-use contract_ffi::value::account::PurseId;
+use contract_ffi::value::account::{PublicKey, PurseId};
 use contract_ffi::value::U512;
+use core::fmt::Write;
 
+const POS_BONDING_PURSE: &str = "pos_bonding_purse";
 const POS_PAYMENT_PURSE: &str = "pos_payment_purse";
 const POS_REWARDS_PURSE: &str = "pos_rewards_purse";
+const MINT_NAME: &str = "mint";
 
 #[repr(u32)]
 enum Error {
@@ -26,6 +29,7 @@ enum Error {
 #[repr(u32)]
 enum Args {
     MintURef = 0,
+    GenesisValidators = 1,
 }
 
 #[no_mangle]
@@ -38,18 +42,48 @@ pub extern "C" fn call() {
     let mint_uref: URef = contract_api::get_arg(Args::MintURef as u32);
     let mint = ContractPointer::URef(UPointer::new(mint_uref.addr(), AccessRights::READ));
 
-    // TODO: set up bonded validators and bonding purse
+    let genesis_validators: BTreeMap<PublicKey, U512> =
+        contract_api::get_arg(Args::GenesisValidators as u32);
 
+    // Add genesis validators to PoS contract object.
+    // For now, we are storing validators in `known_urefs` map of the PoS contract
+    // in the form: key: "v_{validator_pk}_{validator_stake}", value: doesn't
+    // matter.
+    let mut known_urefs: BTreeMap<String, Key> = genesis_validators
+        .iter()
+        .map(|(pub_key, balance)| {
+            let key_bytes = pub_key.value();
+            let mut hex_key = String::with_capacity(64);
+            for byte in &key_bytes[..32] {
+                write!(hex_key, "{:02x}", byte).unwrap();
+            }
+            let mut uref = String::new();
+            uref.write_fmt(format_args!("v_{}_{}", hex_key, balance))
+                .unwrap();
+            uref
+        })
+        .map(|key| (key, Key::Hash([0u8; 32])))
+        .collect();
+
+    // Include the mint contract in its known_urefs
+    known_urefs.insert(String::from(MINT_NAME), Key::URef(mint_uref));
+
+    let total_bonds: U512 = genesis_validators.values().fold(U512::zero(), |x, y| x + y);
+
+    let bonding_purse = mint_purse(&mint, total_bonds);
     let payment_purse = mint_purse(&mint, U512::zero());
     let rewards_purse = mint_purse(&mint, U512::zero());
 
-    let known_urefs: BTreeMap<String, Key> = [
+    // Include PoS purses in its known_urefs
+    [
+        (POS_BONDING_PURSE, bonding_purse.value()),
         (POS_PAYMENT_PURSE, payment_purse.value()),
         (POS_REWARDS_PURSE, rewards_purse.value()),
     ]
     .iter()
-    .map(|(name, uref)| (String::from(*name), Key::URef(*uref)))
-    .collect();
+    .for_each(|(name, uref)| {
+        known_urefs.insert(String::from(*name), Key::URef(*uref));
+    });
 
     let contract = contract_api::fn_by_name("pos_ext", known_urefs);
     let uref: URef = contract_api::new_uref(contract).into();
