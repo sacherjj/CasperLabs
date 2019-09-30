5cc2f290a962212321ff6c8d7888efcc3d31a453
Michael Birch <birchmd@casperlabs.io>
2019-09-30 19:54:12 -0400

    NODE-894: Include ROWID in deploy_headers table
    Co-Authored-By: Akosh Farkash <aakoshh@gmail.com>

--- storage/src/main/resources/db/migration/V20190927_894__Add_deploy_dependencies.sql
+++ storage/src/main/resources/db/migration/V20190927_894__Add_deploy_dependencies.sql
@@ -5,7 +5,7 @@ CREATE TABLE deploy_headers
     timestamp_millis     INTEGER          NOT NULL,
     header               BLOB             NOT NULL,
     FOREIGN KEY (hash) REFERENCES deploys (hash)
-) WITHOUT ROWID;
+);
 
 CREATE INDEX idx_deploy_headers_account ON deploy_headers (account);
 CREATE INDEX idx_deploy_headers_timestamps_millis ON deploy_headers (timestamp_millis);
