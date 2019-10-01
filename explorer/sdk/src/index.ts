65db91b57572090fedd0cee6a00bf2b399f72099
Abner Zheng <abnerzheng@gmail.com>
2019-10-01 21:21:54 +0800

    ECO-127: add index.ts for directory lib and run test before publish.

--- explorer/sdk/src/index.ts
+++ explorer/sdk/src/index.ts
@@ -1,12 +1,7 @@
-import * as Args from './lib/Args';
-import * as Contracts from './lib/Contracts';
-import * as Keys from './lib/Keys';
-import * as Serialization from './lib/Serialization';
-
 export type ByteArray = Uint8Array;
 export type DeployHash = ByteArray;
 export type BlockHash = ByteArray;
 
-export { CasperService, BalanceService, GrpcError } from './services';
-export { Args, Contracts, Keys, Serialization};
+export * from './services';
+export * from './lib';
 export {base64to16, encodeBase16, decodeBase16, encodeBase64, decodeBase64} from './lib/Conversions'
