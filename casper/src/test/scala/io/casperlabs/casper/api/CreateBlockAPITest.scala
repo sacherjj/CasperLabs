8e0e5348fd355a8fc890bb00698e83586f7957ea
Akosh Farkash <aakoshh@gmail.com>
2019-10-22 16:44:44 +0100

    NODE-902: Test that BlockAPI.propose protects create and add.

--- casper/src/test/scala/io/casperlabs/casper/api/CreateBlockAPITest.scala
+++ casper/src/test/scala/io/casperlabs/casper/api/CreateBlockAPITest.scala
@@ -16,6 +16,7 @@ import io.casperlabs.casper.consensus.info.DeployInfo.ProcessingResult
 import io.casperlabs.casper.helper.{GossipServiceCasperTestNodeFactory, HashSetCasperTestNode}
 import io.casperlabs.casper.helper.BlockUtil.generateValidator
 import io.casperlabs.casper.util._
+import io.casperlabs.casper.validation.Validation
 import io.casperlabs.catscontrib.TaskContrib._
 import io.casperlabs.crypto.codec.Base16
 import io.casperlabs.crypto.signatures.SignatureAlgorithm.Ed25519
@@ -41,7 +42,7 @@ class CreateBlockAPITest extends FlatSpec with Matchers with GossipServiceCasper
     casper.validation.raiseValidateErrorThroughApplicativeError[Task]
   implicit val logEff = new LogStub[Task]
 
-  implicit val validation = HashSetCasperTestNode.makeValidation[Task]
+  implicit val validation: Validation[Task] = HashSetCasperTestNode.makeValidation[Task]
 
   private val (validatorKeys, validators)                      = (1 to 4).map(_ => Ed25519.newKeyPair).unzip
   private val bonds                                            = createBonds(validators)
@@ -97,6 +98,60 @@ class CreateBlockAPITest extends FlatSpec with Matchers with GossipServiceCasper
       response1.isRight shouldBe true
       response2.isRight shouldBe false
       response2.left.get.getMessage should include("ABORTED")
+    } finally {
+      node.tearDown()
+    }
+  }
+
+  "propose" should "handle concurrent calls and not create equivocations" in {
+    val threadCount        = 3
+    implicit val scheduler = Scheduler.fixedPool("propose-threads", threadCount)
+    // Similarly to integration tests, start multiple concurrent processes that
+    // try to deploy and immediately propose. The proposals should be rejected
+    // when another one is already running, but eventually all deploys should be
+    // taken and put into blocks. In the blocks we created should form a simple
+    // chain, there shouldn't be any forks in it, which we should see from the
+    // fact that each rank is occupied by a single block.
+    val node = standaloneEff(genesis, transforms, validatorKeys.head)
+
+    implicit val bs = node.blockStorage
+    implicit val fd = node.safetyOracleEff
+
+    def deployAndPropose(
+        blockApiLock: Semaphore[Task]
+    )(implicit casperRef: MultiParentCasperRef[Task]) =
+      for {
+        d <- ProtoUtil.basicDeploy[Task]()
+        _ <- BlockAPI.deploy[Task](d)
+        _ <- BlockAPI.propose[Task](blockApiLock)
+      } yield ()
+
+    val test: Task[Unit] = for {
+      casperRef    <- MultiParentCasperRef.of[Task]
+      _            <- casperRef.set(node.casperEff)
+      blockApiLock <- Semaphore[Task](1)
+      // Create blocks in waves.
+      results <- Task.sequence {
+                  List.fill(5) {
+                    Task.gatherUnordered {
+                      List.fill(threadCount) {
+                        deployAndPropose(blockApiLock)(casperRef).attempt
+                      }
+                    }
+                  }
+                }
+      // See how many blocks were created.
+      dag    <- node.dagStorage.getRepresentation
+      blocks <- dag.topoSortTail(Int.MaxValue).compile.toList.map(_.flatten)
+    } yield {
+      val successCount  = results.flatten.count(_.isRight)
+      val blocksPerRank = blocks.groupBy(_.getHeader.rank)
+      blocks should have size (1L + successCount.toLong)
+      blocksPerRank should have size (blocks.size.toLong)
+    }
+
+    try {
+      test.unsafeRunSync
     } finally {
       node.tearDown()
     }
