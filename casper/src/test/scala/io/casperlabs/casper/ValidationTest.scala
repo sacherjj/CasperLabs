eb359133b983444aa025a24a78cf42b67c0fa2b4
Mateusz Górski <gorski.mateusz@protonmail.ch>
2019-10-25 15:33:36 +0200

    CON-555: Remove `println` from the test.

--- casper/src/test/scala/io/casperlabs/casper/ValidationTest.scala
+++ casper/src/test/scala/io/casperlabs/casper/ValidationTest.scala
@@ -783,16 +783,12 @@ class ValidationTest
         dag <- dagStorage.getRepresentation
         // v3 hasn't seen v2 equivocating (in contrast to what "local" node saw).
         // It will choose C as a main parent and A as a secondary one.
-        r <- Validation[Task]
+        _ <- Validation[Task]
               .parents(d, genesis.blockHash, dag)
               .map(_.parents.map(_.blockHash))
-              .attempt
-
-        _ = log.all.foreach(println)
-
-        _ = r shouldBe Right(
-          Vector(c.blockHash, a.blockHash)
-        )
+              .attempt shouldBeF Right(
+              Vector(c.blockHash, a.blockHash)
+            )
         // While v0 has seen everything so it will use 0 as v2's weight when scoring.
         _ <- Validation[Task]
               .parents(e, genesis.blockHash, dag)
