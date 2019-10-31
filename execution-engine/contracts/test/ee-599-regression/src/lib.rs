1541e7af08379301d1a4595378ea837d43d40cbf
Michał Papierski <michal@casperlabs.io>
2019-10-31 12:51:26 +0100

    EE-745: Add test for transferring users funds.
    This test shows that the contract can transfer users funds from his main
    purse by executing possibly malicious contract. All three methods
    (transfer_to_account, transfer_from_purse_to_purse,
    transfer_from_purse_to_account) aren't forbidding this behaviour.

---
+++ execution-engine/contracts/test/ee-599-regression/src/lib.rs
@@ -0,0 +1,159 @@
+#![no_std]
+
+extern crate alloc;
+extern crate contract_ffi;
+
+use alloc::collections::BTreeMap;
+use alloc::string::String;
+use alloc::vec::Vec;
+use contract_ffi::contract_api::{account, runtime, storage, system, Error};
+use contract_ffi::key::Key;
+use contract_ffi::unwrap_or_revert::UnwrapOrRevert;
+use contract_ffi::value::account::{PublicKey, PurseId};
+use contract_ffi::value::U512;
+
+const DONATION_AMOUNT: u64 = 1;
+// Different name just to make sure any routine that deals with named keys coming from different
+// sources wouldn't overlap (if ever that's possible)
+const DONATION_BOX_COPY: &str = "donation_box_copy";
+const DONATION_BOX: &str = "donation_box";
+const MAINTAINER: &str = "maintainer";
+const TRANSFER_FROM_PURSE_TO_ACCOUNT: &str = "transfer_from_purse_to_account";
+const TRANSFER_FROM_PURSE_TO_PURSE: &str = "transfer_from_purse_to_purse";
+const TRANSFER_FUNDS_EXT: &str = "transfer_funds_ext";
+const TRANSFER_FUNDS_KEY: &str = "transfer_funds";
+const TRANSFER_TO_ACCOUNT: &str = "transfer_to_account";
+
+enum DelegateArg {
+    Method = 0,
+    ContractKey,
+    SubContractMethodFwd,
+}
+
+enum TransferFunds {
+    Method = 0,
+}
+
+#[repr(u16)]
+enum ContractError {
+    InvalidTransferFundsMethod = 0,
+    InvalidDelegateMethod = 1,
+}
+
+impl Into<Error> for ContractError {
+    fn into(self) -> Error {
+        Error::User(self as u16)
+    }
+}
+
+fn get_maintainer_public_key() -> Result<PublicKey, Error> {
+    // Obtain maintainer address from the contract's named keys
+    let maintainer_key = runtime::get_key(MAINTAINER).ok_or(Error::GetKey)?;
+    maintainer_key
+        .as_account()
+        .ok_or(Error::UnexpectedKeyVariant)
+        .map(PublicKey::new)
+}
+
+fn get_donation_box_purse() -> Result<PurseId, Error> {
+    let donation_box_key = runtime::get_key(DONATION_BOX).ok_or(Error::GetKey)?;
+    donation_box_key
+        .as_uref()
+        .cloned()
+        .ok_or(Error::UnexpectedKeyVariant)
+        .map(PurseId::new)
+}
+
+/// This method is possibly ran from a context of a different user than the initial deployer
+fn transfer_funds() -> Result<(), Error> {
+    let method: String = runtime::get_arg(TransferFunds::Method as u32)
+        .ok_or(Error::MissingArgument)?
+        .map_err(|_| Error::InvalidArgument)?;
+
+    // Caller's main purse (different from the owner of the contract)
+    let main_purse = account::get_main_purse();
+    // Donation box is the purse funds will be transferred into
+    let donation_box_purse = get_donation_box_purse()?;
+    // This is the address of account which installed the contract
+    let maintainer_public_key = get_maintainer_public_key()?;
+
+    match method.as_str() {
+        TRANSFER_FROM_PURSE_TO_PURSE => system::transfer_from_purse_to_purse(
+            main_purse,
+            donation_box_purse,
+            U512::from(DONATION_AMOUNT),
+        )?,
+        TRANSFER_FROM_PURSE_TO_ACCOUNT => {
+            let _transferred_to = system::transfer_from_purse_to_account(
+                main_purse,
+                maintainer_public_key,
+                U512::from(DONATION_AMOUNT),
+            )?;
+        }
+        TRANSFER_TO_ACCOUNT => {
+            let _transferred_to =
+                system::transfer_to_account(maintainer_public_key, U512::from(DONATION_AMOUNT))?;
+        }
+        _ => return Err(ContractError::InvalidTransferFundsMethod.into()),
+    }
+
+    Ok(())
+}
+
+#[no_mangle]
+fn transfer_funds_ext() {
+    transfer_funds().unwrap_or_revert();
+}
+
+/// Registers a function and saves it in callers named keys
+fn delegate() -> Result<(), Error> {
+    let method: String = runtime::get_arg(DelegateArg::Method as u32)
+        .ok_or(Error::MissingArgument)?
+        .map_err(|_| Error::InvalidArgument)?;
+    match method.as_str() {
+        "install" => {
+            // Create a malicious purse that should be known to the contract regardless of the
+            // calling context still owned by the account that deploys the contract
+            let donation_box = system::create_purse();
+            let maintainer = runtime::get_caller();
+            // Keys below will make it possible to use within the called contract
+            let known_keys = {
+                let mut keys = BTreeMap::new();
+                // "donation_box" is the purse owner of the contract can transfer funds from callers
+                keys.insert(DONATION_BOX.into(), donation_box.value().into());
+                // "maintainer" is the person who installed this contract
+                keys.insert(MAINTAINER.into(), Key::Account(maintainer.value()));
+                keys
+            };
+            // Install the contract with associated owner-related keys
+            let contract_ref = storage::store_function_at_hash(TRANSFER_FUNDS_EXT, known_keys);
+            runtime::put_key(TRANSFER_FUNDS_KEY, &contract_ref.into());
+            // For easy access in outside world here `donation_box` purse is also attached
+            // to the account
+            runtime::put_key(DONATION_BOX_COPY, &donation_box.value().into());
+        }
+        "call" => {
+            // This comes from outside i.e. after deploying the contract, this key is queried, and
+            // then passed into the call
+            let contract_key: Key = runtime::get_arg(DelegateArg::ContractKey as u32)
+                .ok_or(Error::MissingArgument)?
+                .map_err(|_| Error::InvalidArgument)?;
+            let contract_ref = contract_key.to_c_ptr().ok_or(Error::UnexpectedKeyVariant)?;
+            // This is a method that's gets forwarded into the sub contract
+            let subcontract_method: String =
+                runtime::get_arg(DelegateArg::SubContractMethodFwd as u32)
+                    .ok_or(Error::MissingArgument)?
+                    .map_err(|_| Error::InvalidArgument)?;
+
+            let subcontract_args = (subcontract_method,);
+            runtime::call_contract::<_, ()>(contract_ref, &subcontract_args, &Vec::new());
+        }
+        _ => return Err(ContractError::InvalidDelegateMethod.into()),
+    }
+    Ok(())
+}
+
+#[no_mangle]
+pub extern "C" fn call() {
+    delegate().unwrap_or_revert();
+}
---
+++ execution-engine/engine-tests/src/test/regression/ee_599.rs
@@ -0,0 +1,311 @@
+use crate::support::test_support::{self, ExecuteRequestBuilder, InMemoryWasmTestBuilder};
+use crate::test::{DEFAULT_ACCOUNT_ADDR, DEFAULT_GENESIS_CONFIG, DEFAULT_PAYMENT};
+use contract_ffi::value::account::{PublicKey, PurseId};
+use contract_ffi::value::U512;
+use engine_core::engine_state::CONV_RATE;
+use engine_shared::motes::Motes;
+
+const CONTRACT_EE_599_REGRESSION: &str = "ee_599_regression.wasm";
+const CONTRACT_TRANSFER_TO_ACCOUNT: &str = "transfer_to_account.wasm";
+const DONATION_BOX_COPY_KEY: &str = "donation_box_copy";
+const TRANSFER_FUNDS_KEY: &str = "transfer_funds";
+const DONATION_AMOUNT: u64 = 1;
+const VICTIM_ADDR: [u8; 32] = [42; 32];
+
+#[ignore]
+#[test]
+fn should_not_be_able_to_transfer_funds_with_transfer_purse_to_purse() {
+    let victim_initial_funds = DEFAULT_PAYMENT.as_u64() * 10;
+
+    // Creates victim account
+    let exec_request_1 = {
+        let args = (PublicKey::new(VICTIM_ADDR), victim_initial_funds);
+        ExecuteRequestBuilder::standard(DEFAULT_ACCOUNT_ADDR, CONTRACT_TRANSFER_TO_ACCOUNT, args)
+            .build()
+    };
+
+    // Deploy contract
+    let exec_request_2 = {
+        let args = ("install".to_string(),);
+        ExecuteRequestBuilder::standard(DEFAULT_ACCOUNT_ADDR, CONTRACT_EE_599_REGRESSION, args)
+            .build()
+    };
+
+    let result = InMemoryWasmTestBuilder::default()
+        .run_genesis(&DEFAULT_GENESIS_CONFIG)
+        .exec(exec_request_1)
+        .expect_success()
+        .commit()
+        .exec(exec_request_2)
+        .expect_success()
+        .commit()
+        .finish();
+
+    let victim_account = result
+        .builder()
+        .get_account(VICTIM_ADDR)
+        .expect("should have victim account");
+    let victim_purse_id = victim_account.purse_id();
+
+    let default_account = result
+        .builder()
+        .get_account(DEFAULT_ACCOUNT_ADDR)
+        .expect("should have default account");
+    let steal_funds = default_account
+        .named_keys()
+        .get(TRANSFER_FUNDS_KEY)
+        .cloned()
+        .unwrap_or_else(|| panic!("should have {}", TRANSFER_FUNDS_KEY));
+    let donation_box_copy_key = default_account
+        .named_keys()
+        .get(DONATION_BOX_COPY_KEY)
+        .cloned()
+        .unwrap_or_else(|| panic!("should have {}", DONATION_BOX_COPY_KEY));
+
+    let donation_box_copy_uref = donation_box_copy_key.as_uref().expect("should be uref");
+    let donation_box_copy = PurseId::new(*donation_box_copy_uref);
+
+    let exec_request_3 = {
+        let args = (
+            "call".to_string(),
+            steal_funds,
+            "transfer_from_purse_to_purse",
+        );
+        ExecuteRequestBuilder::standard(VICTIM_ADDR, CONTRACT_EE_599_REGRESSION, args).build()
+    };
+
+    let result_2 = InMemoryWasmTestBuilder::from_result(result)
+        .exec(exec_request_3)
+        .expect_success()
+        .commit()
+        .finish();
+
+    let victim_balance = result_2.builder().get_purse_balance(victim_purse_id);
+
+    let exec_3_response = result_2
+        .builder()
+        .get_exec_response(0)
+        .expect("should have response");
+
+    let gas_cost = Motes::from_gas(test_support::get_exec_costs(&exec_3_response)[0], CONV_RATE)
+        .expect("should convert");
+
+    assert_eq!(
+        victim_balance,
+        U512::from(victim_initial_funds) - gas_cost.value() - DONATION_AMOUNT,
+    );
+
+    assert_eq!(
+        result_2.builder().get_purse_balance(donation_box_copy),
+        U512::from(DONATION_AMOUNT),
+    );
+}
+
+#[ignore]
+#[test]
+fn should_not_be_able_to_transfer_funds_with_transfer_from_purse_to_account() {
+    let victim_initial_funds = DEFAULT_PAYMENT.as_u64() * 10;
+
+    // Creates victim account
+    let exec_request_1 = {
+        let args = (PublicKey::new(VICTIM_ADDR), victim_initial_funds);
+        ExecuteRequestBuilder::standard(DEFAULT_ACCOUNT_ADDR, CONTRACT_TRANSFER_TO_ACCOUNT, args)
+            .build()
+    };
+
+    // Deploy contract
+    let exec_request_2 = {
+        let args = ("install".to_string(),);
+        ExecuteRequestBuilder::standard(DEFAULT_ACCOUNT_ADDR, CONTRACT_EE_599_REGRESSION, args)
+            .build()
+    };
+
+    let result = InMemoryWasmTestBuilder::default()
+        .run_genesis(&DEFAULT_GENESIS_CONFIG)
+        .exec(exec_request_1)
+        .expect_success()
+        .commit()
+        .exec(exec_request_2)
+        .expect_success()
+        .commit()
+        .finish();
+
+    let victim_account = result
+        .builder()
+        .get_account(VICTIM_ADDR)
+        .expect("should have victim account");
+
+    let default_account = result
+        .builder()
+        .get_account(DEFAULT_ACCOUNT_ADDR)
+        .expect("should have default account");
+
+    let default_account_balance = result
+        .builder()
+        .get_purse_balance(default_account.purse_id());
+
+    let steal_funds = default_account
+        .named_keys()
+        .get(TRANSFER_FUNDS_KEY)
+        .cloned()
+        .unwrap_or_else(|| panic!("should have {}", TRANSFER_FUNDS_KEY));
+    let donation_box_copy_key = default_account
+        .named_keys()
+        .get(DONATION_BOX_COPY_KEY)
+        .cloned()
+        .unwrap_or_else(|| panic!("should have {}", DONATION_BOX_COPY_KEY));
+
+    let donation_box_copy_uref = donation_box_copy_key.as_uref().expect("should be uref");
+    let donation_box_copy = PurseId::new(*donation_box_copy_uref);
+
+    let exec_request_3 = {
+        let args = (
+            "call".to_string(),
+            steal_funds,
+            "transfer_from_purse_to_account",
+        );
+        ExecuteRequestBuilder::standard(VICTIM_ADDR, CONTRACT_EE_599_REGRESSION, args).build()
+    };
+
+    println!("main transfer");
+    let result_2 = InMemoryWasmTestBuilder::from_result(result)
+        .exec(exec_request_3)
+        .expect_success()
+        .commit()
+        .finish();
+
+    let victim_balance = result_2
+        .builder()
+        .get_purse_balance(victim_account.purse_id());
+
+    let exec_3_response = result_2
+        .builder()
+        .get_exec_response(0)
+        .expect("should have response");
+
+    let gas_cost = Motes::from_gas(test_support::get_exec_costs(&exec_3_response)[0], CONV_RATE)
+        .expect("should convert");
+
+    assert_eq!(
+        victim_balance,
+        U512::from(victim_initial_funds) - gas_cost.value() - DONATION_AMOUNT,
+    );
+
+    // In this variant of test `donation_box` is left unchanged i.e. zero balance
+    assert_eq!(
+        result_2.builder().get_purse_balance(donation_box_copy),
+        U512::zero(),
+    );
+
+    // Instead, main purse of the 'contract maintainer' is updated
+    let updated_default_account_balance = result_2
+        .builder()
+        .get_purse_balance(default_account.purse_id());
+
+    assert_eq!(
+        updated_default_account_balance - default_account_balance,
+        U512::from(DONATION_AMOUNT),
+    )
+}
+
+#[ignore]
+#[test]
+fn should_not_be_able_to_transfer_funds_with_transfer_to_account() {
+    let victim_initial_funds = DEFAULT_PAYMENT.as_u64() * 10;
+
+    // Creates victim account
+    let exec_request_1 = {
+        let args = (PublicKey::new(VICTIM_ADDR), victim_initial_funds);
+        ExecuteRequestBuilder::standard(DEFAULT_ACCOUNT_ADDR, CONTRACT_TRANSFER_TO_ACCOUNT, args)
+            .build()
+    };
+
+    // Deploy contract
+    let exec_request_2 = {
+        let args = ("install".to_string(),);
+        ExecuteRequestBuilder::standard(DEFAULT_ACCOUNT_ADDR, CONTRACT_EE_599_REGRESSION, args)
+            .build()
+    };
+
+    let result = InMemoryWasmTestBuilder::default()
+        .run_genesis(&DEFAULT_GENESIS_CONFIG)
+        .exec(exec_request_1)
+        .expect_success()
+        .commit()
+        .exec(exec_request_2)
+        .expect_success()
+        .commit()
+        .finish();
+
+    let victim_account = result
+        .builder()
+        .get_account(VICTIM_ADDR)
+        .expect("should have victim account");
+
+    let default_account = result
+        .builder()
+        .get_account(DEFAULT_ACCOUNT_ADDR)
+        .expect("should have default account");
+
+    let default_account_balance = result
+        .builder()
+        .get_purse_balance(default_account.purse_id());
+
+    let steal_funds = default_account
+        .named_keys()
+        .get(TRANSFER_FUNDS_KEY)
+        .cloned()
+        .unwrap_or_else(|| panic!("should have {}", TRANSFER_FUNDS_KEY));
+    let donation_box_copy_key = default_account
+        .named_keys()
+        .get(DONATION_BOX_COPY_KEY)
+        .cloned()
+        .unwrap_or_else(|| panic!("should have {}", DONATION_BOX_COPY_KEY));
+
+    let donation_box_copy_uref = donation_box_copy_key.as_uref().expect("should be uref");
+    let donation_box_copy = PurseId::new(*donation_box_copy_uref);
+
+    let exec_request_3 = {
+        let args = ("call".to_string(), steal_funds, "transfer_to_account");
+        ExecuteRequestBuilder::standard(VICTIM_ADDR, CONTRACT_EE_599_REGRESSION, args).build()
+    };
+
+    let result_2 = InMemoryWasmTestBuilder::from_result(result)
+        .exec(exec_request_3)
+        .expect_success()
+        .commit()
+        .finish();
+
+    let victim_balance = result_2
+        .builder()
+        .get_purse_balance(victim_account.purse_id());
+
+    let exec_3_response = result_2
+        .builder()
+        .get_exec_response(0)
+        .expect("should have response");
+
+    let gas_cost = Motes::from_gas(test_support::get_exec_costs(&exec_3_response)[0], CONV_RATE)
+        .expect("should convert");
+
+    assert_eq!(
+        victim_balance,
+        U512::from(victim_initial_funds) - gas_cost.value() - DONATION_AMOUNT,
+    );
+
+    // In this variant of test `donation_box` is left unchanged i.e. zero balance
+    assert_eq!(
+        result_2.builder().get_purse_balance(donation_box_copy),
+        U512::zero(),
+    );
+
+    // Instead, main purse of the 'contract maintainer' is updated
+    let updated_default_account_balance = result_2
+        .builder()
+        .get_purse_balance(default_account.purse_id());
+
+    assert_eq!(
+        updated_default_account_balance - default_account_balance,
+        U512::from(DONATION_AMOUNT),
+    )
+}
