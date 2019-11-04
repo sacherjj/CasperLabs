f5afae10d8aa55db719a19c72be9fa28dfaaa740
Abner Zheng <abnerzheng@gmail.com>
2019-11-04 19:26:46 +0800

    NODE-892: Add an environment variable "IN_DOCKER" so that when running integration tests in docker, we can use hostname to visit clarity.

--- integration-testing/test/test_clarity.py
+++ integration-testing/test/test_clarity.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 
 def test_clarity_running(one_node_network_with_clarity):
@@ -6,5 +7,22 @@ def test_clarity_running(one_node_network_with_clarity):
     Feature file: clarity.feature
     Scenario: Clarity is running.
     """
-    f = requests.get("http://localhost:8080")
-    assert f.text.find("<title>CasperLabs Clarity</title>") != -1
+    in_docker = os.getenv("IN_DOCKER") == "true"
+    if in_docker:
+        # If these integration tests are running in a docker container, then we need connect the docker container
+        # to the network of clarity
+        clarity_node = one_node_network_with_clarity.clarity_node
+        network_name = clarity_node.config.network
+        network = clarity_node.network_from_name(network_name)
+        # Gets name of container name of integration_testing
+        integration_test_node = os.getenv("HOSTNAME")
+        network.connect(integration_test_node)
+
+        host = f"http://{one_node_network_with_clarity.clarity_node.name}:8080"
+        content = requests.get(host).text
+        # Disconnect container from the network, so that we can prune the network
+        network.disconnect(integration_test_node)
+    else:
+        host = "http://127.0.0.1:8080"
+        content = requests.get(host).text
+    assert content.find("<title>CasperLabs Clarity</title>") != -1
