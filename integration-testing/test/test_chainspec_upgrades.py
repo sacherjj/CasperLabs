c3ade37f53b2275e27ac379bd9c9f1d3ca0a0cec
Piotr Kuchta <piotr@casperlabs.io>
2019-11-06 17:21:22 +0100

    NODE-945: Reformat test. Remove test-chainspec/genesis/accounts.csv, it is written by test framework while setting up modes.

--- integration-testing/test/test_chainspec_upgrades.py
+++ integration-testing/test/test_chainspec_upgrades.py
@@ -19,43 +19,29 @@ def propose_and_get_cost(cli):
     return deployInfo.cost
 
 
+# fmt: off
+
 def test_upgrades_applied(cli):
     account = cli.node.test_account
 
     cli.set_default_deploy_args(
-        "--from",
-        account.public_key_hex,
-        "--private-key",
-        cli.private_key_path(account),
-        "--public-key",
-        cli.public_key_path(account),
+        "--from", account.public_key_hex,
+        "--private-key", cli.private_key_path(account),
+        "--public-key", cli.public_key_path(account),
     )
-    cli(
-        "deploy",
-        "--payment-amount",
-        10000000,
-        "--session",
-        cli.resource(Contract.COUNTER_DEFINE),
-    )
+
+    cli("deploy", "--payment-amount", 10000000, "--session", cli.resource(Contract.COUNTER_DEFINE))
     propose_and_get_cost(cli)
 
-    cli(
-        "deploy",
-        "--payment-amount",
-        10000000,
-        "--session",
-        cli.resource(Contract.COUNTER_CALL),
-    )
+    cli("deploy", "--payment-amount", 10000000, "--session", cli.resource(Contract.COUNTER_CALL))
     cost = propose_and_get_cost(cli)
 
     for i in range(1, 35):
-        cli(
-            "deploy",
-            "--payment-amount",
-            10000000,
-            "--session",
-            cli.resource(Contract.COUNTER_CALL),
-        )
+        cli("deploy", "--payment-amount", 10000000, "--session", cli.resource(Contract.COUNTER_CALL))
         c = propose_and_get_cost(cli)
         if c != cost:
+            # TODO:
+            # When activation-point-rank of an upgrade is reached,
+            # and upgrade is executed,
+            # cost of execution should change
             raise Exception(f"Cost different at iteration {i}, was {cost}, now {c}. ")
