78e95eadcb64579e9bcba628cb5ffefe152b79d0
Piotr Kuchta <piotr@casperlabs.io>
2019-11-13 01:05:15 +0100

    NODE-988: Address review comments from @sacherjj

--- integration-testing/casperlabs_local_net/casperlabs_local_node.py
+++ integration-testing/casperlabs_local_net/casperlabs_local_node.py
@@ -1,29 +1,26 @@
+import os
 import time
 import docker
 import traceback
 from casperlabs_local_net.casperlabs_network import InterceptedOneNodeNetwork
-
-
-def prune_docker(docker_api):
-    docker_api.volumes.prune()
-    docker_api.networks.prune()
-    docker_api.containers.prune()
+from casperlabs_local_net.docker_cleanup_assurance import cleanup
 
 
 def main():
     docker_api = docker.from_env()
-    prune_docker(docker_api)
+    tag_name = os.environ.get("TAG_NAME") or "test"
+    cleanup(tag_name)
     network = InterceptedOneNodeNetwork(docker_api)
     try:
         network.create_cl_network()
         print("Node and proxy is running, press Ctrl-C to stop it.")
         node = network.docker_nodes[0]
-        logs = ""
+        read_logs_length = 0
         while True:
-            new_logs = node.logs()
-            if new_logs != logs:
-                print(new_logs[len(logs) :])
-                logs = new_logs
+            new_logs_length = len(node.logs())
+            if new_logs_length > read_logs_length:
+                read_logs_length = new_logs_length
+                print(node.logs()[read_logs_length:])
             time.sleep(0.1)
     except KeyboardInterrupt as e:
         print("\nControl-C was pressed")
@@ -31,5 +28,5 @@ def main():
         print(str(e))
     finally:
         print("Prune docker volumes, networks and containers...")
-        prune_docker(docker_api)
+        cleanup(tag_name)
         print("See you later!")
