a8773dfe6736f05af0cd1b76fca45dbb8efe7198
Piotr Kuchta <piotr@casperlabs.io>
2019-11-25 12:47:19 +0100

    NODE-1043: Add prototype of vdag

---
+++ integration-testing/vdag.py
@@ -0,0 +1,143 @@
+from collections import defaultdict
+import casperlabs_client
+
+# ~/CasperLabs/protobuf/io/casperlabs/node/api/casper.proto
+# ~/CasperLabs/protobuf/io/casperlabs/casper/consensus/info.proto
+# ~/CasperLabs/protobuf/io/casperlabs/casper/consensus/consensus.proto
+
+
+# DOT generation
+
+INVISIBLE = "dotted"
+
+
+def attributes(**kwargs):
+    return "[" + " ".join(f"{name}={value}" for (name, value) in kwargs.items()) + "]"
+
+
+def node(label, **kwargs):
+    return f'    "{label}" {attributes(**kwargs)}'
+
+
+def edge(l1, l2, **kwargs):
+    return f'    "{l1}" -> "{l2}" {attributes(**kwargs)}'
+
+
+def subgraph(*args, name=None, label=None):
+    return f"""  subgraph "{name}" {{
+    label = "{label}"
+{cat(*args)}
+  }}
+"""
+
+
+def graph(*args):
+    return f"""
+digraph "dag" {{
+  rankdir=BT
+  node [width=0 height=0 margin=0.03 fontsize=8]
+  splines=false
+{cat(*args)}
+}}
+"""
+
+
+def cat(*args):
+    return "\n".join(type(arg) == list and cat(*arg) or str(arg) for arg in args)
+
+
+# Protobuf BlockInfo abstraction/shortcuts.
+
+
+def justifications(block_info):
+    return [j.hex() for j in block_info.summary.header.justifications]
+
+
+def rank(block_info):
+    return block_info.summary.rank
+
+
+def parents(block_info):
+    ps = [h.hex() for h in block_info.summary.header.parent_hashes]
+    if len(ps) == 0:
+        return [""]
+    return ps
+
+
+def block_hash(block_info):
+    return block_info.summary.block_hash.hex()
+
+
+def short_hash(s: str) -> str:
+    return f"{s[:10]}..."
+
+
+def block_id(block_info) -> str:
+    return short_hash(block_hash(block_info))
+
+
+# DAG diagram construction.
+
+
+def lane(validator, block_infos):
+    validator_id = short_hash(validator)
+    nodes = [node(block_id(b)) for b in block_infos]
+
+    edges = [
+        edge(block_id(b), short_hash(parents(b)[0]), style="bold", constraint="false")
+        for b in block_infos
+    ]
+
+    alignment_edges = [
+        edge(block_id(block_infos[i]), block_id(block_infos[i + 1]), style=INVISIBLE)
+        for i in range(len(block_infos) - 1)
+    ]
+    return subgraph(
+        nodes,
+        edges,
+        alignment_edges,
+        name=f"cluster_{validator_id}",
+        label=f"{validator_id}",
+    )
+
+
+def plot(block_infos):
+    genesis_block = ""
+    validator_blocks = defaultdict(list)
+    for b in block_infos:
+        pk = b.summary.header.validator_public_key
+        if len(pk):
+            validator_blocks[pk.hex()].append(b)
+        else:
+            genesis_block = node(short_hash(block_hash(b)))
+
+    genesis_block = genesis_block
+
+    last_finalized_block_hash = next(
+        (block_hash(b) for b in block_infos if b.status.fault_tolerance > 0), ""
+    )
+
+    last_finalized_block_hash = last_finalized_block_hash
+
+    lanes = [
+        lane(validator, block_infos)
+        for (validator, block_infos) in validator_blocks.items()
+    ]
+
+    for b in block_infos:
+        if len(parents(b)) != 1:
+            raise Exception(f"{b} => {parents(b)}")
+    return graph(lanes)
+
+
+def main():
+    # client = casperlabs_client.CasperLabsClient("deploy.casperlabs.io")
+    client = casperlabs_client.CasperLabsClient(
+        "localhost", port=40411, port_internal=40412
+    )
+    block_infos = list(client.showBlocks(depth=10))
+
+    print(plot(block_infos))
+
+
+main()
