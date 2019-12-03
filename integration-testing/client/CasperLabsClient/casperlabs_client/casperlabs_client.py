ad25d977ac7a6ad11ad90dd0b33ba9a6051065f5
Piotr Kuchta <piotr@casperlabs.io>
2019-12-03 14:40:37 +0100

    NODE-1063: Add transfer CLI

--- integration-testing/client/CasperLabsClient/casperlabs_client/casperlabs_client.py
+++ integration-testing/client/CasperLabsClient/casperlabs_client/casperlabs_client.py
@@ -985,7 +985,22 @@ def unbond_command(casperlabs_client, args):
             )
         )
 
-    logging.info(f" XXX unbond_command: args.session_args={args.session_args}")
+    return deploy_command(casperlabs_client, args)
+
+
+@guarded_command
+def transfer_command(casperlabs_client, args):
+    _set_session(args, "transfer_to_account.wasm")
+
+    if not args.session_args:
+        args.session_args = ABI.args_to_json(
+            ABI.args(
+                [
+                    ABI.account("account", args.account),
+                    ABI.long_value("amount", args.amount),
+                ]
+            )
+        )
 
     return deploy_command(casperlabs_client, args)
 
@@ -1166,6 +1181,7 @@ def natural(number):
 def deploy_options(keys_required=False, private_key_accepted=True):
     return ([
         [('-f', '--from'), dict(required=True, type=str, help="The public key of the account which is the context of this deployment, base16 encoded.")],
+        [('-chain-name',), dict(required=False, type=str, help="Name of the chain to optionally restrict the deploy from being accidentally included anywhere else.")],
         [('--dependencies',), dict(required=False, nargs="+", default=None, help="List of deploy hashes (base16 encoded) which must be executed before this deploy.")],
         [('--payment-amount',), dict(required=False, type=int, default=None, help="Standard payment amount. Use this with the default payment, or override with --payment-args if custom payment code is used.")],
         [('--gas-price',), dict(required=False, type=int, default=10, help='The price of gas for this transaction in units dust/gas. Must be positive integer.')],
@@ -1287,6 +1303,11 @@ def main():
     parser.addCommand('unbond', unbond_command, 'Issues unbonding request',
                       [[('-a', '--amount'),
                        dict(required=False, default=None, type=int, help='Amount of motes to unbond. If not provided then a request to unbond with full staked amount is made.')]] + deploy_options(keys_required=True))
+
+    parser.addCommand('transfer', transfer_command, 'Transfers funds between accounts',
+                      [[('-a', '--amount'), dict(required=False, default=None, type=int, help='Amount of motes to transfer. Note: a mote is the smallest, indivisible unit of a token.')],
+                       [('-t', '--target-account'), dict(required=True, type=str, help="base64 representation of target account's public key")],
+                       ] + deploy_options(keys_required=True))
 
     parser.addCommand('propose', propose_command, 'Force a node to propose a block based on its accumulated deploys.', [])
 
