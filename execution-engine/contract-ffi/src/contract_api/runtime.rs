c1551102cd263c7c3bd65326c1ddc6c71117af76
Bartłomiej Kamiński <bart@casperlabs.io>
2019-12-06 17:55:13 +0100

    NO-TICKET: execution-engine: remove str_ref_to_ptr

--- execution-engine/contract-ffi/src/contract_api/runtime.rs
+++ execution-engine/contract-ffi/src/contract_api/runtime.rs
@@ -3,7 +3,7 @@ use alloc::{collections::BTreeMap, string::String, vec::Vec};
 use super::{
     alloc_bytes,
     error::{result_from, Error},
-    str_ref_to_ptr, to_ptr, ContractRef, TURef,
+    to_ptr, ContractRef, TURef,
 };
 use crate::{
     args_parser::ArgsParser,
@@ -72,7 +72,7 @@ pub fn call_contract<A: ArgsParser, T: FromBytes>(
 /// that URef with a new Contract instance containing the original contract's named_keys, the
 /// current protocol version, and the newly created bytes of the stored function.
 pub fn upgrade_contract_at_uref(name: &str, uref: TURef<Contract>) {
-    let (name_ptr, name_size, _bytes) = str_ref_to_ptr(name);
+    let (name_ptr, name_size, _bytes) = to_ptr(name);
     let key: Key = uref.into();
     let (key_ptr, key_size, _bytes) = to_ptr(&key);
     let result_value =
@@ -139,7 +139,7 @@ pub fn get_phase() -> Phase {
 /// name. This either comes from the named_keys of the account or contract,
 /// depending on whether the current module is a sub-call or not.
 pub fn get_key(name: &str) -> Option<Key> {
-    let (name_ptr, name_size, _bytes) = str_ref_to_ptr(name);
+    let (name_ptr, name_size, _bytes) = to_ptr(name);
     let key_size = unsafe { ext_ffi::get_key(name_ptr, name_size) };
     let dest_ptr = alloc_bytes(key_size);
     let key_bytes = unsafe {
@@ -154,21 +154,21 @@ pub fn get_key(name: &str) -> Option<Key> {
 
 /// Check if the given name corresponds to a known unforgable reference
 pub fn has_key(name: &str) -> bool {
-    let (name_ptr, name_size, _bytes) = str_ref_to_ptr(name);
+    let (name_ptr, name_size, _bytes) = to_ptr(name);
     let result = unsafe { ext_ffi::has_key(name_ptr, name_size) };
     result == 0
 }
 
 /// Put the given key to the named_keys map under the given name
 pub fn put_key(name: &str, key: &Key) {
-    let (name_ptr, name_size, _bytes) = str_ref_to_ptr(name);
+    let (name_ptr, name_size, _bytes) = to_ptr(name);
     let (key_ptr, key_size, _bytes2) = to_ptr(key);
     unsafe { ext_ffi::put_key(name_ptr, name_size, key_ptr, key_size) };
 }
 
 /// Removes Key persisted under [name] in the current context's map.
 pub fn remove_key(name: &str) {
-    let (name_ptr, name_size, _bytes) = str_ref_to_ptr(name);
+    let (name_ptr, name_size, _bytes) = to_ptr(name);
     unsafe { ext_ffi::remove_key(name_ptr, name_size) }
 }
 
