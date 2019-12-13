676a74355629b2da440641f31c66872000354bb0
Abner Zheng <abnerzheng@gmail.com>
2019-12-13 22:13:41 +0800

    NODE-962: make NoOpsEventEmitter.blockAdded emit BlockInfo rather than BlockSummary.
    Signed-off-by: Abner Zheng <abnerzheng@gmail.com>

--- casper/src/test/scala/io/casperlabs/casper/helper/NoOpsEventEmitter.scala
+++ casper/src/test/scala/io/casperlabs/casper/helper/NoOpsEventEmitter.scala
@@ -3,11 +3,11 @@ package io.casperlabs.casper.helper
 import cats.Applicative
 import cats.implicits._
 import io.casperlabs.casper.EventEmitter
-import io.casperlabs.casper.consensus.BlockSummary
+import io.casperlabs.casper.consensus.info.BlockInfo
 
 object NoOpsEventEmitter {
   def create[F[_]: Applicative](): EventEmitter[F] =
     new EventEmitter[F] {
-      override def blockAdded(blockSummary: BlockSummary): F[Unit] = ().pure[F]
+      override def blockAdded(block: BlockInfo): F[Unit] = ().pure[F]
     }
 }
