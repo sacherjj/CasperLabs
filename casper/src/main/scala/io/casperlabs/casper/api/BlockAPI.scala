873643fac69980c1e8610a478d81268e3348018d
Akosh Farkash <aakoshh@gmail.com>
2019-12-16 15:30:42 +0000

    NODE-982: Fix formatting.

--- casper/src/main/scala/io/casperlabs/casper/api/BlockAPI.scala
+++ casper/src/main/scala/io/casperlabs/casper/api/BlockAPI.scala
@@ -126,7 +126,9 @@ object BlockAPI {
 
         case false =>
           raise(
-            Aborted("There is another propose in progress, or node hasn't synced yet. Try again later.")
+            Aborted(
+              "There is another propose in progress, or node hasn't synced yet. Try again later."
+            )
           )
       }
     }
