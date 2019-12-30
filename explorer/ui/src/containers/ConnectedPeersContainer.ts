d6bf4b0ee909c968d84ed969db40e77ebbb4f46f
Abner Zheng <abnerzheng@gmail.com>
2019-12-30 19:30:46 +0800

    ECO-153: show connected peers in Clarity.

---
+++ explorer/ui/src/containers/ConnectedPeersContainer.ts
@@ -0,0 +1,24 @@
+import ErrorContainer from './ErrorContainer';
+import { DiagnosticsService } from 'casperlabs-sdk';
+import { observable, runInAction } from 'mobx';
+import { Node } from 'casperlabs-grpc/io/casperlabs/comm/discovery/node_pb';
+
+
+export class ConnectedPeersContainer {
+  @observable peers: Array<Node> | null = null;
+
+  constructor(
+    private errors: ErrorContainer,
+    private diagnosticsService: DiagnosticsService
+  ) {
+  }
+
+  async refreshPeers() {
+    let peers = await this.diagnosticsService.listPeers();
+    runInAction(() => {
+      this.peers = peers.getPeersList();
+    });
+  }
+}
+
+export default ConnectedPeersContainer;
