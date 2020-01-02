b7aeb8dc60d482e4bf998ea6d49cf21c12fd36ad
Mateusz Górski <gorski.mateusz@protonmail.ch>
2020-01-02 18:47:21 +0100

    NODE-1093: Push new LFB to the event stream.

--- node/src/main/scala/io/casperlabs/node/api/EventStream.scala
+++ node/src/main/scala/io/casperlabs/node/api/EventStream.scala
@@ -7,20 +7,21 @@ import cats.syntax.apply._
 import io.casperlabs.casper.Estimator.BlockHash
 import io.casperlabs.casper.consensus.info.{BlockInfo, Event}
 import io.casperlabs.casper.{EventEmitter, LastFinalizedBlockHashContainer}
-import io.casperlabs.casper.consensus.info.Event.{BlockAdded, Value}
+import io.casperlabs.casper.consensus.info.Event.{BlockAdded, NewFinalizedBlock, Value}
 import io.casperlabs.mempool.DeployBuffer
 import io.casperlabs.metrics.Metrics
 import io.casperlabs.node.api.casper.StreamEventsRequest
 import io.casperlabs.node.api.graphql.FinalizedBlocksStream
 import io.casperlabs.storage.block.BlockStorage
 import io.casperlabs.storage.deploy.DeployStorage
-import monix.execution.Scheduler
+import monix.execution.{Ack, Scheduler}
 import monix.reactive.{Observable, OverflowStrategy}
 import monix.reactive.subjects.ConcurrentSubject
 import simulacrum.typeclass
 import io.casperlabs.shared.Log
 import io.casperlabs.catscontrib.effect.implicits.fiberSyntax
 import io.casperlabs.storage.dag.FinalityStorage
+import monix.execution.atomic.AtomicLong
 
 @typeclass trait EventStream[F[_]] extends EventEmitter[F] {
   def subscribe(request: StreamEventsRequest): Observable[Event]
@@ -29,7 +30,8 @@ import io.casperlabs.storage.dag.FinalityStorage
 object EventStream {
   def create[F[_]: Concurrent: DeployStorage: BlockStorage: FinalityStorage: Log: Metrics](
       scheduler: Scheduler,
-      eventStreamBufferSize: Int
+      eventStreamBufferSize: Int,
+      lfbIdx: AtomicLong = AtomicLong(0) // TODO: Pull from the DB
   ): EventStream[F] = {
     val source =
       ConcurrentSubject.publish[Event](OverflowStrategy.DropOld(eventStreamBufferSize))(scheduler)
@@ -40,6 +42,7 @@ object EventStream {
           _.value match {
             case Empty               => false
             case Value.BlockAdded(_) => request.blockAdded
+            case Value.NewLfb(_)     => request.blockFinalized
           }
         }
       }
@@ -55,7 +58,13 @@ object EventStream {
           indirectlyFinalized: Set[BlockHash]
       ): F[Unit] =
         FinalityStorage[F].markAsFinalized(lfb, indirectlyFinalized) >>
-          DeployBuffer.removeFinalizedDeploys(indirectlyFinalized + lfb).forkAndLog
+          DeployBuffer.removeFinalizedDeploys(indirectlyFinalized + lfb).forkAndLog >>
+          Sync[F].delay {
+            val event = Event().withNewLfb(
+              NewFinalizedBlock(lfbIdx.getAndIncrement(), lfb, indirectlyFinalized.toSeq)
+            )
+            source.onNext(event)
+          }
     }
   }
 }
