79c71ec200f146b8963ed392edee3b2c48e8c1e0
Piotr Kuchta <piotr@casperlabs.io>
2020-01-14 11:12:11 +0100

    NODE-1112: Address review comments by @aakoshh

--- lrt/dramatiq_erc20.py
+++ lrt/dramatiq_erc20.py
@@ -1,12 +1,13 @@
 import argparse
 import logging
 import random
+import jsonpickle
 from dramatiq import actor
 from erc20 import ERC20, Node, Agent, DeployedERC20
 
 AGENTS = [Agent("account-0"), Agent("account-1"), Agent("account-2")]
 NODES = [Node("localhost")]
-FAUCET = Agent("faucet-account")
+ERC20_DEPLOYER = Agent("faucet-account")
 
 TOKEN_NAME = "ABC"
 TOTAL_TOKEN_SUPPLY = 200000
@@ -18,17 +19,25 @@ INITIAL_AGENT_CLX_FUNDS = 10 ** 7
 
 @actor
 def transfer_tokens(
-    sender_name, recipient_name, amount, token_name=TOKEN_NAME, wait_for_processed=False
+    pickled_erc20_deployer,
+    token_name,
+    node_host,
+    node_port,
+    pickled_sender,
+    pickled_recipient,
+    amount,
+    wait_for_processed=False,
 ):
     try:
-        sender = Agent(sender_name)
-        recipient = Agent(recipient_name)
+        erc20_deployer = jsonpickle.decode(pickled_erc20_deployer)
+        node = Node(node_host, node_port)
+        sender = jsonpickle.decode(pickled_sender)
+        recipient = jsonpickle.decode(pickled_recipient)
 
-        deployer = FAUCET.on(NODES[0])
-        abc = DeployedERC20.create(deployer, token_name)
+        deployed_erc20 = DeployedERC20.create(erc20_deployer.on(node), token_name)
 
-        sender.on(random.sample(NODES, 1)[0]).call_contract(
-            abc.transfer(
+        sender.on(node).call_contract(
+            deployed_erc20.transfer(
                 sender_private_key=sender.private_key,
                 recipient_public_key_hex=recipient.public_key_hex,
                 amount=amount,
@@ -37,13 +46,13 @@ def transfer_tokens(
         )
     except Exception as e:
         logging.error(
-            f"transfer_tokens({sender_name}, {recipient_name}, {amount}) => {str(e)}"
+            f"transfer_tokens({sender.public_key_hex}, {recipient.public_key_hex}, {amount}) => {str(e)}"
         )
 
 
 def initialize_erc20_simulation(
     node,
-    faucet,
+    deployer,
     agents,
     token_name,
     total_token_supply,
@@ -51,32 +60,34 @@ def initialize_erc20_simulation(
     tokens_per_agent,
 ):
     """
-    Deploy ERC20 smart contract from the faucet account.
+    Deploy ERC20 smart contract from the deployer account.
     Transfer initial_agent_clx_funds CLX to each agent.
     Transfer tokens_per_agent ERC20 tokens to each agent.
     """
-    deployer = faucet.on(node)
-    deployer.call_contract(ERC20(token_name).deploy(initial_balance=total_token_supply))
-    abc = DeployedERC20.create(deployer, token_name)
+    bound_deployer = deployer.on(node)
+    bound_deployer.call_contract(
+        ERC20(token_name).deploy(initial_balance=total_token_supply)
+    )
+    abc = DeployedERC20.create(bound_deployer, token_name)
 
-    balance = deployer.query(abc.balance(faucet.public_key_hex))
+    balance = bound_deployer.query(abc.balance(deployer.public_key_hex))
 
     # Initially deployer's balance should be equal the total token supply
     assert balance == total_token_supply
 
     for agent in agents:
-        faucet.on(node).transfer_clx(agent.public_key_hex, initial_agent_clx_funds)
+        bound_deployer.transfer_clx(agent.public_key_hex, initial_agent_clx_funds)
 
     # Transfer tokens from deployer to agents
     for agent in agents:
-        deployer.call_contract(
+        bound_deployer.call_contract(
             abc.transfer(
-                sender_private_key=faucet.private_key,
+                sender_private_key=deployer.private_key,
                 recipient_public_key_hex=agent.public_key_hex,
                 amount=tokens_per_agent,
             )
         )
-        balance = deployer.query(abc.balance(agent.public_key_hex))
+        balance = bound_deployer.query(abc.balance(agent.public_key_hex))
         assert balance == tokens_per_agent
 
 
@@ -88,7 +99,7 @@ if __name__ == "__main__":
     if args.command == "deploy":
         initialize_erc20_simulation(
             NODES[0],
-            FAUCET,
+            ERC20_DEPLOYER,
             AGENTS,
             TOKEN_NAME,
             TOTAL_TOKEN_SUPPLY,
@@ -99,6 +110,15 @@ if __name__ == "__main__":
     if args.command == "run":
         for _ in range(10):
             sender, recipient = random.sample(AGENTS, 2)
+            node = random.sample(NODES, 1)[0]
             amount = random.randint(1, MAX_TRANSFER)
-            # transfer_tokens.send(sender.name, recipient.name, amount)
-            transfer_tokens(sender.name, recipient.name, amount)
+
+            transfer_tokens(
+                jsonpickle.encode(ERC20_DEPLOYER),
+                TOKEN_NAME,
+                node.host,
+                node.port,
+                jsonpickle.encode(sender),
+                jsonpickle.encode(recipient),
+                amount,
+            )
