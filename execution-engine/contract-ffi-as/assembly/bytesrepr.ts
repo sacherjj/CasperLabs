edc77a2c732155d1614abe52412664b3cca18f12
Michał Papierski <michal@casperlabs.io>
2020-01-17 15:45:55 +0100

    EE-{821,822} Fix to/fromBytesU32.
    Now it uses store/load specialized AS functions to obtain the data from
    given bytes and fixes unit tests.
    
    Co-authored-by: Ed Hastings <ed@casperlabs.io>

--- execution-engine/contract-ffi-as/assembly/bytesrepr.ts
+++ execution-engine/contract-ffi-as/assembly/bytesrepr.ts
@@ -1,26 +1,22 @@
 import {CLValue} from "./clvalue";
 import {Key} from "./key";
 
+// Converts u32 to little endian
 export function toBytesU32(num: u32): u8[] {
-    // Converts u32 to little endian
-    // NOTE: AS apparently has store<i32> which could work for us but looks like AS portable stdlib doesn't provide it
-    return [
-        <u8>(num & 0x000000ff),
-        <u8>((num & 0x0000ff00) >> 8) & 255,
-        <u8>((num & 0x00ff0000) >> 16) & 255,
-        <u8>((num & 0xff000000) >> 24) & 255,
-    ];
+    let bytes = new Uint8Array(4);
+    store<u32>(bytes.dataStart, num);
+    let result = new Array<u8>(4);
+    for (var i = 0; i < 4; i++) {
+        result[i] = bytes[i];
+    }
+    return result;
 }
 
 export function fromBytesU32(bytes: Uint8Array): U32 | null {
     if (bytes.length < 4) {
         return null;
     }
-    // NOTE: For whatever reason << and | doesn't produce unsigned integers, so I turned `a << N` into `a * (1<<N)` and bitshift or `|` into addition.
-    const number = bytes[0] +
-        (bytes[1] * (1 << 8)) +
-        (bytes[2] * (1 << 16)) +
-        (bytes[3] * (1 << 24));
+    const number = <u32>load<u32>(bytes.dataStart);
     return <U32>number;
 }
 
@@ -50,17 +46,6 @@ export function toBytesU64(num: u64): u8[] {
         result[i] = bytes[i];
     }
     return result;
-
-    // return [
-    //     <u8>(num &  0x00000000000000ff),
-    //     <u8>((num & 0x000000000000ff00) >> 8) & 255,
-    //     <u8>((num & 0x0000000000ff0000) >> 16) & 255,
-    //     <u8>((num & 0x00000000ff000000) >> 24) & 255,
-    //     <u8>((num & 0x000000ff00000000) >> 32) & 255,
-    //     <u8>((num & 0x0000ff0000000000) >> 40) & 255,
-    //     <u8>((num & 0x00ff000000000000) >> 48) & 255,
-    //     <u8>((num & 0xff00000000000000) >> 56) & 255,
-    // ];
 }
 
 export function fromBytesU64(bytes: Uint8Array): U64 | null {
@@ -69,16 +54,7 @@ export function fromBytesU64(bytes: Uint8Array): U64 | null {
     }
 
     // NOTE: Overflows on unit tests for ranges >= 2**32
-    return <U64>(<i64>load<i64>(bytes.dataStart));
-    // const number = bytes[0] +
-    //     (bytes[1] * (1 << 8)) +
-    //     (bytes[2] * (1 << 16)) +
-    //     (bytes[3] * (1 << 24)) +
-    //     (bytes[4] * (1 << 32)) +
-    //     (bytes[5] * (1 << 40)) +
-    //     (bytes[6] * (1 << 48)) +
-    //     (bytes[7] * (1 << 56));
-    // return <U64>number;
+    return <U64><i64>load<i64>(bytes.dataStart);
 }
 
 export function toBytesPair(key: u8[], value: u8[]): u8[] {
@@ -160,6 +136,13 @@ export function serializeKeys(keys: Key[]): Array<u8> {
 
 export function fromBytesStringList(arr: Uint8Array): String[] | null {
     var len = fromBytesU32(arr);
+    
+    if (len === <U32>0) {
+        // NOTE: There's something wrong about how === operator works (compared to JS/TS).
+        // NOTE: If len is null then `len === null` is true for both null and 0.
+        return [];
+    }
+
     if (len === null) {
         return null;
     }
