8346dcea39c333cf6ec99f3d06bd5ee047b3611c
Piotr Kuchta <piotr@casperlabs.io>
2020-01-18 07:42:42 +0100

    NODE-1115: Make sure connection is always closed

--- integration-testing/client/CasperLabsClient/casperlabs_client/casperlabs_client_aio.py
+++ integration-testing/client/CasperLabsClient/casperlabs_client/casperlabs_client_aio.py
@@ -19,7 +19,31 @@ DEFAULT_HOST = "localhost"
 DEFAULT_PORT = 40401
 
 
-class CasperLabsClientAIO:
+class ScopedChannel(object):
+    def __init__(self, channel):
+        self.channel = channel
+
+    def __enter__(self):
+        return self.channel
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.channel.close()
+
+
+class CasperService(object):
+    def __init__(self, client):
+        self.client = client
+
+    def __getattr__(self, name):
+        async def method(*args):
+            with ScopedChannel(self.client.channel()) as channel:
+                service = casper_grpc.CasperServiceStub(channel)
+                return await getattr(service, name)(*args)
+
+        return method
+
+
+class CasperLabsClientAIO(object):
     """
     gRPC asyncio CasperLabs client.
     """
@@ -27,16 +51,16 @@ class CasperLabsClientAIO:
     def __init__(self, host: str = DEFAULT_HOST, port: int = DEFAULT_PORT):
         self.host = host
         self.port = port
+        self.casper_service = CasperService(self)
+
+    def channel(self):
+        return Channel(self.host, self.port)
 
     async def show_blocks(self, depth=1, max_rank=0, full_view=True):
         view = full_view and info.BlockInfo.View.FULL or info.BlockInfo.View.BASIC
-        channel = Channel(self.host, self.port)
-        service = casper_grpc.CasperServiceStub(channel)
-        result = await service.StreamBlockInfos(
+        return await self.casper_service.StreamBlockInfos(
             casper.StreamBlockInfosRequest(depth=depth, max_rank=max_rank, view=view)
         )
-        channel.close()
-        return result
 
     async def deploy(
         self,
@@ -86,10 +110,7 @@ class CasperLabsClientAIO:
         return deploy.deploy_hash.hex()
 
     async def send_deploy(self, deploy):
-        channel = Channel(self.host, self.port)
-        service = casper_grpc.CasperServiceStub(channel)
-        await service.Deploy(casper.DeployRequest(deploy=deploy))
-        channel.close()
+        return await self.casper_service.Deploy(casper.DeployRequest(deploy=deploy))
 
     async def wait_for_deploy_processed(self, deploy_hash, on_error_raise=True):
         result = None
@@ -110,33 +131,24 @@ class CasperLabsClientAIO:
 
     async def show_deploy(self, deploy_hash_base16: str, full_view=True):
         view = full_view and info.DeployInfo.View.FULL or info.DeployInfo.View.BASIC
-        channel = Channel(self.host, self.port)
-        service = casper_grpc.CasperServiceStub(channel)
-        request = casper.GetDeployInfoRequest(
-            deploy_hash_base16=deploy_hash_base16, view=view
+        return await self.casper_service.GetDeployInfo(
+            casper.GetDeployInfoRequest(
+                deploy_hash_base16=deploy_hash_base16, view=view
+            )
         )
-        response = await service.GetDeployInfo(request)
-        channel.close()
-        return response
 
     async def query_state(self, block_hash: str, key: str, path: str, key_type: str):
         q = casper.StateQuery(key_variant=key_variant(key_type), key_base16=key)
         q.path_segments.extend([name for name in path.split("/") if name])
-
-        channel = Channel(self.host, self.port)
-        service = casper_grpc.CasperServiceStub(channel)
-        response = await service.GetBlockState(
+        return await self.casper_service.GetBlockState(
             casper.GetBlockStateRequest(block_hash_base16=block_hash, query=q)
         )
-        channel.close()
-        return response
 
     async def transfer(self, target_account_hex, amount, **deploy_args):
-        target_account_bytes = bytes.fromhex(target_account_hex)
         deploy_args["session"] = bundled_contract("transfer_to_account.wasm")
         deploy_args["session_args"] = abi.ABI.args(
             [
-                abi.ABI.account("account", target_account_bytes),
+                abi.ABI.account("account", bytes.fromhex(target_account_hex)),
                 abi.ABI.long_value("amount", amount),
             ]
         )
@@ -170,22 +182,14 @@ class CasperLabsClientAIO:
 
     async def show_block(self, block_hash_base16: str, full_view=True):
         view = full_view and info.BlockInfo.View.FULL or info.BlockInfo.View.BASIC
-        channel = Channel(self.host, self.port)
-        service = casper_grpc.CasperServiceStub(channel)
-        block_info = await service.GetBlockInfo(
+        return await self.casper_service.GetBlockInfo(
             casper.GetBlockInfoRequest(block_hash_base16=block_hash_base16, view=view)
         )
-        channel.close()
-        return block_info
 
     async def show_deploys(self, block_hash_base16: str, full_view=True):
         view = full_view and info.DeployInfo.View.FULL or info.DeployInfo.View.BASIC
-        channel = Channel(self.host, self.port)
-        service = casper_grpc.CasperServiceStub(channel)
-        response = await service.StreamBlockDeploys(
+        return await self.casper_service.StreamBlockDeploys(
             casper.StreamBlockDeploysRequest(
                 block_hash_base16=block_hash_base16, view=view
             )
         )
-        channel.close()
-        return response
