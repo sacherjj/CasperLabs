01ab4142f4d4afa8a35b35bf8f9691335f56c63f
Bartłomiej Kamiński <bart@casperlabs.io>
2020-01-23 15:53:25 +0100

    {EE-819, EE-820}: Add some From implementations for Code

--- execution-engine/engine-test-support/src/code.rs
+++ execution-engine/engine-test-support/src/code.rs
@@ -1,4 +1,6 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+
+use types::URef;
 
 use crate::{Hash, URefAddr};
 
@@ -12,4 +14,31 @@ pub enum Code {
     URef(URefAddr),
     /// A hash providing the location of a stored contract.
     Hash(Hash),
+}
+
+// Note: can't just `impl<T: AsRef<Path>> From<T> for Code` because the compiler complains about
+// a conflicting implementation of `From<URef>` - as URef could be made `AsRef<Path>` in the future
+
+impl<'a> From<&'a str> for Code {
+    fn from(path: &'a str) -> Code {
+        Code::Path(path.into())
+    }
+}
+
+impl<'a> From<&'a Path> for Code {
+    fn from(path: &'a Path) -> Code {
+        Code::Path(path.into())
+    }
+}
+
+impl From<PathBuf> for Code {
+    fn from(path: PathBuf) -> Code {
+        Code::Path(path)
+    }
+}
+
+impl From<URef> for Code {
+    fn from(uref: URef) -> Code {
+        Code::URef(uref.addr())
+    }
 }
