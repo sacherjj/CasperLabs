2dc4b7624d332715802d063274b8bbd07ad15926
Henry Till <henrytill@gmail.com>
2020-01-31 16:27:39 -0500

    EE-871: tear the Mint down to the studs

--- execution-engine/contracts/system/mint-token/src/lib.rs
+++ execution-engine/contracts/system/mint-token/src/lib.rs
@@ -3,79 +3,78 @@
 
 extern crate alloc;
 
-mod capabilities;
-
 // These types are purposely defined in a separate module
 // so that their constructors are hidden and therefore
 // we must use the conversion methods from Key elsewhere
 // in the code.
-pub mod internal_purse_id;
 pub mod mint;
 
 use alloc::string::String;
 use core::convert::TryInto;
 
-use capabilities::{RefWithAddRights, RefWithReadAddWriteRights};
 use contract::{
-    contract_api::{runtime, storage},
+    contract_api::{runtime, storage, TURef},
     unwrap_or_revert::UnwrapOrRevert,
 };
-use internal_purse_id::{DepositId, WithdrawId};
 use mint::Mint;
 use types::{
-    account::PUBLIC_KEY_LENGTH,
-    system_contract_errors::mint::{Error, PurseIdError},
-    AccessRights, ApiError, CLValue, Key, URef, U512,
+    account::PUBLIC_KEY_LENGTH, system_contract_errors::mint::Error, ApiError, CLValue, Key, URef,
+    U512,
 };
 
 const SYSTEM_ACCOUNT: [u8; PUBLIC_KEY_LENGTH] = [0u8; PUBLIC_KEY_LENGTH];
 
 pub struct CLMint;
 
-impl Mint<RefWithAddRights<U512>, RefWithReadAddWriteRights<U512>> for CLMint {
-    type PurseId = WithdrawId;
-    type DepOnlyId = DepositId;
-
-    fn mint(&self, initial_balance: U512) -> Result<Self::PurseId, Error> {
+impl Mint for CLMint {
+    fn mint(&self, initial_balance: U512) -> Result<URef, Error> {
         let caller = runtime::get_caller();
         if !initial_balance.is_zero() && caller.value() != SYSTEM_ACCOUNT {
             return Err(Error::InvalidNonEmptyPurseCreation);
         }
 
         let balance_uref: Key = storage::new_turef(initial_balance).into();
-
         let purse_key: URef = storage::new_turef(()).into();
         let purse_uref_name = purse_key.remove_access_rights().as_string();
-
-        let purse_id: WithdrawId = WithdrawId::from_uref(purse_key).unwrap();
 
         // store balance uref so that the runtime knows the mint has full access
         runtime::put_key(&purse_uref_name, balance_uref);
 
         // store association between purse id and balance uref
-        //
-        // Gorski writes:
-        //   I'm worried that this can lead to overwriting of values in the local state.
-        //   Since it accepts a raw byte array it's possible to construct one by hand.
-        // Of course,   a key can be overwritten only when that write is
-        // performed in the "owner" context   so it aligns with other semantics
-        // of write but I would prefer if were able to enforce   uniqueness
-        // somehow.
-        storage::write_local(purse_id.raw_id(), balance_uref);
+        storage::write_local(purse_key.addr(), balance_uref);
 
-        Ok(purse_id)
+        Ok(purse_key)
     }
 
-    fn lookup(&self, p: Self::PurseId) -> Option<RefWithReadAddWriteRights<U512>> {
-        storage::read_local(&p.raw_id())
+    fn lookup(&self, p: URef) -> Option<URef> {
+        storage::read_local(&p.addr())
             .ok()?
             .and_then(|key: Key| key.try_into().ok())
     }
 
-    fn dep_lookup(&self, p: Self::DepOnlyId) -> Option<RefWithAddRights<U512>> {
-        storage::read_local(&p.raw_id())
-            .ok()?
-            .and_then(|key: Key| key.try_into().ok())
+    fn create(&self) -> URef {
+        self.mint(U512::zero())
+            .expect("Creating a zero balance purse should always be allowed.")
+    }
+
+    fn transfer(&self, source: URef, dest: URef, amount: U512) -> Result<(), Error> {
+        let source_bal = self.lookup(source).ok_or(Error::SourceNotFound)?;
+        let source_bal_turef: TURef<U512> =
+            TURef::from_uref(source_bal).map_err(|_| Error::InvalidURef)?;
+        let source_value = match storage::read(source_bal_turef) {
+            Ok(Some(source_value)) => source_value,
+            Ok(None) => return Err(Error::SourceNotFound),
+            Err(err) => runtime::revert(err),
+        };
+        if amount > source_value {
+            return Err(Error::InsufficientFunds);
+        }
+        let dest_bal = self.lookup(dest).ok_or(Error::DestNotFound)?;
+        let dest_bal_turef: TURef<U512> =
+            TURef::from_uref(dest_bal).map_err(|_| Error::InvalidURef)?;
+        storage::write(source_bal_turef, source_value - amount);
+        storage::add(dest_bal_turef, amount);
+        Ok(())
     }
 }
 
@@ -92,31 +91,30 @@ pub fn delegate() {
             let amount: U512 = runtime::get_arg(1)
                 .unwrap_or_revert_with(ApiError::MissingArgument)
                 .unwrap_or_revert_with(ApiError::InvalidArgument);
-
-            let maybe_purse_key = mint
-                .mint(amount)
-                .map(|purse_id| URef::new(purse_id.raw_id(), AccessRights::READ_ADD_WRITE));
-            let return_value = CLValue::from_t(maybe_purse_key).unwrap_or_revert();
-            runtime::ret(return_value)
+            let result: Result<URef, Error> = mint.mint(amount);
+            let ret = CLValue::from_t(result).unwrap_or_revert();
+            runtime::ret(ret)
         }
 
         "create" => {
-            let purse_id = mint.create();
-            let purse_key = URef::new(purse_id.raw_id(), AccessRights::READ_ADD_WRITE);
-            let return_value = CLValue::from_t(purse_key).unwrap_or_revert();
-            runtime::ret(return_value)
+            let uref: URef = mint.create();
+            let ret = CLValue::from_t(uref).unwrap_or_revert();
+            runtime::ret(ret)
         }
 
         "balance" => {
-            let key: URef = runtime::get_arg(1)
+            let uref: URef = runtime::get_arg(1)
                 .unwrap_or_revert_with(ApiError::MissingArgument)
                 .unwrap_or_revert_with(ApiError::InvalidArgument);
-            let purse_id: WithdrawId = WithdrawId::from_uref(key).unwrap();
-            let balance_uref = mint.lookup(purse_id);
-            let balance: Option<U512> =
-                balance_uref.and_then(|uref| storage::read(uref.into()).unwrap_or_default());
-            let return_value = CLValue::from_t(balance).unwrap_or_revert();
-            runtime::ret(return_value)
+            let balance_uref: TURef<U512> = match mint.lookup(uref) {
+                // safe to unwrap because of patten guard
+                Some(uref) if uref.access_rights().is_some() => TURef::from_uref(uref).unwrap(),
+                Some(_) => runtime::revert(ApiError::NoAccessRights),
+                None => runtime::revert(ApiError::ValueNotFound),
+            };
+            let balance: Option<U512> = storage::read(balance_uref).unwrap_or_default();
+            let ret = CLValue::from_t(balance).unwrap_or_revert();
+            runtime::ret(ret)
         }
 
         "transfer" => {
@@ -129,26 +127,13 @@ pub fn delegate() {
             let amount: U512 = runtime::get_arg(3)
                 .unwrap_or_revert_with(ApiError::MissingArgument)
                 .unwrap_or_revert_with(ApiError::InvalidArgument);
-
-            let return_error = |error: PurseIdError| -> ! {
-                let transfer_result: Result<(), Error> = Err(error.into());
-                let return_value = CLValue::from_t(transfer_result).unwrap_or_revert();
-                runtime::ret(return_value)
+            let result: Result<(), Error> = if !source.is_writeable() || !target.is_addable() {
+                Err(Error::InvalidAccessRights)
+            } else {
+                mint.transfer(source, target, amount)
             };
-
-            let source: WithdrawId = match WithdrawId::from_uref(source) {
-                Ok(withdraw_id) => withdraw_id,
-                Err(error) => return_error(error),
-            };
-
-            let target: DepositId = match DepositId::from_uref(target) {
-                Ok(deposit_id) => deposit_id,
-                Err(error) => return_error(error),
-            };
-
-            let transfer_result = mint.transfer(source, target, amount);
-            let return_value = CLValue::from_t(transfer_result).unwrap_or_revert();
-            runtime::ret(return_value);
+            let ret = CLValue::from_t(result).unwrap_or_revert();
+            runtime::ret(ret);
         }
         _ => panic!("Unknown method name!"),
     }
--- execution-engine/contracts/test/modified-mint/src/lib.rs
+++ execution-engine/contracts/test/modified-mint/src/lib.rs
@@ -5,18 +5,11 @@ extern crate alloc;
 use alloc::string::String;
 
 use contract::{
-    contract_api::{runtime, storage},
+    contract_api::{runtime, storage, TURef},
     unwrap_or_revert::UnwrapOrRevert,
 };
-use mint_token::{
-    internal_purse_id::{DepositId, WithdrawId},
-    mint::Mint,
-    CLMint,
-};
-use types::{
-    system_contract_errors::mint::{Error, PurseIdError},
-    AccessRights, ApiError, CLValue, URef, U512,
-};
+use mint_token::{mint::Mint, CLMint};
+use types::{system_contract_errors::mint::Error, ApiError, CLValue, URef, U512};
 
 const VERSION: &str = "1.1.0";
 
@@ -33,32 +26,29 @@ pub fn delegate() {
             let amount: U512 = runtime::get_arg(1)
                 .unwrap_or_revert_with(ApiError::MissingArgument)
                 .unwrap_or_revert_with(ApiError::InvalidArgument);
-
-            let maybe_purse_key = mint
-                .mint(amount)
-                .map(|purse_id| URef::new(purse_id.raw_id(), AccessRights::READ_ADD_WRITE));
-            let return_value = CLValue::from_t(maybe_purse_key).unwrap_or_revert();
-
-            runtime::ret(return_value)
+            let result: Result<URef, Error> = mint.mint(amount);
+            let ret = CLValue::from_t(result).unwrap_or_revert();
+            runtime::ret(ret)
         }
 
         "create" => {
-            let purse_id = mint.create();
-            let purse_key = URef::new(purse_id.raw_id(), AccessRights::READ_ADD_WRITE);
-            let return_value = CLValue::from_t(purse_key).unwrap_or_revert();
-            runtime::ret(return_value)
+            let uref = mint.create();
+            let ret = CLValue::from_t(uref).unwrap_or_revert();
+            runtime::ret(ret)
         }
 
         "balance" => {
-            let key: URef = runtime::get_arg(1)
+            let uref: URef = runtime::get_arg(1)
                 .unwrap_or_revert_with(ApiError::MissingArgument)
                 .unwrap_or_revert_with(ApiError::InvalidArgument);
-            let purse_id: WithdrawId = WithdrawId::from_uref(key).unwrap();
-            let balance_uref = mint.lookup(purse_id);
-            let balance: Option<U512> =
-                balance_uref.and_then(|uref| storage::read(uref.into()).unwrap_or_default());
-            let return_value = CLValue::from_t(balance).unwrap_or_revert();
-            runtime::ret(return_value)
+            let balance_uref: TURef<U512> = match mint.lookup(uref) {
+                Some(uref) if uref.access_rights().is_some() => TURef::from_uref(uref).unwrap(),
+                Some(_) => runtime::revert(ApiError::NoAccessRights),
+                None => runtime::revert(ApiError::ValueNotFound),
+            };
+            let balance: Option<U512> = storage::read(balance_uref).unwrap_or_default();
+            let ret = CLValue::from_t(balance).unwrap_or_revert();
+            runtime::ret(ret)
         }
 
         "transfer" => {
@@ -71,26 +61,9 @@ pub fn delegate() {
             let amount: U512 = runtime::get_arg(3)
                 .unwrap_or_revert_with(ApiError::MissingArgument)
                 .unwrap_or_revert_with(ApiError::InvalidArgument);
-
-            let return_error = |error: PurseIdError| -> ! {
-                let transfer_result: Result<(), Error> = Err(error.into());
-                let return_value = CLValue::from_t(transfer_result).unwrap_or_revert();
-                runtime::ret(return_value)
-            };
-
-            let source: WithdrawId = match WithdrawId::from_uref(source) {
-                Ok(withdraw_id) => withdraw_id,
-                Err(error) => return_error(error),
-            };
-
-            let target: DepositId = match DepositId::from_uref(target) {
-                Ok(deposit_id) => deposit_id,
-                Err(error) => return_error(error),
-            };
-
-            let transfer_result = mint.transfer(source, target, amount);
-            let return_value = CLValue::from_t(transfer_result).unwrap_or_revert();
-            runtime::ret(return_value);
+            let result: Result<(), Error> = mint.transfer(source, target, amount);
+            let ret = CLValue::from_t(result).unwrap_or_revert();
+            runtime::ret(ret);
         }
         "version" => {
             runtime::ret(CLValue::from_t(VERSION).unwrap_or_revert());
