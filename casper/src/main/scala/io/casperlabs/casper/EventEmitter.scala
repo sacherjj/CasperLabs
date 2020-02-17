c8d6d1239faf11ac045510e4c818ad99a45883cb
Akosh Farkash <aakoshh@gmail.com>
2020-02-17 14:50:31 +0000

    NODE-1147: Proto for deploy events.

--- casper/src/main/scala/io/casperlabs/casper/EventEmitter.scala
+++ casper/src/main/scala/io/casperlabs/casper/EventEmitter.scala
@@ -2,6 +2,8 @@ package io.casperlabs.casper
 
 import io.casperlabs.casper.Estimator.BlockHash
 import io.casperlabs.casper.consensus.info.BlockInfo
+import io.casperlabs.casper.consensus.Deploy
+import io.casperlabs.casper.consensus.Block.ProcessedDeploy
 import simulacrum.typeclass
 
 @typeclass trait EventEmitter[F[_]] {
@@ -10,4 +12,13 @@ import simulacrum.typeclass
       lfb: BlockHash,
       indirectlyFinalized: Set[BlockHash]
   ): F[Unit]
+
+  // The following `deploy*` methods are based on the local deploy buffer,
+  // they are not observed on every node (unless there's deploy gossiping).
+  // The missing ones (deployProcessed, deployFinalized) can be handled
+  // internally by the `blockAdded` and `newLastFinalizedBlock` methods.
+
+  def deployAdded(deploy: Deploy): F[Unit]
+  def deployDiscarded(deployHash: DeployHash, message: String): F[Unit]
+  def deployRequeued(deployHash: DeployHash): F[Unit]
 }
