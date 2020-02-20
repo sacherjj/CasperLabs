4a68c034540df4ff09d8da25774973c77ba3baad
Bartłomiej Kamiński <bart@casperlabs.io>
2020-02-20 14:04:37 +0100

    EE-887: add tests exercising debug assertions

--- execution-engine/engine-storage/src/trie_store/operations/tests/keys.rs
+++ execution-engine/engine-storage/src/trie_store/operations/tests/keys.rs
@@ -144,3 +144,104 @@ mod full_tries {
         }
     }
 }
+
+#[cfg(debug_assertions)]
+mod keys_iterator {
+    use engine_shared::newtypes::{Blake2bHash, CorrelationId};
+    use types::bytesrepr;
+
+    use crate::{
+        transaction_source::TransactionSource,
+        trie::{Pointer, Trie},
+        trie_store::operations::{
+            self,
+            tests::{
+                hash_test_tries, HashedTestTrie, HashedTrie, InMemoryTestContext, TestKey,
+                TestValue, TEST_LEAVES,
+            },
+        },
+    };
+
+    fn create_invalid_extension_trie(
+    ) -> Result<(Blake2bHash, Vec<HashedTestTrie>), bytesrepr::Error> {
+        let leaves = hash_test_tries(&TEST_LEAVES[2..3])?;
+        let ext_1 = HashedTrie::new(Trie::extension(
+            vec![0u8, 0],
+            Pointer::NodePointer(leaves[0].hash),
+        ))?;
+
+        let root = HashedTrie::new(Trie::node(&[(0, Pointer::NodePointer(ext_1.hash))]))?;
+        let root_hash = root.hash;
+
+        let tries = vec![root, ext_1, leaves[0].clone()];
+
+        Ok((root_hash, tries))
+    }
+
+    fn create_invalid_path_trie() -> Result<(Blake2bHash, Vec<HashedTestTrie>), bytesrepr::Error> {
+        let leaves = hash_test_tries(&TEST_LEAVES[..1])?;
+
+        let root = HashedTrie::new(Trie::node(&[(1, Pointer::NodePointer(leaves[0].hash))]))?;
+        let root_hash = root.hash;
+
+        let tries = vec![root, leaves[0].clone()];
+
+        Ok((root_hash, tries))
+    }
+
+    fn create_invalid_hash_trie() -> Result<(Blake2bHash, Vec<HashedTestTrie>), bytesrepr::Error> {
+        let leaves = hash_test_tries(&TEST_LEAVES[..2])?;
+
+        let root = HashedTrie::new(Trie::node(&[(0, Pointer::NodePointer(leaves[1].hash))]))?;
+        let root_hash = root.hash;
+
+        let tries = vec![root, leaves[0].clone()];
+
+        Ok((root_hash, tries))
+    }
+
+    macro_rules! return_on_err {
+        ($x:expr) => {
+            match $x {
+                Ok(result) => result,
+                Err(_) => {
+                    return; // we expect the test to panic, so this will cause a test failure
+                }
+            }
+        };
+    }
+
+    fn test_trie(root_hash: Blake2bHash, tries: Vec<HashedTestTrie>) {
+        let correlation_id = CorrelationId::new();
+        let context = return_on_err!(InMemoryTestContext::new(&tries));
+        let txn = return_on_err!(context.environment.create_read_txn());
+        let _tmp = operations::keys::<TestKey, TestValue, _, _>(
+            correlation_id,
+            &txn,
+            &context.store,
+            &root_hash,
+        )
+        .collect::<Vec<_>>();
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_panic_on_leaf_after_extension() {
+        let (root_hash, tries) = return_on_err!(create_invalid_extension_trie());
+        test_trie(root_hash, tries);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_panic_when_key_not_matching_path() {
+        let (root_hash, tries) = return_on_err!(create_invalid_path_trie());
+        test_trie(root_hash, tries);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_panic_on_pointer_to_nonexisting_hash() {
+        let (root_hash, tries) = return_on_err!(create_invalid_hash_trie());
+        test_trie(root_hash, tries);
+    }
+}
