83eba969dfc488d9e97880cc8b24571ce3e5ab46
Akosh Farkash <aakoshh@gmail.com>
2020-02-20 14:48:18 +0000

    CON-629: Remove 2nd package declaration.

--- casper/src/test/scala/io/casperlabs/casper/util/ProtocolVersionsTest.scala
+++ casper/src/test/scala/io/casperlabs/casper/util/ProtocolVersionsTest.scala
@@ -1,7 +1,5 @@
 package io.casperlabs.casper.util
 
-package io.casperlabs.casper.util
-
 import io.casperlabs.casper.consensus.state.ProtocolVersion
 import io.casperlabs.casper.util.ProtocolVersions.Config
 import io.casperlabs.ipc.ChainSpec.DeployConfig
