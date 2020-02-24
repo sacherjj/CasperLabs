3f76d622655a72e5de5bb1930056c25020085587
TomVasile <43349666+TomVasile@users.noreply.github.com>
2020-02-24 18:54:05 +0000

    initial move to node/client using deb for docker

--- hack/docker/template/start-node.sh
+++ hack/docker/template/start-node.sh
@@ -4,12 +4,12 @@ set -e
 
 if [ "$HOSTNAME" = "$BOOTSTRAP_HOSTNAME" ]; then
     # Start the bootstrap node with a fixed key to get a deterministic ID.
-    exec ./bin/casperlabs-node run -s \
+    exec /usr/bin/casperlabs-node run -s \
         --server-host $HOSTNAME
 else
     # Connect to the bootstrap node.
     BOOTSTRAP_ID=$(cat $HOME/.casperlabs/bootstrap/node-id)
-    exec ./bin/casperlabs-node run \
+    exec /usr/bin/casperlabs-node run \
         --server-host $HOSTNAME \
         --server-bootstrap "casperlabs://${BOOTSTRAP_ID}@${BOOTSTRAP_HOSTNAME}?protocol=40400&discovery=40404"
 fi
