bf67e1054980a3fb7a493569cecbf93e171a8372
Akosh Farkash <aakoshh@gmail.com>
2020-03-09 14:53:20 +0000

    NODE-1276: Always bootstrap from every other node.

--- hack/docker/template/start-node.sh
+++ hack/docker/template/start-node.sh
@@ -2,14 +2,22 @@
 
 set -e
 
-if [ "$HOSTNAME" = "$BOOTSTRAP_HOSTNAME" ]; then
-    # Start the bootstrap node with a fixed key to get a deterministic ID.
-    exec /usr/bin/casperlabs-node run -s \
-        --server-host $HOSTNAME
-else
-    # Connect to the bootstrap node.
-    BOOTSTRAP_ID=$(cat $HOME/.casperlabs/bootstrap/node-id)
-    exec /usr/bin/casperlabs-node run \
-        --server-host $HOSTNAME \
-        --server-bootstrap "casperlabs://${BOOTSTRAP_ID}@${BOOTSTRAP_HOSTNAME}?protocol=40400&discovery=40404"
+CL_SERVER_BOOTSTRAP=${CL_SERVER_BOOTSTRAP:-""}
+
+# Unless specified, connect to each other node, so there's no specific bootstrap,
+# therefore we can restart any node without worrying that it will not try to reconnect.
+if [ -z "$CL_SERVER_BOOTSTRAP" ]; then
+    cd $HOME/.casperlabs/nodes
+    while read -r BOOTSTRAP_HOSTNAME ; do
+        if [ "$BOOTSTRAP_HOSTNAME" != "$HOSTNAME" ]; then
+            BOOTSTRAP_ID=$(cat $BOOTSTRAP_HOSTNAME/node-id)
+            BOOTSTRAP="casperlabs://${BOOTSTRAP_ID}@${BOOTSTRAP_HOSTNAME}?protocol=40400&discovery=40404"
+            export CL_SERVER_BOOTSTRAP="$BOOTSTRAP $CL_SERVER_BOOTSTRAP"
+        fi
+    done < <(ls)
+    cd -
 fi
+
+echo CL_SERVER_BOOTSTRAP = $CL_SERVER_BOOTSTRAP
+
+exec /usr/bin/casperlabs-node run --server-host $HOSTNAME
