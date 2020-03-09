4d44ccbd57c1de04ab1e0702c3611933b2fc6f8d
Piotr Kuchta <piotr@casperlabs.io>
2020-03-09 18:36:04 +0100

    NODE-1224: Address review comments by @aakoshh and revert accidental changes to hack/docker

--- hack/docker/highway.sh
+++
@@ -1,6 +0,0 @@
-export CL_HIGHWAY_ENABLED=true
-. scripts/highway-env.sh
-CL_HIGHWAY_INIT_ROUND_EXPONENT=12 make node-0/up
-CL_HIGHWAY_INIT_ROUND_EXPONENT=12 make node-1/up
-CL_HIGHWAY_INIT_ROUND_EXPONENT=12 make node-2/up
-
