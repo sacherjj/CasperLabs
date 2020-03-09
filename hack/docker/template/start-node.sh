c2c9a89fda10a2d7293cab9547281873b86677d3
Akosh Farkash <aakoshh@gmail.com>
2020-03-09 16:19:16 +0000

    NODE-1276: Assume we're not setting CL_SERVER_BOOTSTRAP from outside.

--- hack/docker/template/start-node.sh
+++ hack/docker/template/start-node.sh
@@ -2,23 +2,17 @@
 
 set -e
 
-CL_SERVER_BOOTSTRAP=${CL_SERVER_BOOTSTRAP:-""}
+export CL_SERVER_BOOTSTRAP=""
 BOOTSTRAP_HOSTNAMES=${BOOTSTRAP_HOSTNAMES:-""}
 
-# Unless specified, connect to each other node, so there's no specific bootstrap,
-# therefore we can restart any node without worrying that it will not try to reconnect.
-if [ -z "$CL_SERVER_BOOTSTRAP" ]; then
-    for BOOTSTRAP_HOSTNAME in $BOOTSTRAP_HOSTNAMES; do
-        if [ "$BOOTSTRAP_HOSTNAME" != "$HOSTNAME" ]; then
-            BOOTSTRAP_ID=$(cat $HOME/.casperlabs/nodes/$BOOTSTRAP_HOSTNAME/node-id)
-            BOOTSTRAP="casperlabs://${BOOTSTRAP_ID}@${BOOTSTRAP_HOSTNAME}?protocol=40400&discovery=40404"
-            export CL_SERVER_BOOTSTRAP="$BOOTSTRAP $CL_SERVER_BOOTSTRAP"
-            export CL_CASPER_STANDALONE=false
-        fi
-    done
-else
-    export CL_CASPER_STANDALONE=false
-fi
+for BOOTSTRAP_HOSTNAME in $BOOTSTRAP_HOSTNAMES; do
+    if [ "$BOOTSTRAP_HOSTNAME" != "$HOSTNAME" ]; then
+        BOOTSTRAP_ID=$(cat $HOME/.casperlabs/nodes/$BOOTSTRAP_HOSTNAME/node-id)
+        BOOTSTRAP="casperlabs://${BOOTSTRAP_ID}@${BOOTSTRAP_HOSTNAME}?protocol=40400&discovery=40404"
+        export CL_SERVER_BOOTSTRAP="$BOOTSTRAP $CL_SERVER_BOOTSTRAP"
+        export CL_CASPER_STANDALONE=false
+    fi
+done
 
 export CL_CASPER_STANDALONE=${CL_CASPER_STANDALONE:-true}
 
