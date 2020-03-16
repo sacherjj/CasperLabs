9618a6b20966176a3aca83209ad01eeecd26e7c1
Bartłomiej Kamiński <bart@casperlabs.io>
2020-03-16 17:18:27 +0100

    EE-940: Document Option, Pair and Unit

--- execution-engine/contract-as/assembly/unit.ts
+++ execution-engine/contract-as/assembly/unit.ts
@@ -1,6 +1,10 @@
+/**
+ * A class representing the unit type, i.e. a type that has no values (equivalent to eg. `void` in
+ * C or `()` in Rust).
+ */
 export class Unit{
     toBytes(): Array<u8> {
-        return  new Array<u8>(0);
+        return new Array<u8>(0);
     }
 
     static fromBytes(bytes: Uint8Array): Unit{
