1a078074b4b44120fe0bba193b01c3b9af6d7154
Mateusz Górski <gorski.mateusz@protonmail.ch>
2020-03-17 12:38:02 +0100

    NODE-1300: Add `headOption` to `StreamT.`

--- shared/src/test/scala/io/casperlabs/shared/StreamTSpec.scala
+++ shared/src/test/scala/io/casperlabs/shared/StreamTSpec.scala
@@ -53,6 +53,18 @@ class StreamTSpec extends FunSpec with Matchers with GeneratorDrivenPropertyChec
       }
     }
 
+    it("should correctly compute headOption") {
+      forAll { (list: List[Int]) =>
+        whenever(list.nonEmpty) {
+          val stream: StreamT[Id, Int] = StreamT.fromList[Id, Int](list)
+
+          stream.headOption[Int](mErrId) shouldBe Some(list.head)
+
+          StreamT.fromList[Id, Int](List.empty).headOption[Int](mErrId) shouldBe none[Int]
+        }
+      }
+    }
+
     it("should correctly compute tails") {
       forAll { (list: List[Int]) =>
         whenever(list.nonEmpty) {
