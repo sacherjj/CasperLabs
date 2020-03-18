ef4044714af249b268b2428d5e34efd363c78cb8
Bartłomiej Kamiński <bart@casperlabs.io>
2020-03-18 15:45:38 +0100

    EE-940: Add more comments to unit.ts

--- execution-engine/contract-as/assembly/unit.ts
+++ execution-engine/contract-as/assembly/unit.ts
@@ -3,10 +3,16 @@
  * C or `()` in Rust).
  */
 export class Unit{
+	/**
+	 * Serializes a [[Unit]] - returns an empty array.
+	 */
     toBytes(): Array<u8> {
         return new Array<u8>(0);
     }
 
+	/**
+	 * Deserializes a [[Unit]] - returns a new [[Unit]].
+	 */
     static fromBytes(bytes: Uint8Array): Unit{
         return new Unit();
     }
