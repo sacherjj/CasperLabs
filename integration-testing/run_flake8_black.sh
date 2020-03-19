bebf785595188c9ab790b6ad84ab46d62749d3f0
Piotr Kuchta <piotr@casperlabs.io>
2020-03-19 20:54:16 +0100

    NODE-1297: Debugging CI issue

--- integration-testing/run_flake8_black.sh
+++ integration-testing/run_flake8_black.sh
@@ -5,6 +5,6 @@ PYTHON=python3.7
 
 $PYTHON -m pip install pipenv
 $PYTHON -m pipenv sync
-$PYTHON -m pipenv run pre-commit install || (echo Installation of pre-commit failed:; cat /root/.cache/pre-commit/pre-commit.log)
+$PYTHON -m pipenv run pre-commit install
 EXCLUDE_PATTERN="ignore_test_.*.py\|.*_pb2.py\|.*_pb2_grpc.py\|CasperLabsClient\/build\/lib\/\|.eggs"
-$PYTHON -m pipenv run pre-commit run --files $(find . -type f -name \*.py | grep -v $EXCLUDE_PATTERN)
+$PYTHON -m pipenv run pre-commit run --files $(find . -type f -name \*.py | grep -v $EXCLUDE_PATTERN) || (echo pre-commit run failed:; cat /root/.cache/pre-commit/pre-commit.log)
