3e30031281834c96fc0ea97acb42c92472c90ee1
Akosh Farkash <aakoshh@gmail.com>
2020-03-23 11:17:04 +0000

    NODE-1311: Register nodes automatically.

--- hack/docker/stests/start-console.sh
+++ hack/docker/stests/start-console.sh
@@ -19,6 +19,16 @@ echo 1.  Register network + faucet key:
 stests-set-network $NETWORK
 stests-set-network-faucet-key $NETWORK $KEYS/faucet-account/account-private.pem
 
+echo 2.  Register nodes + node bonding keys:
 
-# Switch to interactive mode.
+for NODE in $NODES; do
+  echo Registering $NODE...
+  n=$(echo $NODE | sed -r 's/node-([0-9])/\1/')
+  i=$(( n + 1 ))
+  stests-set-node $NETWORK:${i} ${NODE}:40401 full
+  stests-set-node-bonding-key $NETWORK:${i} $KEYS/account-${n}/account-private.pem
+done
+
+echo 3.  Switching to interactive mode:
+
 exec bash
