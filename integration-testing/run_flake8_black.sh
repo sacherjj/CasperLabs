cce280587c055d71eeb2e137d1f5cd18d75cde30
Piotr Kuchta <piotr@casperlabs.io>
2020-03-23 12:53:53 +0100

    NODE-1297: Remove not needed python3.7-venv. Update pattern to match all generated files.

--- integration-testing/run_flake8_black.sh
+++ integration-testing/run_flake8_black.sh
@@ -6,5 +6,5 @@ PYTHON=python3.7
 $PYTHON -m pip install pipenv
 $PYTHON -m pipenv sync
 $PYTHON -m pipenv run pre-commit install
-EXCLUDE_PATTERN="ignore_test_.*.py\|.*_pb2.py\|.*_pb2_grpc.py\|CasperLabsClient\/build\/lib\/\|.eggs"
+EXCLUDE_PATTERN="ignore_test_.*.py\|.*_pb2.py\|.*_grpc.py\|CasperLabsClient\/build\/lib\/\|.eggs"
 $PYTHON -m pipenv run pre-commit run --files $(find . -type f -name \*.py | grep -v $EXCLUDE_PATTERN) || (echo pre-commit run failed:; cat /root/.cache/pre-commit/pre-commit.log)
