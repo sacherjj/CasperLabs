68abc05553c6d5b5c288835bd3da86e7bdff11e2
Piotr Kuchta <piotr@casperlabs.io>
2020-03-30 23:52:50 +0200

    NODE-1323: Finish off the test

--- integration-testing/test/test_max_block_size_bytes.py
+++ integration-testing/test/test_max_block_size_bytes.py
@@ -1,7 +1,6 @@
 from casperlabs_local_net.casperlabs_accounts import GENESIS_ACCOUNT, Account
 from casperlabs_client.abi import ABI
 from casperlabs_client.utils import bundled_contract
-import logging
 
 
 def make_transfers(client, account, target_account, amount, n):
@@ -51,7 +50,11 @@ def make_transfers(client, account, target_account, amount, n):
     return deploy_hash, deploy_hashes
 
 
+# chainspec_upgrades_network_minor:
 # resources/test-chainspec-minor/genesis/manifest.toml:  max-block-size-bytes = 10485760
+MAX_BLOCK_SIZE_BYTES = 10485760
+
+
 def test_max_block_size_bytes(chainspec_upgrades_network_minor):
     net = chainspec_upgrades_network_minor
     node = net.docker_nodes[0]
@@ -59,7 +62,11 @@ def test_max_block_size_bytes(chainspec_upgrades_network_minor):
     account = GENESIS_ACCOUNT
     target_account = Account(1)
     amount = 1
-    number_of_deploys = 15
+
+    # Scenario 1. Deploy 10 deploys, each ~800kB. All should fit into MAX_BLOCK_SIZE_BYTES block
+    # (to be precise, the one deploy that all others depend on will be included in a separate
+    # block, so only 9 deploys need to fit in one block).
+    number_of_deploys = 10
 
     deploy_hash, deploy_hashes = make_transfers(
         client, account, target_account, amount, number_of_deploys
@@ -67,15 +74,50 @@ def test_max_block_size_bytes(chainspec_upgrades_network_minor):
 
     # Wait for the deploy that all others depend on to be included in a block.
     deploy_info = client.showDeploy(deploy_hash, wait_for_processed=True)
-    logging.info(f"==>  {deploy_info}")
 
     # Retrieve info on all deploys included in the block.
-    deploy_infos = list(
-        client.showDeploys(
-            deploy_info.processing_results[0].block_info.summary.block_hash.hex(),
-            full_view=False,
-        )
-    )
-    logging.info(f"==>  {deploy_infos}")
+    block_hash = deploy_info.processing_results[0].block_info.summary.block_hash.hex()
+    deploy_infos = list(client.showDeploys(block_hash, full_view=False))
+    assert len(deploy_infos) == 1
 
-    assert len(deploy_infos) > 1
+    deploy_info = client.showDeploy(deploy_hashes[0], wait_for_processed=True)
+    block_hash = deploy_info.processing_results[0].block_info.summary.block_hash.hex()
+    deploys_on_block = [
+        d.deploy.deploy_hash.hex()
+        for d in client.showDeploys(block_hash, full_view=False)
+    ]
+
+    # All deploys should be included in the block.
+    assert set(deploys_on_block) == set(deploy_hashes)
+
+    block_info = client.showBlock(block_hash)
+    assert block_info.status.stats.block_size_bytes < MAX_BLOCK_SIZE_BYTES
+
+    # Scenario 2. Deploy 20 deploys, not all will fit into a block.
+    number_of_deploys = 20
+
+    deploy_hash, deploy_hashes = make_transfers(
+        client, account, target_account, amount, number_of_deploys
+    )
+
+    # Wait for the deploy that all others depend on to be included in a block.
+    deploy_info = client.showDeploy(deploy_hash, wait_for_processed=True)
+
+    # Retrieve info on all deploys included in the block.
+    block_hash = deploy_info.processing_results[0].block_info.summary.block_hash.hex()
+    deploy_infos = list(client.showDeploys(block_hash, full_view=False))
+    assert len(deploy_infos) == 1
+
+    deploy_info = client.showDeploy(deploy_hashes[0], wait_for_processed=True)
+    block_hash = deploy_info.processing_results[0].block_info.summary.block_hash.hex()
+    deploys_on_block = [
+        d.deploy.deploy_hash.hex()
+        for d in client.showDeploys(block_hash, full_view=False)
+    ]
+
+    # Only subset of the deploys can be included in the block.
+    assert len(deploys_on_block) < len(deploy_hashes)
+    assert set(deploys_on_block).issubset(set(deploy_hashes))
+
+    block_info = client.showBlock(block_hash)
+    assert block_info.status.stats.block_size_bytes < MAX_BLOCK_SIZE_BYTES
