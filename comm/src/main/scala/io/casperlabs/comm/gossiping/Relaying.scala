026520e54c2cb44577201ac126a252cdbeed6145
Igor Ramazanov <igor@casperlabs.io>
2020-04-02 17:08:13 +0300

    NODE-1178: Implement DeployRelaying reusing the extracted common code

--- comm/src/main/scala/io/casperlabs/comm/gossiping/Relaying.scala
+++ comm/src/main/scala/io/casperlabs/comm/gossiping/Relaying.scala
@@ -166,3 +166,65 @@ class BlockRelayingImpl[F[_]](
     service.newBlocks(NewBlocksRequest(local.some, blockHashes)).map(_.isNew)
   override val requestName = "NewBlocks"
 }
+
+object DeployRelayingImpl {
+  implicit val metricsSource: Metrics.Source =
+    Metrics.Source(DeployGossipingMetricsSource, "Relaying")
+
+  /** Export base 0 values so we have non-empty series for charts. */
+  def establishMetrics[F[_]: Monad: Metrics] =
+    for {
+      _ <- Metrics[F].incrementCounter("relay_accepted", 0)
+      _ <- Metrics[F].incrementCounter("relay_rejected", 0)
+      _ <- Metrics[F].incrementCounter("relay_failed", 0)
+    } yield ()
+
+  def apply[F[_]: ContextShift: Concurrent: Parallel: Log: Metrics: NodeAsk](
+      egressScheduler: Scheduler,
+      nodeDiscovery: NodeDiscovery[F],
+      connectToGossip: GossipService.Connector[F],
+      relayFactor: Int,
+      relaySaturation: Int,
+      isSynchronous: Boolean = false
+  ): DeployRelaying[F] = {
+    val maxToTry = if (relaySaturation == 100) {
+      Int.MaxValue
+    } else {
+      (relayFactor * 100) / (100 - relaySaturation)
+    }
+    new DeployRelayingImpl[F](
+      egressScheduler,
+      nodeDiscovery,
+      connectToGossip,
+      relayFactor,
+      maxToTry,
+      isSynchronous
+    )
+  }
+}
+
+/**
+  * https://techspec.casperlabs.io/technical-details/global-state/communications#picking-nodes-for-gossip
+  */
+class DeployRelayingImpl[F[_]](
+    val egressScheduler: Scheduler,
+    val nodeDiscovery: NodeDiscovery[F],
+    val connectToGossip: Node => F[GossipService[F]],
+    val relayFactor: Int,
+    val maxToTry: Int,
+    val isSynchronous: Boolean
+)(
+    implicit
+    override val CS: ContextShift[F],
+    override val C: Concurrent[F],
+    override val P: Parallel[F],
+    override val L: Log[F],
+    override val M: Metrics[F],
+    override val N: NodeAsk[F],
+    override val S: Metrics.Source
+) extends DeployRelaying[F]
+    with RelayingImpl[F] {
+  override val request = (service, local, blockHashes) =>
+    service.newDeploys(NewDeploysRequest(local.some, blockHashes)).map(_.isNew)
+  override val requestName = "NewDeploys"
+}
