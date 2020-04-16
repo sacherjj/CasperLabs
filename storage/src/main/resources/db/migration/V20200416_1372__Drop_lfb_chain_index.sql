1abb331f46f449025e10e134ba57805c08e0a650
Akosh Farkash <aakoshh@gmail.com>
2020-04-16 23:42:46 +0100

    NODE-1372: Dropping index instead, it's not used.

---
+++ storage/src/main/resources/db/migration/V20200416_1372__Drop_lfb_chain_index.sql
@@ -0,0 +1,2 @@
+-- No longer in use. We use the lfb_chain for last finalized block.
+DROP INDEX lfb_main_chain_idx;
--- storage/src/main/resources/db/migration/V20200416_1372__Recreate_LFB_index.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX lfb_main_chain_idx;
-
-CREATE INDEX lfb_main_chain_idx ON block_metadata (j_rank, block_hash) WHERE is_main_chain and is_finalized;
