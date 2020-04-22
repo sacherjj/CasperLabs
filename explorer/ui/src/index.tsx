0e88bd042532cf9aea635559e83f4b4cf033930c
Abner Zheng <abnerzheng@gmail.com>
2020-04-22 16:52:22 +0800

    ECO-349: replace default font with Sarabun.

--- explorer/ui/src/index.tsx
+++ explorer/ui/src/index.tsx
@@ -5,12 +5,8 @@ import { HashRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-// https://startbootstrap.com/template-overviews/sb-admin/
-import './styles/sb-admin/sb-admin.scss';
 import './styles/custom.scss';
-
 // Make `jQuery` available in the window in case any Javascript we import directly uses it.
 import * as jQuery from 'jquery';
 
