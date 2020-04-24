dd8633d703dc494751c0931aec430cf547a19600
Michał Papierski <michal@casperlabs.io>
2020-04-24 18:15:39 +0200

    EE-{974,975}: Refactor mailing example.
    Adds versioning.
    
    Co-authored-by: Joe Sacher <joe@casperlabs.io>

--- execution-engine/contracts/examples/mailing-list-define/src/main.rs
+++ execution-engine/contracts/examples/mailing-list-define/src/main.rs
@@ -6,23 +6,28 @@ extern crate alloc;
 // Can be removed once https://github.com/rust-lang/rustfmt/issues/3362 is resolved.
 #[rustfmt::skip]
 use alloc::vec;
-use alloc::{collections::BTreeMap, string::String, vec::Vec};
+use alloc::{
+    boxed::Box,
+    collections::BTreeMap,
+    string::{String, ToString},
+    vec::Vec,
+};
 use core::convert::TryInto;
 
 use contract::{
     contract_api::{runtime, storage},
     unwrap_or_revert::UnwrapOrRevert,
 };
-use types::{ApiError, CLValue, Key, URef};
+use types::{
+    ApiError, Arg, CLType, CLValue, EntryPoint, EntryPointAccess, EntryPointType, Key, SemVer, URef,
+};
 
 const LIST_KEY: &str = "list";
 const MAILING_KEY: &str = "mailing";
 const MAILING_LIST_EXT: &str = "mailing_list_ext";
 
-enum Arg {
-    MethodName = 0,
-    Arg1 = 1,
-}
+const ARG_METHOD_NAME: &str = "method_name";
+const ARG_ARG1: &str = "arg1";
 
 #[repr(u16)]
 enum Error {
@@ -72,10 +77,10 @@ fn publish(msg: String) {
 
 #[no_mangle]
 pub extern "C" fn mailing_list_ext() {
-    let method_name: String = runtime::get_arg(Arg::MethodName as u32)
+    let method_name: String = runtime::get_named_arg(ARG_METHOD_NAME)
         .unwrap_or_revert_with(ApiError::MissingArgument)
         .unwrap_or_revert_with(ApiError::InvalidArgument);
-    let arg1: String = runtime::get_arg(Arg::Arg1 as u32)
+    let arg1: String = runtime::get_named_arg(ARG_ARG1)
         .unwrap_or_revert_with(ApiError::MissingArgument)
         .unwrap_or_revert_with(ApiError::InvalidArgument);
     match method_name.as_str() {
@@ -89,13 +94,11 @@ pub extern "C" fn mailing_list_ext() {
                 runtime::ret(return_value)
             }
         },
-        //Note that this is totally insecure. In reality
-        //the pub method would be only available under an
-        //unforgable reference because otherwise anyone could
-        //spam the mailing list.
-        "pub" => {
-            publish(arg1);
-        }
+        // Note that this is totally insecure. In reality
+        // the pub method would be only available under an
+        // unforgable reference because otherwise anyone could
+        // spam the mailing list.
+        "pub" => publish(arg1),
         _ => runtime::revert(Error::UnknownMethodName),
     }
 }
@@ -105,11 +108,35 @@ pub extern "C" fn call() {
     let init_list: Vec<String> = Vec::new();
     let list_key = storage::new_uref(init_list);
 
-    //create map of references for stored contract
+    let (metadata_hash, access_uref) = storage::create_contract_metadata_at_hash();
+    runtime::put_key(MAILING_KEY, metadata_hash);
+
+    let methods = {
+        let mut methods = BTreeMap::new();
+        let entrypoint_hello = EntryPoint::new(
+            vec![
+                Arg::new(ARG_METHOD_NAME, CLType::String),
+                Arg::new(ARG_ARG1, CLType::String),
+            ],
+            CLType::Option(Box::new(CLType::Key)),
+            EntryPointAccess::Public,
+            EntryPointType::Session,
+        );
+        methods.insert(MAILING_LIST_EXT.to_string(), entrypoint_hello);
+        methods
+    };
+
+    // Create map of references for stored contract
     let mut mailing_list_urefs: BTreeMap<String, Key> = BTreeMap::new();
     let key_name = String::from(LIST_KEY);
     mailing_list_urefs.insert(key_name, list_key.into());
 
-    let pointer = storage::store_function_at_hash(MAILING_LIST_EXT, mailing_list_urefs);
-    runtime::put_key(MAILING_KEY, pointer.into())
+    storage::add_contract_version(
+        metadata_hash,
+        access_uref,
+        SemVer::V1_0_0,
+        methods,
+        mailing_list_urefs,
+    )
+    .unwrap_or_revert();
 }
