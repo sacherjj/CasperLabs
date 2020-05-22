86fb8f28be45d3e158c21de5b0de4c5fd33de9de
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-05-21 22:28:18 -0400

    Improving variable naming.

--- execution-engine/contracts/test/transfer-main-purse-to-two-purses/src/main.rs
+++ execution-engine/contracts/test/transfer-main-purse-to-two-purses/src/main.rs
@@ -29,25 +29,25 @@ enum CustomError {
 pub extern "C" fn call() {
     let main_purse: URef = account::get_main_purse();
 
-    let dst_purse_one_name: String = runtime::get_arg(Args::DestinationPurseOne as u32)
+    let destination_purse_one_name: String = runtime::get_arg(Args::DestinationPurseOne as u32)
         .unwrap_or_revert_with(ApiError::MissingArgument)
         .unwrap_or_revert_with(ApiError::InvalidArgument);
 
-    let dst_purse_one = if !runtime::has_key(&dst_purse_one_name) {
+    let destination_purse_one = if !runtime::has_key(&destination_purse_one_name) {
         // Create and store purse if doesn't exist
         let purse = system::create_purse();
-        runtime::put_key(&dst_purse_one_name, purse.into());
+        runtime::put_key(&destination_purse_one_name, purse.into());
         purse
     } else {
-        let destination_purse_one_key =
-            runtime::get_key(&dst_purse_one_name).unwrap_or_revert_with(ApiError::InvalidArgument);
+        let destination_purse_one_key = runtime::get_key(&destination_purse_one_name)
+            .unwrap_or_revert_with(ApiError::InvalidArgument);
         match destination_purse_one_key.as_uref() {
             Some(uref) => *uref,
             None => runtime::revert(ApiError::UnexpectedKeyVariant),
         }
     };
 
-    let dst_purse_two_name: String = runtime::get_arg(Args::DestinationPurseTwo as u32)
+    let destination_purse_two_name: String = runtime::get_arg(Args::DestinationPurseTwo as u32)
         .unwrap_or_revert_with(ApiError::MissingArgument)
         .unwrap_or_revert_with(ApiError::InvalidArgument);
 
@@ -55,14 +55,14 @@ pub extern "C" fn call() {
         .unwrap_or_revert_with(ApiError::MissingArgument)
         .unwrap_or_revert_with(ApiError::InvalidArgument);
 
-    let dst_purse_two = if !runtime::has_key(&dst_purse_two_name) {
+    let destination_purse_two = if !runtime::has_key(&destination_purse_two_name) {
         // Create and store purse if doesn't exist
         let purse = system::create_purse();
-        runtime::put_key(&dst_purse_two_name, purse.into());
+        runtime::put_key(&destination_purse_two_name, purse.into());
         purse
     } else {
-        let destination_purse_two_key =
-            runtime::get_key(&dst_purse_two_name).unwrap_or_revert_with(ApiError::InvalidArgument);
+        let destination_purse_two_key = runtime::get_key(&destination_purse_two_name)
+            .unwrap_or_revert_with(ApiError::InvalidArgument);
         match destination_purse_two_key.as_uref() {
             Some(uref) => *uref,
             None => runtime::revert(ApiError::UnexpectedKeyVariant),
@@ -72,8 +72,8 @@ pub extern "C" fn call() {
         .unwrap_or_revert_with(ApiError::MissingArgument)
         .unwrap_or_revert_with(ApiError::InvalidArgument);
 
-    system::transfer_from_purse_to_purse(main_purse, dst_purse_one, transfer_amount_one)
+    system::transfer_from_purse_to_purse(main_purse, destination_purse_one, transfer_amount_one)
         .unwrap_or_revert_with(ApiError::User(CustomError::TransferToPurseOneFailed as u16));
-    system::transfer_from_purse_to_purse(main_purse, dst_purse_two, transfer_amount_two)
+    system::transfer_from_purse_to_purse(main_purse, destination_purse_two, transfer_amount_two)
         .unwrap_or_revert_with(ApiError::User(CustomError::TransferToPurseTwoFailed as u16));
 }
