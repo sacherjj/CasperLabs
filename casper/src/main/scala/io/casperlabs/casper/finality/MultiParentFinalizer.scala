92a59d1b8b77184ca7478461a3b14212d13e06da
Akosh Farkash <aakoshh@gmail.com>
2020-06-03 10:18:32 +0100

    TNET-36: Remove leftover Log import.

--- casper/src/main/scala/io/casperlabs/casper/finality/MultiParentFinalizer.scala
+++ casper/src/main/scala/io/casperlabs/casper/finality/MultiParentFinalizer.scala
@@ -8,7 +8,6 @@ import io.casperlabs.casper.CasperMetricsSource
 import io.casperlabs.casper.finality.MultiParentFinalizer.FinalizedBlocks
 import io.casperlabs.metrics.implicits._
 import io.casperlabs.metrics.Metrics
-import io.casperlabs.shared.Log
 import io.casperlabs.models.Message
 import io.casperlabs.storage.dag.DagRepresentation
 import simulacrum.typeclass
