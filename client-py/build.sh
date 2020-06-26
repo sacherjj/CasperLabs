ca80dd29f06875f1dc10a926370cf1d80f4aa34c
Joe Sacher <321623+sacherjj@users.noreply.github.com>
2020-06-26 11:38:37 -0400

    Removing .wasm check in build.sh and unused constant for wasm in consts.py

--- client-py/build.sh
+++ client-py/build.sh
@@ -7,17 +7,5 @@ cd ${DIR}
 
 rm -rf dist/*
 
-# Verify .wasm has been generated
-count=`ls -1 casperlabs_client/*.wasm 2>/dev/null | wc -l`
-if [ $count == 0 ]
-then
-  echo "--------------------------------------------"
-  echo "REQUIRED FILES MISSING"
-  echo ".wasm files have not been built"
-  echo "Run with 'make build-python-client' in root"
-  echo "--------------------------------------------"
-  exit 1
-fi
-
 pipenv sync
 pipenv run python setup.py sdist
